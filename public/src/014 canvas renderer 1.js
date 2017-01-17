42502cd586203719520817cba32282888bc4b729
Richard Davey <rdavey@gmail.com>
2017-01-17 17:26:58 +0000

    Canvas renderer tests

--- public/src/014 canvas renderer 1.js
+++ public/src/014 canvas renderer 1.js
@@ -1,16 +1,22 @@
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create
+    }
+};
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(config);
 
 function preload() {
 
-    game.load.image('einstein', 'assets/pics/ra_einstein.png');
+    this.load.image('einstein', 'assets/pics/ra_einstein.png');
 
 }
 
-var image;
-
 function create() {
 
-    image = game.add.image(0, 0, 'einstein', null, game.stage);
+    var image = this.add.image(0, 0, 'einstein');
 
 }

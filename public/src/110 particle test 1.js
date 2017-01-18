b12ca4653b80772399ae5fae9347a470fcdc6e32
Richard Davey <rdavey@gmail.com>
2017-01-18 01:19:00 +0000

    Added particle renderer test.

---
+++ public/src/110 particle test 1.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+    this.load.image('bunny', 'assets/sprites/bunny.png');
+
+}
+
+function create() {
+
+
+}
+
+function update() {
+
+
+}

f91f10e037bf12d3ab755e02ed7186d7b6e655d1
photonstorm <rich@photonstorm.com>
2017-01-20 19:06:16 +0000

    Atlas test.

---
+++ public/src/sprite renderer/atlas frame 1.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+    this.load.atlas('atlas', 'assets/atlas/megasetHD-0.png', 'assets/atlas/megasetHD-0.json');
+
+}
+
+function create() {
+
+    this.add.image(0, 0, 'atlas', 'aya_touhou_teng_soldier');
+    this.add.image(260, 60, 'atlas', 'exocet_spaceman');
+    this.add.image(120, 300, 'atlas', 'hotdog');
+
+}
+
+function update() {
+
+}

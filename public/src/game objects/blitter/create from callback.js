88193162a69da4be5634889c372818434dfc2d8e
photonstorm <rich@photonstorm.com>
2017-01-24 13:25:34 +0000

    New blitter examples.

---
+++ public/src/game objects/blitter/create from callback.js
@@ -0,0 +1,45 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var x = 0;
+var y = 0;
+var i = 0;
+var t = 16;
+
+function preload() {
+
+    this.load.image('ball', 'assets/demoscene/blue_ball.png');
+
+}
+
+function create() {
+
+    var blitter = this.add.blitter(0, 0, 'ball');
+
+    blitter.createFromCallback(placeBob.bind(this), t * 7);
+
+}
+
+function placeBob(bob) {
+
+    if (i % 16 === 0)
+    {
+        x += 96;
+        y = 0;
+    }
+
+    y += 32;
+
+    bob.x = x + (Math.sin(y) * 16);
+    bob.y = y;
+
+    i++;
+}

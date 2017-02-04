febc5cd02d0817d8215747bf520e162abe11cf1e
Richard Davey <rdavey@gmail.com>
2017-02-04 20:01:07 +0000

    Working on mini Nemesis game to show canvas zooming.

---
+++ public/src/canvas renderer/zoom x2.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.CANVAS,
+    width: 160,
+    height: 144,
+    zoom: 4,
+    pixelArt: true,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+    this.load.image('title', 'assets/tests/zoom/title.png');
+
+}
+
+function create() {
+
+    var image = this.add.image(0, 0, 'title');
+
+}

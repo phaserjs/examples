e80ef000eaf82260c3f972a488a479a780efd916
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-02-08 14:27:36 -0300

    camera overlap example

---
+++ public/src/camera/camera-overlap.js
@@ -0,0 +1,41 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    width: 800,
+    height: 600
+};
+
+var game = new Phaser.Game(config);
+var iter = 0;
+var image;
+var smallCamera;
+
+function preload() {
+
+    this.load.image('einstein', 'assets/tests/camera/CherilPerils.png');
+
+}
+
+function create() {
+
+    image = this.add.image(0, 0, 'einstein');
+    smallCamera = this.sys.addCamera(580, 20, 200, 150);
+}
+function update()
+{
+    var halfWidth = image.texture.source[0].width / 2;
+    var quarterWidth = halfWidth / 2;
+    var halfHeight = image.texture.source[0].height / 2;
+    var quarterHeight = halfHeight / 2;
+
+    smallCamera.scrollX = (halfWidth - quarterWidth + (Math.cos(iter) * quarterWidth))|0;
+    smallCamera.scrollY = halfHeight;
+    smallCamera.rotation += 0.01;
+    smallCamera.zoom = 1 + Math.abs(Math.sin(iter) * 2);
+    iter += 0.02;
+}

15880afc109dc86c3a000df7fb9bcc3603fe775f
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-02-28 18:12:36 -0300

    Graphics rendering example

---
+++ public/src/game objects/graphics/Graphics Rendering.js
@@ -0,0 +1,49 @@
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    state: {
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function create() {
+
+   var graphics = this.add.graphics();
+
+   // set a fill and line style
+    graphics.beginFill(0xFF3300);
+    
+    // draw a shape
+    graphics.moveTo(50,50);
+    graphics.lineTo(250, 50);
+    graphics.lineTo(100, 100);
+    graphics.lineTo(250, 220);
+    graphics.lineTo(50, 220);
+    graphics.lineTo(50, 50);
+    graphics.endFill();
+    
+    // set a fill and line style again
+    graphics.beginFill(0xFF700B);
+    
+    // draw a second shape
+    graphics.moveTo(210,300);
+    graphics.lineTo(450,320);
+    graphics.lineTo(570,350);
+    graphics.lineTo(330,120);
+    graphics.lineTo(410,200);
+    graphics.lineTo(210,300);
+    graphics.endFill();
+    
+    // draw a rectangle
+    graphics.drawRect(50, 250, 100, 100);
+    
+    // draw a circle
+    graphics.beginFill(0xFFFF0B, 0.5);
+    graphics.drawCircle(470, 200, 200);
+    graphics.endFill();
+
+    graphics.moveTo(30,30);
+    graphics.lineTo(600, 300);
+}

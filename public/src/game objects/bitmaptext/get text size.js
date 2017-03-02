cf350539c12ffcc16e7e55d2086f9b93f0ea0505
Richard Davey <rdavey@gmail.com>
2017-03-02 23:35:09 +0000

    Text size tests.

--- public/src/game objects/bitmaptext/get text size.js
+++ public/src/game objects/bitmaptext/get text size.js
@@ -10,8 +10,12 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var text;
-var bounds;
+var text1;
+var text2;
+var text3;
+var bounds1;
+var bounds2;
+var bounds3;
 
 function preload() 
 {
@@ -22,22 +26,25 @@ function preload()
 
 function create() 
 {
-    // text = this.add.bitmapText(0, 0, 'desyrel', 'welcome to the jungle\nwe got fun and games');
-    // text = this.add.bitmapText(0, 0, 'atari', 'Welcome!', 70);
-    // text = this.add.bitmapText(100, 100, 'gothic', 'Welcome to the Jungle!', 40);
-    text = this.add.bitmapText(100, 100, 'hyper', 'Terminator 2', 128);
+    text1 = this.add.bitmapText(32, 32, 'atari', 'Welcome!', 70);
+    text2 = this.add.bitmapText(64, 160, 'gothic', 'Welcome to the Jungle!', 40);
+    text3 = this.add.bitmapText(100, 300, 'hyper', 'Terminator', 128);
 
-    // text.setScale(2);
-
-    bounds = text.getTextBounds();
-
-    console.log(bounds);
+    bounds1 = text1.getTextBounds();
+    bounds2 = text2.getTextBounds();
+    bounds3 = text3.getTextBounds();
 }
 
 function render()
 {
     var ctx = this.sys.context;
 
-    ctx.strokeStyle = 'rgba(0,255,0,1)';
-    ctx.strokeRect(bounds.x, bounds.y, bounds.width, bounds.height);
+    ctx.strokeStyle = 'rgba(255,0,0,1)';
+    ctx.strokeRect(bounds1.x, bounds1.y, bounds1.width, bounds1.height);
+
+    ctx.strokeStyle = 'rgba(255,255,0,1)';
+    ctx.strokeRect(bounds2.x, bounds2.y, bounds2.width, bounds2.height);
+
+    ctx.strokeStyle = 'rgba(0,225,0,1)';
+    ctx.strokeRect(bounds3.x, bounds3.y, bounds3.width, bounds3.height);
 }

142b2ebb3f172320d149711f7d4ae0352325bf93
Richard Davey <rdavey@gmail.com>
2017-04-05 17:06:11 +0100

    New Animation debug example and sprite sheet.

---
+++ public/src/animation/animation data.js
@@ -0,0 +1,72 @@
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    backgroundColor: '#7d7d7d',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var anim;
+var sprite;
+var progress;
+var frameView;
+
+function preload ()
+{
+    this.load.spritesheet('mummy', 'assets/animations/mummy37x45.png', { frameWidth: 37, frameHeight: 45 });
+}
+
+function create ()
+{
+    //  Frame debug view
+
+    frameView = this.add.graphics({ fillStyle: { color: 0xff00ff }, x: 32, y: 32 });
+
+    //  Show the whole animation sheet
+    this.add.image(32, 32, 'mummy', '__BASE').setOrigin(0);
+
+    var config = {
+        frames: this.anims.generateFrameNumbers('mummy'),
+        framerate: 6,
+        yoyo: true,
+        repeat: 4
+    };
+
+    anim = this.anims.create('walk', config);
+
+    console.log(anim);
+
+    sprite = this.add.sprite(400, 300, 'mummy').setScale(4);
+    sprite.anims.load('walk');
+
+    //  Debug text
+
+    progress = this.add.text(100, 500, 'Progress: 0%', { fill: '#00ff00' });
+
+    document.addEventListener('mouseup', function () {
+
+        sprite.anims.play('walk');
+
+    });
+
+}
+
+function updateFrameView ()
+{
+    frameView.clear();
+    frameView.fillRect(sprite.frame.cutX, 0, 37, 45);
+}
+
+function update ()
+{
+    updateFrameView();
+
+    progress.setText('Progress: ' + sprite.anims.progress() + '%');
+}

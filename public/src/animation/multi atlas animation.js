45670d303e623ab23204c85236e1d852bc0a1bd1
Richard Davey <rdavey@gmail.com>
2017-04-08 01:14:11 +0100

    New multi-atlas animation.

---
+++ public/src/animation/multi atlas animation.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    pixelArt: true,
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.path = 'assets/animations/cybercity/';
+
+    this.load.multiatlas('cybercity', 2);
+}
+
+function create ()
+{
+    this.anims.create('fly', { frames: this.anims.generateFrameNames('cybercity', { start: 0, end: 98 }), repeat: -1 });
+
+    this.add.sprite().setScale(2.7).play('fly');
+}

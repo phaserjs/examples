cbd932c6eb878d1c0768f125db77aa7e0c8ed6db
Richard Davey <rdavey@gmail.com>
2017-04-21 03:11:55 +0100

    More examples

--- public/src/game objects/render pass/Image Morph.js
+++ public/src/game objects/render pass/Image Morph.js
@@ -14,26 +14,22 @@ var config = {
 var effect;
 var time = 0;
 var tex;
+var image1;
 var renderPassMaskApply;
 
 var maskApply = [
     'precision mediump float;',
     'uniform sampler2D sampler;',
     'uniform sampler2D mask;',
-    'uniform sampler2D pic2;',
     'varying vec2 v_tex_coord;',
     'varying vec3 v_color;',
     'varying float v_alpha;',
     'void main(void) {',
     '   vec2 uv = vec2(gl_FragCoord.x / 800.0, gl_FragCoord.y / 600.0);',
     '   vec4 color = texture2D(sampler, v_tex_coord);',
-    '   vec4 color2 = texture2D(pic2, v_tex_coord);',
     '   vec4 maskColor = texture2D(mask, uv);',
-    //'   gl_FragColor = color * maskColor;',
     '   if (maskColor.r == 0.0) {',
     '     gl_FragColor = color;',
-    '   } else {',
-    '     gl_FragColor = color2 * maskColor;',
     '   }',
     '}'
 ].join('\n');
@@ -42,26 +38,21 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    // this.load.glsl('plasma', 'assets/shaders/plasma-mask.frag');
-    this.load.glsl('plasma', 'assets/shaders/distance-field-morph.frag');
-    this.load.image('einstein', 'assets/pics/cougar-dragonsun.png');
-    this.load.image('hotshot', 'assets/pics/hotshot-chaos-in-tokyo.png');
+    this.load.glsl('morph', 'assets/shaders/distance-field-morph.frag');
+    this.load.image('dragon', 'assets/pics/cougar-dragonsun.png');
+    this.load.image('rain', 'assets/pics/thalion-rain.png');
 }
 
 function create ()
 {
-    effect = this.add.effectLayer(0, 0, 800, 600, 'maskEFfect', this.cache.shader.get('plasma'));
+    this.add.image(400, 300, 'rain').setScale(4);
+
+    effect = this.add.effectLayer(0, 0, 800, 600, 'maskEFfect', this.cache.shader.get('morph'));
     effect.renderOffScreen();
 
     renderPassMaskApply = this.add.renderPass(0, 0, 800, 600, 'maskApply', maskApply);
 
-    image1 = this.make.image({x: 400, y: 300, key: 'einstein', add: false});
-
-    var pic2 = this.textures.get('hotshot');
-
-    this.game.renderer.createTexture(pic2.source[0]);
-
-    tex = pic2.source[0].glTexture;
+    image1 = this.make.image({x: 400, y: 300, key: 'dragon', add: false});
 }
 
 function update ()
@@ -70,7 +61,6 @@ function update ()
     time += 0.01;
 
     renderPassMaskApply.setRenderTextureAt(effect.renderTexture, 'mask', 1);
-    renderPassMaskApply.setRenderTextureAt(tex, 'pic2', 2);
     renderPassMaskApply.clearColorBuffer(0, 0, 0, 0);
     renderPassMaskApply.render(image1, this.cameras.main);
 }
--- public/src/game objects/render pass/Ripples.js
+++ public/src/game objects/render pass/Ripples.js
@@ -14,27 +14,23 @@ var config = {
 var effect;
 var time = 0;
 var tex;
+var image1;
 var renderPassMaskApply;
 
 var maskApply = [
     'precision mediump float;',
     'uniform sampler2D sampler;',
     'uniform sampler2D mask;',
-    'uniform sampler2D pic2;',
     'varying vec2 v_tex_coord;',
     'varying vec3 v_color;',
     'varying float v_alpha;',
     'void main(void) {',
     '   vec2 uv = vec2(gl_FragCoord.x / 800.0, gl_FragCoord.y / 600.0);',
     '   vec4 color = texture2D(sampler, v_tex_coord);',
-    '   vec4 color2 = texture2D(pic2, v_tex_coord);',
     '   vec4 maskColor = texture2D(mask, uv);',
-    //'   gl_FragColor = color * maskColor;',
-    '   if (maskColor.r == 0.0) {',
-    '     gl_FragColor = color;',
-    '   } else {',
-    '     gl_FragColor = color2 * maskColor;',
-    '   }',
+    //'   if (maskColor.r == 0.0) {',
+    '     gl_FragColor = color * maskColor;',
+    //'   }',
     '}'
 ].join('\n');
 
@@ -42,35 +38,26 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    // this.load.glsl('plasma', 'assets/shaders/plasma-mask.frag');
-    this.load.glsl('plasma', 'assets/shaders/distance-field-morph.frag');
-    this.load.image('einstein', 'assets/pics/cougar-dragonsun.png');
-    this.load.image('hotshot', 'assets/pics/hotshot-chaos-in-tokyo.png');
+    this.load.glsl('ripples', 'assets/shaders/cosmic-ripples.frag');
+    this.load.image('dragon', 'assets/pics/cougar-dragonsun.png');
 }
 
 function create ()
 {
-    effect = this.add.effectLayer(0, 0, 800, 600, 'maskEFfect', this.cache.shader.get('plasma'));
+    effect = this.add.effectLayer(0, 0, 800, 600, 'maskEFfect', this.cache.shader.get('ripples'));
     effect.renderOffScreen();
 
     renderPassMaskApply = this.add.renderPass(0, 0, 800, 600, 'maskApply', maskApply);
 
-    image1 = this.make.image({x: 400, y: 300, key: 'einstein', add: false});
-
-    var pic2 = this.textures.get('hotshot');
-
-    this.game.renderer.createTexture(pic2.source[0]);
-
-    tex = pic2.source[0].glTexture;
+    image1 = this.make.image({x: 400, y: 300, key: 'dragon', add: false});
 }
 
 function update ()
 {
     effect.setFloat('time', time);
-    time += 0.01;
+    time += 0.025;
 
     renderPassMaskApply.setRenderTextureAt(effect.renderTexture, 'mask', 1);
-    renderPassMaskApply.setRenderTextureAt(tex, 'pic2', 2);
     renderPassMaskApply.clearColorBuffer(0, 0, 0, 0);
     renderPassMaskApply.render(image1, this.cameras.main);
 }

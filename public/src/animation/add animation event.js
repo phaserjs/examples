2d966bea24c2f4390b443b3d5d9dc3ed232b30da
Richard Davey <rdavey@gmail.com>
2017-05-02 16:59:33 +0100

    New Animation Examples

---
+++ public/src/animation/add animation event.js
@@ -0,0 +1,62 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    pixelArt: true,
+    width: 800,
+    height: 600,
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var i = 0;
+var y = 100;
+
+function preload ()
+{
+    this.load.atlas('gems', 'assets/tests/columns/gems.png', 'assets/tests/columns/gems.json');
+}
+
+function create ()
+{
+    //  Each time a new animation is added to the Animation Manager we'll call this function
+    this.anims.events.on('ADD_ANIMATION_EVENT', addAnimation.bind(this));
+
+    var _anims = this.anims;
+
+    //  Click to add an animation
+    document.addEventListener('mouseup', function () {
+
+        switch (i)
+        {
+            case 0:
+                _anims.create({ key: 'diamond', frames: _anims.generateFrameNames('gems', { prefix: 'diamond_', end: 15, zeroPad: 4 }), repeat: -1 });
+                break;
+
+            case 1:
+                _anims.create({ key: 'prism', frames: _anims.generateFrameNames('gems', { prefix: 'prism_', end: 6, zeroPad: 4 }), repeat: -1 });
+                break;
+
+            case 2:
+                _anims.create({ key: 'ruby', frames: _anims.generateFrameNames('gems', { prefix: 'ruby_', end: 6, zeroPad: 4 }), repeat: -1 });
+                break;
+
+            case 3:
+                _anims.create({ key: 'square', frames: _anims.generateFrameNames('gems', { prefix: 'square_', end: 14, zeroPad: 4 }), repeat: -1 });
+                break;
+        }
+
+        i++;
+
+    });
+}
+
+function addAnimation (event)
+{
+    this.add.sprite(400, y, 'gems').play(event.key);
+
+    y += 100;
+}
---
+++ public/src/animation/pause animation instances.js
@@ -0,0 +1,59 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    pixelArt: true,
+    width: 800,
+    height: 600,
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.atlas('gems', 'assets/tests/columns/gems.png', 'assets/tests/columns/gems.json');
+}
+
+function create ()
+{
+    var diamond = this.anims.create({ key: 'diamond', frames: this.anims.generateFrameNames('gems', { prefix: 'diamond_', end: 15, zeroPad: 4 }), repeat: -1 });
+    var prism = this.anims.create({ key: 'prism', frames: this.anims.generateFrameNames('gems', { prefix: 'prism_', end: 6, zeroPad: 4 }), repeat: -1 });
+    var ruby = this.anims.create({ key: 'ruby', frames: this.anims.generateFrameNames('gems', { prefix: 'ruby_', end: 6, zeroPad: 4 }), repeat: -1 });
+    var square = this.anims.create({ key: 'square', frames: this.anims.generateFrameNames('gems', { prefix: 'square_', end: 14, zeroPad: 4 }), repeat: -1 });
+
+    //  square added twice just to make sure there are more of them
+    var keys = [ 'diamond', 'prism', 'ruby', 'square', 'square' ];
+
+    var x = 100;
+    var y = 100;
+
+    for (var i = 0; i < 35; i++)
+    {
+        this.add.sprite(x, y, 'gems').play(keys[Phaser.Math.Between(0, 4)]);
+
+        x += 100;
+
+        if (x === 800)
+        {
+            x = 100;
+            y += 100;
+        }
+    }
+
+    document.addEventListener('mouseup', function () {
+
+        if (square.paused)
+        {
+            square.resume();
+        }
+        else
+        {
+            square.pause();
+        }
+
+    });
+
+}

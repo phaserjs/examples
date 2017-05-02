0cbd971c0b9f4cb24d423abda5ef7050c1c353f3
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-05-02 17:57:47 -0300

    Tile Sprite simple example

--- public/src/game objects/tile sprite/Example.js
+++ public/src/game objects/tile sprite/Example.js
@@ -11,20 +11,28 @@ var config = {
     }
 };
 
-var image;
+var image0;
+var image1;
 var game = new Phaser.Game(config);
+var iter = 0;
 
 function preload ()
 {
-    this.load.image('einstein', 'assets/pics/ra-einstein.png');
+    this.load.image('image0', 'assets/pics/ra-einstein.png');
+    this.load.image('image1', 'assets/sprites/mushroom2.png');
 }
 
 function create ()
 {
-    image = this.add.tileSprite(400, 300, 'einstein');
+    image0 = this.add.tileSprite(400, 300, 500, 500, 'image0');
+    image1 = this.add.tileSprite(400, 300, 150, 150, 'image1');
 }
 
 function update ()
 {
-    image.rotation += 0.01;
+    image0.tilePositionX = Math.cos(iter) * 400;
+    image0.tilePositionY = Math.sin(iter) * 400;
+    image1.tilePositionX = Math.cos(-iter) * 400;
+    image1.tilePositionY = Math.sin(-iter) * 400;
+    iter += 0.01;
 }

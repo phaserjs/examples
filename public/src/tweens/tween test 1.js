45c0188ec3b03a7c918c1ce3caf5ae1b85ae87ff
Richard Davey <rdavey@gmail.com>
2017-05-10 04:30:21 +0100

    Start of the tween examples.

---
+++ public/src/tweens/tween test 1.js
@@ -0,0 +1,36 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('block', 'assets/sprites/block.png');
+}
+
+function create ()
+{
+    var image = this.add.image(100, 300, 'block');
+
+    var tween = this.tweens.add({
+        targets: image,
+        props: {
+            x: 600
+        },
+        duration: 2,
+        ease: 'Power1'
+    });
+
+    console.log(tween);
+    console.log(tween.props);
+
+}

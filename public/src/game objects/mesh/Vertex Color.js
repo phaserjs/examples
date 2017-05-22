33cef5f260619d9c94d7243b772a3db532a42e50
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-05-22 15:37:12 -0400

    Vertex Color example

---
+++ public/src/game objects/mesh/Vertex Color.js
@@ -0,0 +1,60 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+var mesh0;
+var time = 0;
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('image0', 'assets/pics/trsipic1-lazur.jpg');
+}
+
+function create ()
+{
+    mesh0 = this.make.mesh({
+        key: 'image0',
+        x: 400,
+        y: 250,
+        vertices: [
+        /*  X   |   Y  */
+        /* ----------- */
+            -150, -150,
+            -300, 150,
+            300, 150,
+            150, -150
+        ],
+        uv: [
+        /*  U   |   V  */
+        /* ----------- */
+            0,      0,
+            0,      1,
+            1,      1,
+            1,      0
+        ],
+        indices: [0, 1, 2, 0, 2, 3],
+        colors: [0x000000, 0xFFFFFF, 0xFFFFFF, 0x000000]
+    });
+}
+
+function update ()
+{   
+    var factorX = 20 * 0.1;
+    var factorY = 5 * 0.1;
+
+    mesh0.vertices[2] += Math.cos(time) * factorX;
+    mesh0.vertices[3] += Math.sin(time) * factorY;
+    mesh0.vertices[4] += Math.cos(time) * factorX;
+    mesh0.vertices[5] += Math.sin(time) * factorY;
+
+    time += 0.01;
+}

cdc6aac81f326c44c294103b6be711bdde8e02df
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-06-01 18:43:17 -0400

    Tilemap assets and Tilemap example updated

--- public/src/game objects/tilemap/static/Tilemap Example.js
+++ public/src/game objects/tilemap/static/Tilemap Example.js
@@ -12,25 +12,38 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var time = 0;
 
 function preload ()
 {
-    this.load.image('image', 'assets/sprites/balls.png');
+    this.load.image('image', 'assets/tilemaps/tiles/catastrophi_tiles_16.png');
+    this.load.text('map', 'assets/tilemaps/csv/catastrophi_level3.csv');
 }
 
 function create ()
 {
-    var tilemap = this.add.staticTilemap([
-            1, 1, 1, 1, 1, 1, 1,
-            1, 0, 0, 0, 0, 0, 1,
-            1, 0, 0, 0, 1, 0, 1,
-            1, 0, 1, 0, 1, 0, 1,
-            1, 0, 1, 0, 0, 0, 1,
-            1, 0, 0, 0, 0, 0, 1,
-            1, 1, 1, 1, 1, 1, 1
-        ], 0, 0, 17, 17, 7, 7, 'image');
+    var rows = game.cache.text.get('map').split('\n');
+    var mapWidth = rows[0].split(',').length;
+    var mapHeight = rows.length;
+    var tileSize = 16;
+    var mapData = [];
+
+    for (var i = 0; i < mapHeight; ++i)
+    {
+        var row = rows[i].split(',').map(function (a) { return parseInt(a);});
+        for (var j = 0; j < mapWidth; ++j)
+        {
+            var id = row[j];
+            mapData.push(id);
+        }
+    }
+    var tilemap = this.add.staticTilemap(mapData, 0, 0, tileSize, tileSize, mapWidth, mapHeight, 'image');
 }
 
 function update ()
 {
+    this.cameras.main.scrollX = (200 + Math.cos(time) * 200)|0;
+    this.cameras.main.scrollY = (500 + Math.sin(time) * 500)|0;
+
+    time += 0.01;
 }

584775e4f040b324068488cd50a88eb8325282b1
photonstorm <rich@photonstorm.com>
2017-06-19 15:58:07 +0100

    Updated game parts

---
+++ public/src/games/snake/part1.js
@@ -0,0 +1,38 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 640,
+    height: 480,
+    backgroundColor: '#bfcc00',
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var snake;
+var food;
+var cursors;
+
+//  Direction consts
+var UP = 0;
+var DOWN = 1;
+var LEFT = 2;
+var RIGHT = 3;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('food', 'assets/games/snake/food.png');
+    this.load.image('body', 'assets/games/snake/body.png');
+}
+
+function create ()
+{
+}
+
+function update (time, delta)
+{
+}

4c1aa4d114feb3bf12896298483efbbc9cbdd0cd
photonstorm <rich@photonstorm.com>
2017-06-19 14:06:46 +0100

    Camera shake example

---
+++ public/src/camera/shake.js
@@ -0,0 +1,44 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    width: 800,
+    height: 600
+};
+
+var shakeTime = 0;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('pic', 'assets/pics/a-new-link-to-the-past-by-ptimm.jpg');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'pic');
+
+    this.input.events.on('MOUSE_DOWN_EVENT', function (event) {
+
+        console.log('shake');
+
+        shakeTime = 1000;
+
+    });
+
+}
+
+function update (time, delta)
+{
+    if (shakeTime > 0)
+    {
+        shakeTime -= delta;
+
+        this.cameras.main.shake(1000);
+    }
+}

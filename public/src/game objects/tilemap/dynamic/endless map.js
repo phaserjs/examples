cc934ed090afdecec387fe999509344532372e86
Richard Davey <rdavey@gmail.com>
2017-06-26 22:44:27 +0100

    Created an endless tilemap example

---
+++ public/src/game objects/tilemap/dynamic/endless map.js
@@ -0,0 +1,98 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var map;
+var text;
+var sx = 0;
+var mapWidth = 51;
+var mapHeight = 37
+var distance = 0;
+var tiles = [ 7, 7, 7, 6, 6, 6, 0, 0, 0, 1, 1, 2, 3, 4, 5 ];
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('tiles', 'assets/tilemaps/tiles/muddy-ground.png');
+    this.load.bitmapFont('nokia16', 'assets/fonts/bitmap/nokia16.png', 'assets/fonts/bitmap/nokia16.xml');
+}
+
+function create ()
+{
+    var mapData = [];
+
+    for (var y = 0; y < mapHeight; y++)
+    {
+        for (var x = 0; x < mapWidth; x++)
+        {
+            //  Scatter the tiles so we get more mud and less stones
+            mapData.push(Phaser.Math.RND.weightedPick(tiles));
+        }
+    }
+
+    var mapConfig = {
+        map: {
+            data: mapData,
+            width: mapWidth,
+            height: mapHeight,
+        },
+        tile: {
+            width: 16,
+            height: 16,
+            texture: 'tiles'
+        }
+    };
+
+    map = this.make.tilemap(mapConfig);
+
+    this.cameras.main.setBounds(0, 0, mapWidth * 32, mapHeight * 32);
+
+    text = this.add.bitmapText(0, 0, 'nokia16').setScrollFactor(0);
+}
+
+function update (time, delta)
+{
+    //  Any speed as long as it's a multiple of 16
+    sx += 4;
+
+    distance += sx;
+
+    text.setText("Distance: " + distance + 'px');
+
+    if (sx === 16)
+    {
+        //  Reset and create new strip
+
+        var tile;
+        var prev;
+
+        for (var y = 0; y < mapHeight; y++)
+        {
+            for (var x = 1; x < mapWidth; x++)
+            {
+                tile = map.getTileAt(x, y);
+                prev = map.getTileAt(x - 1, y);
+
+                prev.setId(tile.id);
+
+                if (x === mapWidth - 1)
+                {
+                    tile.setId(Phaser.Math.RND.weightedPick(tiles));
+                }
+            }
+        }
+
+        sx = 0;
+    }
+
+    this.cameras.main.scrollX = sx;
+}

165b1d876ab5ddf26d1557f9cb30bdba4e4d8f62
Richard Davey <rdavey@gmail.com>
2017-07-12 12:22:37 +0100

    New input tests

---
+++ public/src/game objects/group/random frame.js
@@ -0,0 +1,36 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    state: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.spritesheet('bobs', 'assets/sprites/bobs-by-cleathley.png', { frameWidth: 32, frameHeight: 32 });
+}
+
+function create ()
+{
+    //  This will create a Group with 400 children.
+    //  Each child will use the 'bobs' texture and a random frame number between 0 and 399 (inclusive)
+    //  Change 'randomFrame' to false to see the difference it makes
+
+    var group = this.make.group({
+        key: 'bobs',
+        frame: Phaser.Utils.Array.NumberArray(0, 399),
+        randomFrame: true,
+        gridAlign: {
+            width: 25,
+            height: 25,
+            cellWidth: 32,
+            cellHeight: 32
+        }
+    });
+}
---
+++ public/src/input/mouse/mass sprite test.js
@@ -0,0 +1,68 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    state: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var mouse = {x: 0, y: 0};
+var highlighted;
+var group;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.spritesheet('bobs', 'assets/sprites/bobs-by-cleathley.png', { frameWidth: 32, frameHeight: 32 });
+}
+
+function create ()
+{
+    //  Create a little 32x32 texture to use to show where the mouse is
+    var graphics = this.make.graphics({ x: 0, y: 0, add: false, fillStyle: { color: 0xff00ff, alpha: 1 } });
+
+    graphics.fillRect(0, 0, 32, 32);
+
+    graphics.generateTexture('block', 32, 32);
+
+    highlighted = this.add.image(16, 16, 'block');
+
+    //  Create 400 sprites aligned in a grid
+    group = this.make.group({
+        key: 'bobs',
+        frame: Phaser.Utils.Array.NumberArray(0, 399),
+        randomFrame: true,
+        gridAlign: {
+            width: 25,
+            height: 25,
+            cellWidth: 32,
+            cellHeight: 32
+        }
+    });
+
+    //  Track movement
+    this.input.events.on('MOUSE_MOVE_EVENT', function (event) {
+
+        mouse.x = event.x;
+        mouse.y = event.y;
+
+    });
+}
+
+function update ()
+{
+    var objects = this.input.pointScreenToWorldHitTest(group.children.entries, mouse.x, mouse.y, this.cameras.main);
+
+    if (objects && objects.length > 0)
+    {
+        var x = objects[0].x;
+        var y = objects[0].y;
+
+        highlighted.setPosition(x, y);
+    }
+}

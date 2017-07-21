93f45454346751631e0ecadc4eb8fd36ee53336a
Richard Davey <rdavey@gmail.com>
2017-07-21 04:12:03 +0100

    Updated examples

---
+++ public/src/input/mouse/over and out events.js
@@ -0,0 +1,38 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('eye', 'assets/pics/lance-overdose-loader-eye.png');
+}
+
+function create ()
+{
+    window.scene = this;
+
+    var sprite = this.add.sprite(400, 300, 'eye').setInteractive();
+
+    //  Events
+
+    this.input.events.on('POINTER_OVER_EVENT', function (event) {
+
+        event.gameObject.setTint(0xff0000);
+
+    });
+
+    this.input.events.on('POINTER_OUT_EVENT', function (event) {
+
+        event.gameObject.clearTint();
+
+    });
+}

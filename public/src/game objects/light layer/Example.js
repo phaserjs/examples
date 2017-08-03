ab2ba403a42e2adccf4c2fc24785a0b0b4384944
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-08-03 16:03:11 -0400

    Light Layer example update

--- public/src/game objects/light layer/Example.js
+++ public/src/game objects/light layer/Example.js
@@ -16,11 +16,16 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('mainImg', 'assets/sprites/phaser2.png');
+    this.load.image('normImg', 'assets/normal-maps/phaser2_n.png');
 }
 
 function create ()
 {
     var lights = this.add.lightLayer(0, 0, 800, 600);
+    var logo = this.add.image(400, 500, 'mainImg');
+
+    lights.addSprite(logo, this.textures.get('normImg'));
 }
 
 function update ()

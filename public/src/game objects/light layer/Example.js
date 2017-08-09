e3c6d577b90ad600b3475823487fc97378752c4d
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-08-09 19:37:15 -0400

    Updated example displaying light count

--- public/src/game objects/light layer/Example.js
+++ public/src/game objects/light layer/Example.js
@@ -6,11 +6,13 @@ var config = {
     parent: 'phaser-example',
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var image;
+var lights;
 var light = null;
 var game = new Phaser.Game(config);
 var colorIdx = 0;
@@ -28,19 +30,55 @@ function preload ()
     this.load.image('mainImg', 'assets/sprites/phaser2.png');
     this.load.image('normImg', 'assets/normal-maps/phaser2_n.png');
 }
+var camera;
+var hmove = false;
+var vmove = false;
+var moveX = 0.0;
+var moveY = 0.0;
+var text;
+function addLight(x, y)
+{
+    var color = colors[colorIdx];
+    colorIdx = (colorIdx + 1) % colors.length;
+    if (light)
+    {
+        light.scrollFactorX = 1;
+        light.scrollFactorY = 1;
+        light.x += camera.scrollX;
+        light.y += camera.scrollY;
+    }
+
+    light = lights.addLight(x, y, 0.1, color[0], color[1], color[2], 1.0, 1);
+    if (light != null)
+    {
+        light.scrollFactorX = 0;
+        light.scrollFactorY = 0;
+        if (text)
+            text.setText('Current Light Count: ' + lights.getLightCount());
+    }
+}
 function create ()
 {
-    var lights = this.add.lightLayer();
+    var space = 4096;
+    camera = this.cameras.main;
+    lights = this.add.lightLayer();
 
-    for (var i = 0; i < 200; ++i)
+    for (var i = 0; i < 1000; ++i)
     {
-        var logo = this.add.image(Math.random() * 800, Math.random() * 600, 'mainImg');
+        var logo = this.add.image(-(space / 2) + Math.random() * space, -(space / 2) + Math.random() * space, 'mainImg');
         logo.setScale(Math.random());
         lights.addSprite(logo, this.textures.get('normImg'));
     }
 
-    lights.setAmbientLightColor(0.0, 0.0, 0.1);
+    space *= 3;
 
+    for (var i = 0; i < ((lights.getMaxLights() / 2)|0) - 1; ++i)
+    {
+        addLight(-(space / 2) + Math.random() * space, -(space / 2) + Math.random() * space);
+    }
+
+    lights.setAmbientLightColor(0.0, 0.0, 0.1);
+    addLight(400, 300);
     this.input.events.on('POINTER_MOVE_EVENT', function (event) {
         if (light !== null)
         {
@@ -49,9 +87,73 @@ function create ()
         }
     });
     this.input.events.on('POINTER_DOWN_EVENT', function (event) {
-        var color = colors[colorIdx];
-        colorIdx = (colorIdx + 1) % colors.length;
-        light = lights.addLight(event.x, event.y, 0.1, color[0], color[1], color[2], 1.0, 0);
+        addLight(event.x, event.y);
     });
+
+    this.input.events.on('KEY_DOWN_EVENT', function (event) {
+        var speed = 5;
+        if (event.data.code === 'KeyA')
+        {
+            moveX = -speed;
+            hmove = true;
+        }
+        else if (event.data.code === 'KeyD')
+        {
+            moveX = speed;
+            hmove = true;
+        }
+
+        if (event.data.code === 'KeyW')
+        {
+            moveY = -speed;
+            vmove = true;
+        }
+        else if (event.data.code === 'KeyS')
+        {
+           moveY = speed;
+           vmove = true;
+        }
+    });
+
+    this.input.events.on('KEY_UP_EVENT', function (event) {
+        var speed = 5;
+        if (event.data.code === 'KeyA' || event.data.code === 'KeyD')
+        {
+            hmove = false;
+        }
+
+        if (event.data.code === 'KeyW' || event.data.code === 'KeyS')
+        {
+            vmove = false;
+        }
+    });
+    var text0;
+    if (lights.isDeferred())
+    {
+        text0 = this.add.text(0, 0, 'Use WASD to scroll camera and mouse button to add lights\nDeferred Renderer enabled\nMax light count is ' + lights.getMaxLights(), { font: '14px Courier', fill: '#00ff00' });
+    }
+    else
+    {
+        text0 = this.add.text(0, 0, 'Use WASD to scroll camera and mouse button to add lights\nForward Renderer enabled\nMax light count is ' + lights.getMaxLights(), { font: '14px Courier', fill: '#00ff00' });
+    }
+    text0.scrollFactorX = 0;
+    text0.scrollFactorY = 0;
+    text =  this.add.text(0, 50, 'Current Active Light Count: ' + lights.getLightCount(), { font: '14px Courier', fill: '#ffff00' });
+    text.scrollFactorX = 0;
+    text.scrollFactorY = 0;
+
 }
 
+function update()
+{
+
+    camera.scrollX += moveX;
+    camera.scrollY += moveY;
+
+    if (!hmove)
+        moveX *= 0.9;
+    
+    if (!vmove)
+        moveY *= 0.9;
+
+}

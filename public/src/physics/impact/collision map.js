05437635b03e7a734f0c182471b5b382edd46409
photonstorm <rich@photonstorm.com>
2017-08-18 17:03:45 +0100

    Updated map.

--- public/src/physics/impact/collision map.js
+++ public/src/physics/impact/collision map.js
@@ -1,8 +1,16 @@
 var config = {
     type: Phaser.WEBGL,
-    width: 864,
+    width: 800,
     height: 600,
     parent: 'phaser-example',
+    physics: {
+        default: 'impact',
+        impact: {
+            gravity: 800,
+            setMaxVelocityX: 400,
+            setMaxVelocityY: 800
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -10,25 +18,14 @@ var config = {
     }
 };
 
-var world;
-
-var bodyA;
-var bodyB;
-var bodyC;
-
-var imageA;
-var imageB;
-var imageC;
-
+var player;
 var cursors;
-var graphics;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
     this.load.image('clown', 'assets/sprites/clown.png');
-    // this.load.image('clown', 'assets/sprites/default.png');
     this.load.json('map', 'assets/tilemaps/maps/impact2.json');
     this.load.image('tiles', 'assets/tilemaps/tiles/slopes32.png');
 }
@@ -59,54 +56,36 @@ function create ()
 
     this.add.staticTilemap(mapData, 0, 0, impactData.tilesize, impactData.tilesize, impactData.width, impactData.height, 'tiles');
 
-    // imageA = this.add.image(64, 300, 'clown').setOrigin(0);
+    this.physics.world.setCollisionMap(impactData.tilesize, impactData.data);
 
-    world = new Phaser.Physics.Impact.World(800);
+    player = this.physics.add.image(64, 300, 'clown');
 
-    world.collisionMap = new Phaser.Physics.Impact.CollisionMap(impactData.tilesize, impactData.data);
+    player.setFriction(800, 0);
 
-    // bodyA = world.create(32, 32, imageA.width, imageA.height);
-    bodyA = world.create(32, 32, 40, 40);
-    bodyA.setMaxVelocity(400, 800);
-    bodyA.friction.x = 800;
-    bodyA.friction.y = 0;
-
-    bodyA.accelGround = 1200;
-    bodyA.accelAir = 600;
-    bodyA.jumpSpeed = 500;
-
-
-    graphics = this.add.graphics();
+    player.body.accelGround = 1200;
+    player.body.accelAir = 600;
+    player.body.jumpSpeed = 500;
 }
 
 function update (time, delta)
 {
-    var accel = bodyA.standing ? bodyA.accelGround : bodyA.accelAir;
+    var accel = player.body.standing ? player.body.accelGround : player.body.accelAir;
 
     if (cursors.left.isDown)
     {
-        bodyA.setAccelerationX(-accel);
+        player.setAccelerationX(-accel);
     }
     else if (cursors.right.isDown)
     {
-        bodyA.setAccelerationX(accel);
+        player.setAccelerationX(accel);
     }
     else
     {
-        bodyA.setAccelerationX(0);
+        player.setAccelerationX(0);
     }
 
-    if (cursors.up.isDown && bodyA.standing)
+    if (cursors.up.isDown && player.body.standing)
     {
-        bodyA.setVelocityY(-bodyA.jumpSpeed);
+        player.setVelocityY(-player.body.jumpSpeed);
     }
-
-    world.update(time, delta);
-
-    graphics.clear();
-    graphics.lineStyle(1, 0xffff00, 1);
-    graphics.strokeRect(bodyA.pos.x, bodyA.pos.y, bodyA.size.x, bodyA.size.y);
-
-    // imageA.setPosition(bodyA.pos.x, bodyA.pos.y);
-
 }

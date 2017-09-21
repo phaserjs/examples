7ea4b3ef441a0230e9d1c6ce0acd9a0b99a8621d
Richard Davey <rdavey@gmail.com>
2017-09-21 03:08:02 +0100

    More examples

---
+++ public/src/paths/ellipse curve.js
@@ -0,0 +1,62 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        create: create,
+        update: update
+    }
+};
+
+var path;
+var curve;
+var graphics;
+
+var game = new Phaser.Game(config);
+
+function create ()
+{
+    graphics = this.add.graphics();
+
+    path = { t: 0, vec: new Phaser.Math.Vector2() };
+
+    // aX, aY, xRadius, yRadius, aStartAngle, aEndAngle, aClockwise, aRotation
+    // curve = new Phaser.Curves.Ellipse(400, 300, 260, 260, 0, 180, false, 90);
+
+    //  With minimal arguments it creates a circle of radius 260 centered on 400x300:
+    curve = new Phaser.Curves.Ellipse(400, 300, 260);
+
+    this.tweens.add({
+        targets: path,
+        t: 1,
+        ease: 'Linear',
+        duration: 4000,
+        repeat: -1
+    });
+
+    //  By adjusting the radius you can create a spiral effect
+
+    /*
+    this.tweens.add({
+        targets: curve,
+        xRadius: 50,
+        yRadius: 50,
+        ease: 'Sine.easeInOut',
+        duration: 16000,
+        yoyo: true,
+        repeat: -1
+    });
+    */
+}
+
+function update ()
+{
+    graphics.clear();
+
+    curve.getPoint(path.t, path.vec);
+
+    graphics.fillStyle(0xff0000, 1);
+    graphics.fillCircle(path.vec.x, path.vec.y, 8);
+}
--- public/src/paths/get tangent vectors on cubic bezier curve.js
+++ public/src/paths/get tangent vectors on cubic bezier curve.js
@@ -22,19 +22,17 @@ function create ()
 
     var curve = new Phaser.Curves.CubicBezier(p0, p1, p2, p3);
 
+    var max = 16;
     var points = [];
     var tangents = [];
 
-    for (var c = 0; c <= 16; c++)
+    for (var c = 0; c <= max; c++)
     {
-        var t = curve.getUtoTmapping(c / 16);
+        var t = curve.getUtoTmapping(c / max);
 
         points.push(curve.getPoint(t));
         tangents.push(curve.getTangent(t));
     }
-
-    // console.log(points);
-    // console.log(tangents);
 
     var tempVec = new Phaser.Math.Vector2();
 
@@ -53,6 +51,7 @@ function create ()
         graphics.lineStyle(1, 0x00ff00, 1);
         graphics.lineBetween(p.x, p.y, tempVec.x, tempVec.y);
 
+        //  Draw the right-hand tangent vector
         tempVec.copy(tangents[i]).normalizeRightHand().scale(-32).add(p);
 
         graphics.lineStyle(1, 0xff00ff, 1);

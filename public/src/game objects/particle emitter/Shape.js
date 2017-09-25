62abfa2af8c543d8a9e6ff1bb61ce3686813253b
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-09-25 12:36:08 -0300

    particle shape example

---
+++ public/src/game objects/particle emitter/Shape.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('spark', 'assets/particles/blue.png');
+}
+
+function create ()
+{
+
+}
+
+function update ()
+{
+
+}

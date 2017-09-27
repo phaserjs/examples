4f66bcad1dc6fad91c01b136540bb371f9bd6c4c
Richard Davey <rdavey@gmail.com>
2017-09-27 22:14:10 +0100

    Tidying up path examples

---
+++ public/src/paths/followers/basic follower.js
@@ -0,0 +1,54 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('ball', 'assets/sprites/shinyball.png');
+}
+
+function create ()
+{
+    var points = [];
+
+    points.push(new Phaser.Math.Vector2(50, 400));
+    points.push(new Phaser.Math.Vector2(200, 200));
+    points.push(new Phaser.Math.Vector2(350, 300));
+    points.push(new Phaser.Math.Vector2(500, 500));
+    points.push(new Phaser.Math.Vector2(700, 400));
+
+    var curve = new Phaser.Curves.Spline(points);
+
+    var graphics = this.add.graphics();
+
+    graphics.lineStyle(1, 0xffffff, 1);
+
+    curve.draw(graphics, 64);
+
+    graphics.fillStyle(0x00ff00, 1);
+
+    for (var i = 0; i < points.length; i++)
+    {
+        graphics.fillCircle(points[i].x, points[i].y, 4);
+    }
+
+    var ball1 = this.add.follower(curve, 50, 350, 'ball');
+    var ball2 = this.add.follower(curve, 50, 400, 'ball');
+    var ball3 = this.add.follower(curve, 50, 450, 'ball');
+
+    //  Providing just a number sets the duration for following the path
+
+    ball1.start(4000);
+    ball2.start(4000);
+    ball3.start(4000);
+}

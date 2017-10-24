bc239c3beef83008a21a1c5b5c97f4001893b7d7
Richard Davey <rich@photonstorm.com>
2017-10-24 16:04:25 +0100

    Accel

---
+++ public/src/game objects/particle emitter/acceleration.js
@@ -0,0 +1,32 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.atlas('flares', 'assets/particles/flares.png', 'assets/particles/flares.json');
+}
+
+function create ()
+{
+    var particles = this.add.particles('flares');
+
+    particles.createEmitter({
+        frame: 'green',
+        x: 0,
+        y: 300,
+        lifespan: 4000,
+        speedX: { start: 0, end: 600 },
+        scale: 0.4
+    });
+}

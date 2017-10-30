53edf2a4f7eb3aeae7ff864473ba1115ae8e6c53
Richard Davey <rich@photonstorm.com>
2017-10-30 16:01:16 +0000

    Better :)

--- public/src/game objects/particle emitter/edge zone.js
+++ public/src/game objects/particle emitter/edge zone.js
@@ -19,22 +19,36 @@ function preload ()
 
 function create ()
 {
-    var shape1 = new Phaser.Geom.Circle(0, 0, 100);
+    var shape1 = new Phaser.Geom.Circle(0, 0, 160);
     var shape2 = new Phaser.Geom.Ellipse(0, 0, 500, 150);
     var shape3 = new Phaser.Geom.Rectangle(-150, -150, 300, 300);
     var shape4 = new Phaser.Geom.Line(-150, -150, 150, 150);
-    var shape5 = new Phaser.Geom.Triangle(0, -200, 200, 200, -200, 200);
+    var shape5 = new Phaser.Geom.Triangle.BuildEquilateral(0, -140, 300);
+    var shapes = [ shape1, shape2, shape3, shape4, shape5 ];
+
+    var i = 0;
 
     var particles = this.add.particles('flares');
 
-    particles.createEmitter({
-        frame: 'blue',
-        x: 400, y: 300,
-        speed: 0,
-        lifespan: 1000,
-        quantity: 1,
-        scale: { start: 0.4, end: 0 },
+    var emitter = particles.createEmitter({
+        frame: { frames: [ 'red', 'green', 'blue' ], cycle: true },
+        x: 400,
+        y: 300,
+        scale: { start: 0.5, end: 0 },
         blendMode: 'ADD',
-        emitZone: { type: 'edge', source: shape2, quantity: 48, yoyo: false }
+        emitZone: { type: 'edge', source: shape1, quantity: 48, yoyo: false }
+    });
+
+    this.input.events.on('POINTER_DOWN_EVENT', function (event) {
+
+        i++;
+
+        if (i === shapes.length)
+        {
+            i = 0;
+        }
+
+        emitter.setEmitZone({ type: 'edge', source: shapes[i], quantity: 48, yoyo: false });
+
     });
 }

461e07c85f4ab3b95acad73b026d2705f62216a0
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-11-17 18:00:25 -0300

    Masking Examples update

---
+++ public/src/display/masks/BitmapMask Masked EffectLayer.js
@@ -0,0 +1,109 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var effects = [];
+var currentEffect = null;
+var effectIndex = 0;
+var time = 0;
+var mouse = {x: 0, y: 0};
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('myImage', 'assets/sprites/phaser1.png');
+    this.load.glsl('shader0', 'assets/shaders/shader0.frag');
+    this.load.glsl('shader1', 'assets/shaders/shader1.frag');
+    this.load.glsl('shader2', 'assets/shaders/shader2.frag');
+    this.load.glsl('shader3', 'assets/shaders/shader3.frag');
+}
+
+function create ()
+{
+
+    var graphics = this.make.graphics();
+    var mask = new Phaser.Display.Masks.BitmapMask(this, graphics);
+
+    graphics.fillCircle(0, 0, 100);
+
+
+    for (var i = 0; i < 5; ++i)
+    {
+        // We create our game object as usual
+        var image = this.add.image(Math.random() * 800, Math.random() * 600, 'myImage');
+    }
+
+    var effect0 = this.add.effectLayer(0, 0, 800, 600, 'effect0', this.cache.shader.get('shader0'));
+    var effect1 = this.add.effectLayer(0, 0, 800, 600, 'effect1', this.cache.shader.get('shader1'));
+    var effect2 = this.add.effectLayer(0, 0, 800, 600, 'effect2', this.cache.shader.get('shader2'));
+    var effect3 = this.add.effectLayer(0, 0, 800, 600, 'effect3', this.cache.shader.get('shader3'));
+    
+
+    for (var i = 0; i < 5; ++i)
+    {
+        // We create our game object as usual
+        var image = this.add.image(Math.random() * 800, Math.random() * 600, 'myImage');
+    }
+
+    effect0.visible = true;
+    effect1.visible = false;
+    effect2.visible = false;
+    effect3.visible = false;
+
+    effect0.mask = mask;
+    effect1.mask = mask;
+    effect2.mask = mask;
+    effect3.mask = mask;
+
+    effect0.setFloat2('resolution', 800, 600);
+    effect1.setFloat2('resolution', 800, 600);
+    effect2.setFloat2('resolution', 800, 600);
+    effect3.setFloat2('resolution', 800, 600);
+
+    this.input.events.on('POINTER_MOVE_EVENT', function (event) {
+
+        mouse.x = 800 / event.x;
+        mouse.y = 600 / event.y;
+
+        graphics.x = event.x;
+        graphics.y = event.y;
+
+    });
+
+    this.input.events.on('POINTER_DOWN_EVENT', function (event) {
+
+        if (currentEffect)
+        {
+            currentEffect.visible = false;
+        }
+        
+        effectIndex = (effectIndex + 1) % effects.length;
+        currentEffect = effects[effectIndex];
+        currentEffect.visible = true;
+
+    });
+
+    currentEffect = effect0;
+
+    effects.push(effect0, effect1, effect2, effect3);
+}
+
+function update()
+{
+    if (currentEffect)
+    {
+        currentEffect.setFloat('time', time);
+        currentEffect.setFloat2('mouse', mouse.x, mouse.y);
+    }
+
+    time += 0.01;
+}
---
+++ public/src/display/masks/BitmapMask RenderPass.js
@@ -0,0 +1,151 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var image0;
+var image1;
+var image2;
+var image3;
+var images = [];
+var renderPassNormal;
+var renderPassLigths;
+var iter = 0;
+var lightPosition = {x: 0, y: 0, z: 0.08};
+var game = new Phaser.Game(config);
+
+var normalGenerator = [
+    'precision mediump float;',
+    'uniform sampler2D sampler;',
+    'varying vec2 v_tex_coord;',
+    'varying vec3 v_color;',
+    'varying float v_alpha;',
+    'void main(void) {',
+    '   vec4 color = texture2D(sampler, v_tex_coord);',
+    '   gl_FragColor = vec4(v_tex_coord.xy, 1.0, 1.0) * color.a;',
+    '}'
+].join('\n');
+
+var lightShader = [
+    'precision mediump float;',
+    'varying vec2 v_tex_coord;',
+    'varying vec3 v_color;',
+    'varying float v_alpha;',
+    'uniform sampler2D sampler;',
+    'uniform sampler2D u_normal_tex;',
+    'uniform vec3 u_light_pos;',
+    'uniform vec4 u_light_color;',
+    'uniform vec4 u_ambient_color;',
+    'uniform vec4 u_falloff;',
+    'void main () {',
+    '   vec2 uv = vec2(gl_FragCoord.x / 800.0, gl_FragCoord.y / 600.0);',
+    '   vec4 color = texture2D(sampler, v_tex_coord);',
+    '   vec4 normal_map = texture2D(u_normal_tex, uv);',
+    '   vec3 light_dir = vec3(u_light_pos.xy - (gl_FragCoord.xy / vec2(800.0, 600.0)), u_light_pos.z);',
+    '   float D = length(light_dir);',
+    '   vec3 N = normalize(vec3(normal_map.rgb * 2.0 - 1.0));',
+    '   vec3 L = normalize(light_dir);',
+    '   vec3 diffuse = (u_light_color.rgb * u_light_color.a ) * max(dot(N, L), 0.0);',
+    '   vec3 ambient = (u_ambient_color.rgb * u_ambient_color.a);',
+    '   float attenuation = 1.0 / (u_falloff.x + (u_falloff.y * D) + (u_falloff.z * D * D* D * D* D * D* D * D* D * D));',
+    '   vec3 intensity = ambient + diffuse * attenuation;',
+    '   vec3 final_color = color.rgb * intensity;',
+    '   gl_FragColor = vec4(final_color, 1.0) * color.a;',
+    '}'
+].join('\n');
+
+function preload ()
+{
+    this.load.image('einstein', 'assets/pics/ra-einstein.png');
+    this.load.image('mushroom', 'assets/sprites/mushroom2.png');
+    this.load.image('phaser', 'assets/sprites/phaser1.png');
+    this.load.image('phaser2', 'assets/sprites/phaser2.png');
+}
+
+function create ()
+{
+    var keys = ['mushroom', 'phaser'];
+    renderPassNormal = this.make.renderPass({add: false, x: 0, y: 0, width: 800, height: 600, shaderName: 'normalGenerator', fragmentShader: normalGenerator});
+    renderPassLigths = this.add.renderPass(0, 0, 800, 600, 'lights', lightShader);
+
+    for (var i = 0; i < 100; i++)
+    {
+        var image = this.make.image({x: Math.random() * 800, y: Math.random() * 600, key: keys[Math.floor(Math.random() * 2)], add: false});
+        images.push(image);
+    }
+    
+    renderPassLigths.setFloat4('u_light_color', 1.0, 0.5, 0.3, 1.0);
+    renderPassLigths.setFloat4('u_ambient_color', 0.2, 0.2, 0.2, 1.0);
+    renderPassLigths.setFloat4('u_falloff', 0.2, 3.0, 1.0, 1.0);
+
+    renderPassLigths.setRenderTextureAt(renderPassNormal.renderTexture, 'u_normal_tex', 1);
+
+    var phaser2 = this.make.sprite({
+        x: game.config.width / 2,
+        y: game.config.height / 2,
+        key: 'phaser2',
+        add: false
+    });
+
+    renderPassLigths.mask = new Phaser.Display.Masks.BitmapMask(this, phaser2);
+
+    this.input.events.on('POINTER_MOVE_EVENT', function (event) {
+
+        lightPosition.x = event.x;
+        lightPosition.y = event.y;
+
+        phaser2.x = event.x;
+        phaser2.y = event.y;
+
+    });
+
+   this.input.events.on('POINTER_DOWN_EVENT', function (event) {
+
+        renderPassLigths.setFloat4('u_light_color', Math.random(), Math.random(), Math.random(), 1.0);
+
+        lightPosition.z = Math.random() * 0.1;
+
+        renderPassLigths.setFloat4('u_falloff', Math.random(), 2.0 + Math.random() * 3.0, 1.0, 1.0);
+
+    });
+}
+
+function update ()
+{
+    var imageCount = images.length;
+
+    /* Update */
+    for (var i = 0; i < imageCount; ++i)
+    {
+        var image = images[i];
+
+        image.x += Math.cos(iter + i) * 3;
+        image.y += Math.sin(iter + i) * 3;
+    }
+
+    iter += 0.1;
+
+    /* Render */ 
+    renderPassNormal.clearColorBuffer(0, 0, 0, 0);
+
+    for (var i = 0; i < imageCount; ++i)
+    {
+        renderPassNormal.render(images[i], this.cameras.main);
+    }
+
+    renderPassLigths.clearColorBuffer(0, 0, 0, 0);
+    renderPassLigths.setFloat3('u_light_pos', lightPosition.x / 800, -lightPosition.y / 600 + 1, lightPosition.z);
+
+    for (var i = 0; i < imageCount; ++i)
+    {
+        renderPassLigths.render(images[i], this.cameras.main);
+    }
+}

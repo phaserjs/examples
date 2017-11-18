b960406bc31dfc2ccdc88616338e5222d599a735
Michael Hadley <michaelwesthadley@gmail.com>
2017-11-18 15:54:48 -0600

    Porting paint tiles demo to dynamic tilemap api

---
+++ public/src/game objects/tilemap/dynamic/paint-tiles.js
@@ -0,0 +1,91 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var controls;
+var marker;
+var map;
+var shiftKey;
+var selectedTile;
+
+function preload ()
+{
+    this.load.image('tiles', 'assets/tilemaps/tiles/tmw_desert_spacing.png');
+    this.load.tilemapJSON('map', 'assets/tilemaps/maps/desert.json');
+}
+
+function create ()
+{
+    map = this.make.tilemap({ key: 'map' });
+
+    // The first parameter is the name of the tileset in Tiled and the second parameter is the key
+    // of the tileset image used when loading the file in preload.
+    var tiles = map.addTilesetImage('Desert', 'tiles');
+
+    // You can load a layer from the map using the layer name from Tiled ('Ground' in this case), or
+    // by using the layer index. Since we are going to be manipulating the map, this needs to be a
+    // dynamic tilemap layer, not a static one.
+    map.createDynamicLayer('Ground', tiles, 0, 0);
+
+    selectedTile = map.getTileAt(2, 3);
+
+    marker = this.add.graphics();
+    marker.lineStyle(2, 0x000000, 1);
+    marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    var cursors = this.input.keyboard.createCursorKeys();
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    };
+    controls = this.cameras.addKeyControl(controlConfig);
+
+    shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+
+    var help = this.add.text(16, 16, ' Left-click to paint. Shift + Left-click to select tile. Arrows to scroll. ', {
+        font: '20px Arial',
+        backgroundColor: '#000000',
+        fill: '#ffffff'
+    });
+    help.setScrollFactor(0);
+}
+
+function update (time, delta)
+{
+    controls.update(delta);
+
+    var pointerTileX = map.worldToTileX(this.input.x);
+    var pointerTileY = map.worldToTileY(this.input.y);
+
+    marker.x = pointerTileX * map.tileWidth;
+    marker.y = pointerTileY * map.tileHeight;
+
+    if (this.input.manager.activePointer.isDown)
+    {
+        if (shiftKey.isDown)
+        {
+            selectedTile = map.getTileAt(pointerTileX, pointerTileY);
+        }
+        else
+        {
+            map.putTileAt(selectedTile, pointerTileX, pointerTileY);
+        }
+    }
+}

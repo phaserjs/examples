701f7410647c833c65b1c534c316b948e03e0095
Michael Hadley <michaelwesthadley@gmail.com>
2017-11-18 15:53:44 -0600

    Adding 3 three new static tilemap examples - creating from csv, tiled, array

---
+++ public/src/game objects/tilemap/static/create-from-array.js
@@ -0,0 +1,42 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 11 * 16, // Number of tiles * size of the tile
+    height: 10 * 16,
+    zoom: 4,
+    parent: 'phaser-example',
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('mario-tiles', 'assets/tilemaps/tiles/super-mario.png');
+}
+
+function create ()
+{
+    // Load a map from a 2D array of tile indices
+    const level = [
+      [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
+      [  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0, 14, 13, 14,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0, 14, 14, 14, 14, 14,  0,  0,  0, 15 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0,  0, 15, 15 ],
+      [ 35, 36, 37,  0,  0,  0,  0,  0, 15, 15, 15 ],
+      [ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+    ]
+
+    // When loading from an array, make sure to specify the tileWidth and tileHeight
+    const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
+    const tiles = map.addTilesetImage('mario-tiles');
+    const layer = map.createStaticLayer(0, tiles, 0, 0);
+}
---
+++ public/src/game objects/tilemap/static/csv-map.js
@@ -0,0 +1,54 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var controls;
+
+function preload ()
+{
+    this.load.image('tiles', 'assets/tilemaps/tiles/catastrophi_tiles_16.png');
+    this.load.tilemapCSV('map', 'assets/tilemaps/csv/catastrophi_level2.csv');
+}
+
+function create ()
+{
+    // When loading a CSV map, make sure to specify the tileWidth and tileHeight
+    var map = this.make.tilemap({ key: 'map', tileWidth: 16, tileHeight: 16 });
+    var tileset = map.addTilesetImage('tiles');
+    var layer = map.createStaticLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    var cursors = this.input.keyboard.createCursorKeys();
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    };
+    controls = this.cameras.addKeyControl(controlConfig);
+
+    var help = this.add.text(16, 16, 'Arrow keys to scroll', {
+        font: '20px Arial',
+        fill: '#ffffff'
+    });
+    help.setScrollFactor(0);
+}
+
+function update (time, delta)
+{
+    controls.update(delta);
+}

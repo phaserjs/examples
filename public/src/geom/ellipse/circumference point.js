594725bb4e8ac8cb196822cc3058ba5f6fbf6e53
Richard Davey <rdavey@gmail.com>
2017-12-02 02:04:58 +0000

    Nicer colors

--- public/src/geom/ellipse/circumference point.js
+++ public/src/geom/ellipse/circumference point.js
@@ -9,12 +9,14 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
-
 var graphics;
 var ellipse;
 var point;
 var a = 0;
+var mm;
+var hsv;
+
+var game = new Phaser.Game(config);
 
 function create ()
 {
@@ -22,6 +24,9 @@ function create ()
 
     ellipse = new Phaser.Geom.Ellipse(400, 300, 600, 300);
     point = new Phaser.Geom.Rectangle(0, 0, 8, 8);
+
+    hsv = Phaser.Display.Color.HSVColorWheel();
+    mm = new Phaser.Math.MinMax2(0, 359, 359);
 }
 
 function update ()
@@ -34,17 +39,21 @@ function update ()
         graphics.clear();
 
         //  Change the ellipse
-        ellipse.width = Phaser.Math.Between(100, 700);
-        ellipse.height = Phaser.Math.Between(100, 500);
+        ellipse.width = Phaser.Math.Between(0, 800);
+        ellipse.height = Phaser.Math.Between(0, 600);
     }
 
     Phaser.Geom.Ellipse.CircumferencePoint(ellipse, a, point);
+
+    var c = hsv[mm.getNext()].color;
+
+    graphics.fillStyle(c);
 
     graphics.fillRect(point.x - 4, point.y - 4, point.width, point.height);
 
     //  Draw a line from the center of the ellipse to the point on the perimeter
 
-    graphics.lineStyle(2, 0xffffff);
+    graphics.lineStyle(2, c);
     graphics.beginPath();
     graphics.moveTo(ellipse.x, ellipse.y);
     graphics.lineTo(point.x, point.y);

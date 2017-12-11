308bb7bd4cf8497531c1b20457907fd8f3fcd2ec
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-12-11 12:49:07 +0100

    Added nyancat animation to seek example

--- public/src/audio/Seek.js
+++ public/src/audio/Seek.js
@@ -5,7 +5,8 @@ var config = {
     height: 600,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     },
     pixelArt: true
 };
@@ -14,15 +15,25 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('bg', 'assets/animations/nyan/bg.png');
+
+    this.load.image('rainbow', 'assets/animations/nyan/rainbow.png');
+
+    this.load.spritesheet('cat', 'assets/animations/nyan/cat/cat.png', { frameWidth: 97, frameHeight: 59 });
+
     this.load.audio('CatAstroPhi', [
         'assets/audio/CatAstroPhi_shmup_normal.ogg',
         'assets/audio/CatAstroPhi_shmup_normal.mp3'
     ]);
 }
 
+var cat;
+var catAstroPhi;
+var rainbowMask;
+
 function create () {
 
-    var catAstroPhi = this.game.sound.add('CatAstroPhi');
+    catAstroPhi = this.game.sound.add('CatAstroPhi');
 
     var gui = new dat.GUI();
 
@@ -45,4 +56,40 @@ function create () {
 
     // catAstroPhi.play();
     // catAstroPhi.seek = 3.000;
+
+    var bg = this.add.image(400, 300, 'bg');
+
+    rainbowMask = this.make.graphics();
+
+    var rainbow = this.add.image(400, 300, 'rainbow');
+    rainbow.mask = new Phaser.Display.Masks.GeometryMask(this, rainbowMask);
+
+    this.anims.create({
+        key: 'cat',
+        frames: this.anims.generateFrameNumbers('cat', { start: 0, end: 6, first: 0 }),
+        frameRate: 15,
+        repeat: -1
+    });
+
+    cat = this.add.sprite(0, 300, 'cat');
+    cat.play('cat');
+}
+
+function update ()
+{
+    cat.x = cat.width/2 + (catAstroPhi.seek / catAstroPhi.duration) * (800 - cat.width);
+
+    rainbowMask.clear();
+    rainbowMask.fillRect(0, 0, cat.x - 15, 600);
+
+    if(!catAstroPhi.isPlaying && cat.anims.isPlaying)
+    {
+        cat.anims.pause();
+    }
+    else if (catAstroPhi.isPlaying && !cat.anims.isPlaying)
+    {
+        cat.anims.resume();
+    }
+
+    cat.anims.timeScale(catAstroPhi.totalRate);
 }

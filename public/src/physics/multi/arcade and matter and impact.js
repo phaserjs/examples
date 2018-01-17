7bc9ea5ed92475980c64e2c42dc6397797343c83
Richard Davey <rich@photonstorm.com>
2018-01-17 15:50:11 +0000

    3 at once!

--- public/src/physics/multi/arcade and matter and impact.js
+++ public/src/physics/multi/arcade and matter and impact.js
@@ -14,6 +14,18 @@ var config = {
             matter: {
                 debug: true,
                 gravity: { y: 0.5 }
+            },
+            impact: {
+                gravity: 100,
+                debug: true,
+                setBounds: {
+                    x: 100,
+                    y: 100,
+                    width: 600,
+                    height: 300,
+                    thickness: 32
+                },
+                maxVelocity: 500
             }
         }
     }
@@ -26,6 +38,7 @@ function preload ()
     this.load.image('fuji', 'assets/sprites/fuji.png');
     this.load.image('block', 'assets/sprites/block.png');
     this.load.image('platform', 'assets/sprites/platform.png');
+    this.load.image('gem', 'assets/sprites/gem.png');
 }
 
 function create ()
@@ -37,11 +50,11 @@ function create ()
 
     this.matter.add.image(400, 550, 'platform', null, { isStatic: true });
 
+    //  Impact Physics
+
+    this.impact.add.image(300, 300, 'gem').setActive().setVelocity(300, 200).setBounce(1);
+
     //  Arcade Physics:
 
-    var block = this.physics.add.image(400, 100, 'fuji');
-
-    block.setVelocity(100, 200);
-    block.setBounce(1, 1);
-    block.setCollideWorldBounds(true);
+    this.physics.add.image(400, 100, 'fuji').setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true);
 }

804010b03dcf5b066a04c58332b7533fd8bb0367
Antriel <Antriel@users.noreply.github.com>
2018-01-18 12:25:50 +0100

    Improved API usage and variable names for Line/ReflectAngle.

--- public/src/geom/line/reflect angle.js
+++ public/src/geom/line/reflect angle.js
@@ -11,47 +11,48 @@ var config = {
 
 var game = new Phaser.Game(config);
 var graphics;
-var line;
-var line2;
+var aimLine;
+var reflectingLine;
+var reflectedLine;
 var text;
 
 function create ()
 {
     graphics = this.add.graphics({ lineStyle: { width: 4, color: 0xaa00aa } });
 
-    line = new Phaser.Geom.Line(100, 100, 400, 300);
-    line2 = new Phaser.Geom.Line(250, 300, 550, 300);
+    aimLine = new Phaser.Geom.Line(100, 100, 400, 300);
+    reflectingLine = new Phaser.Geom.Line(250, 300, 550, 300);
+    reflectedLine = new Phaser.Geom.Line(250, 300, 550, 300);
 
     text = this.add.text(50, 50, '');
 
     this.input.on('pointermove', function (pointer) {
 
-        line.x1 = pointer.x;
-        line.y1 = pointer.y;
+        aimLine.x1 = pointer.x;
+        aimLine.y1 = pointer.y;
 
     });
 }
 
 function update ()
 {
-    Phaser.Geom.Line.Rotate(line2, 0.005);
+    Phaser.Geom.Line.Rotate(reflectingLine, 0.005);
 
-    var reflectAngle = Phaser.Geom.Line.ReflectAngle(line, line2);
+    var reflectAngle = Phaser.Geom.Line.ReflectAngle(aimLine, reflectingLine);
 
     text.setText("Reflect Angle: " + Phaser.Math.RadToDeg(reflectAngle));
 
     graphics.clear();
 
-    graphics.strokeLineShape(line);
+    graphics.strokeLineShape(aimLine);
 
     graphics.lineStyle(4, 0x0000aa);// blue
-    graphics.strokeLineShape(line2);
+    graphics.strokeLineShape(reflectingLine);
 
-    graphics.lineStyle(2, 0x00aa00);// green – reflected line
+    var length = Phaser.Geom.Line.Length(aimLine);
 
-    var sin = Math.sin(reflectAngle);
-    var cos = Math.cos(reflectAngle);
-    var length = Phaser.Geom.Line.Length(line);
+    Phaser.Geom.Line.SetToAngle(reflectedLine, 400, 300, reflectAngle, length);
 
-    graphics.lineBetween(400, 300, 400 + cos * length, 300 + sin * length);
+    graphics.lineStyle(2, 0x00aa00);// green
+    graphics.strokeLineShape(reflectedLine);
 }

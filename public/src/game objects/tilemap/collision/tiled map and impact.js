f2712cba3f656daa99a583e9607b0f2e4cf363c2
Michael Hadley <michaelwesthadley@gmail.com>
2018-01-25 08:33:57 -0600

    New tiled + impact demo using slope maps

--- public/src/game objects/tilemap/collision/tiled map and impact.js
+++ public/src/game objects/tilemap/collision/tiled map and impact.js
@@ -22,44 +22,42 @@ var cursors;
 
 function preload ()
 {
-    this.load.tilemapTiledJSON('map', 'assets/tilemaps/maps/slopes.json');
-    this.load.image('tiles', 'assets/tilemaps/tiles/slopes32mud.png');
+    this.load.tilemapTiledJSON('map', 'assets/tilemaps/maps/impact-tilemap.json');
+    this.load.image('kenney', 'assets/tilemaps/tiles/kenney.png');
     this.load.image('player', 'assets/sprites/phaser-dude.png');
 }
 
 function create ()
 {
     var map = this.make.tilemap({ key: 'map' });
-    var tileset = map.addTilesetImage('slopes32mud', 'tiles');
-    var backgroundLayer = map.createStaticLayer('Background Layer', tileset, 0, 0);
-    var layer = map.createStaticLayer('Ground Layer', tileset, 0, 0);
+    var tileset = map.addTilesetImage('kenney');
+    var layer = map.createStaticLayer(0, tileset, 0, 0);
 
-    // Construct a collision map - a 2D array that indicates what type of slope each tile has. This
-    // corresponds to the slope defs in Phaser.Impact. Check out slopes32mud.png to see a visual
-    // representation. E.g. "1" is a normal rectangle collision, "2" is a tile with 45 degree slope,
-    // "49" is a blank tile.
-    var collisionMap = [];
-    for (var y = 0; y < map.height; y++)
-    {
-        var row = [];
-        for (var x = 0; x < map.width; x++)
-        {
-            var tile = map.getTileAt(x, y);
-            if (!tile)
-            {
-                row.push(49); // No collider
-            }
-            else
-            {
-                row.push(tile.index);
-            }
-        }
-        collisionMap.push(row);
-    }
+    // Any tile with the collides property set to true (in Tiled) will be set to collide
+    layer.setCollisionByProperty({ collides: true });
 
-    this.impact.world.setCollisionMap(collisionMap, map.tileWidth);
+    // Impact collisions with the world work by using slope IDs. An ID of 0 is non-colliding, an ID
+    // of 1 is a solid rectangle, an ID of 2 is a 45 degree upward slope, etc. Check out this visual
+    // reference:
+    // https://github.com/photonstorm/phaser3-examples/blob/master/public/assets/tilemaps/tiles/slopes.png
 
-    player = this.impact.add.image(64, 300, 'player');
+    // We need to construct a collision map (of slope IDs) in order to register collisions with a
+    // tilemap layer. We can do this in one of two ways:
+
+    // Method 1. If we assign the slope IDs as tile properties in Tiled's tileset editor, we can
+    // load them by passing in the property name, e.g. impact-tilemap.json has a property on every
+    // tile called "slope":
+    this.impact.world.setCollisionMapFromTilemapLayer(layer, { slopeProperty: 'slope' });
+
+    // Method 2. If we don't have slopes defined in Tiled, we can manually map tile index to slope
+    // ID using an object:
+    var slopeMap = { 32: 1, 77: 1, 82: 1, 95: 24, 137: 2, 140: 24, 36: 2 };
+    this.impact.world.setCollisionMapFromTilemapLayer(layer, { slopeMap: slopeMap });
+
+    // Note: the collision map is static! If you remove/change the colliding tiles, it will not be
+    // updated.
+
+    player = this.impact.add.image(64, 200, 'player');
     player.setMaxVelocity(300, 400).setFriction(800, 0);
     player.body.accelGround = 1200;
     player.body.accelAir = 600;

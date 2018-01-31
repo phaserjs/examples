06d4c5df7eec5dab6cb699d1c55866df3afe052d
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-31 14:39:47 +0100

    Updated game destroy example to reuse audio context

--- public/src/game config/game destroy.js
+++ public/src/game config/game destroy.js
@@ -1,48 +1,8 @@
-var config2 = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'phaser-example',
-    scene: {
-        preload: preload2,
-        create: create2
-    }
-};
-
-function preload2 ()
-{
-    this.load.image('taikodrummaster', 'assets/pics/taikodrummaster.jpg');
-    this.load.image('sukasuka-chtholly', 'assets/pics/sukasuka-chtholly.png');
-}
-
-function create2 ()
-{
-    this.add.image(400, 300, 'taikodrummaster');
-
-    var chtholly = this.add.image(400, 500, 'sukasuka-chtholly');
-
-    this.tweens.add({
-        targets: chtholly,
-        x: 200,
-        ease: 'Sine.easeInOut',
-        duration: 2000,
-        yoyo: true,
-        repeat: -1
-    });
-
-    this.input.on('pointerdown', function () {
-    
-       this.sys.game.destroy(true);
-
-        document.addEventListener('mousedown', function newGame () {
-
-            game = new Phaser.Game(config);
-
-            document.removeEventListener('mousedown', newGame);
-    
-        });
-    
-    }, this);
+var audioContext;
+try {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+} catch (e) {
+    console.error(e);
 }
 
 var config = {
@@ -53,6 +13,9 @@ var config = {
     scene: {
         preload: preload,
         create: create
+    },
+    audio: {
+        context: audioContext
     }
 };
 
@@ -72,7 +35,8 @@ function create ()
 
     this.tweens.add({
         targets: chtholly,
-        y: 600,
+        y: Math.random() * 600,
+        x: Math.random() * 200,
         ease: 'Sine.easeInOut',
         duration: 2000,
         yoyo: true,
@@ -80,15 +44,15 @@ function create ()
     });
 
     this.input.on('pointerdown', function () {
-    
+
         this.sys.game.destroy(true);
 
         document.addEventListener('mousedown', function newGame () {
 
-            game = new Phaser.Game(config2);
+            game = new Phaser.Game(config);
 
             document.removeEventListener('mousedown', newGame);
-    
+
         });
 
     }, this);

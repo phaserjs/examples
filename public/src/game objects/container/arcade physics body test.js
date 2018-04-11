3a8296af3d5aadce1e7f09c1d9306c5292e5e496
Richard Davey <rich@photonstorm.com>
2018-04-11 13:37:56 +0100

    Physics demo

--- public/src/game objects/container/arcade physics body test.js
+++ public/src/game objects/container/arcade physics body test.js
@@ -13,9 +13,12 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
+
+var container;
 
 var game = new Phaser.Game(config);
 
@@ -26,13 +29,29 @@ function preload ()
 
 function create ()
 {
-    var image1 = this.add.image(0, 0, 'mushroom');
-    var image2 = this.add.image(-40, 60, 'mushroom');
-    var image3 = this.add.image(40, 60, 'mushroom');
+    var image1 = this.add.image(0, -30, 'mushroom');
+    var image2 = this.add.image(-40, 30, 'mushroom');
+    var image3 = this.add.image(40, 30, 'mushroom');
 
-    var container = this.add.container(400, 200, [ image1, image2, image3 ]);
+    container = this.add.container(400, 200, [ image1, image2, image3 ]);
+
+    //  A Container has a default size of 0x0, so we need to give it a size before enabling a physics
+    //  body or it'll be given the default body size of 64x64.
+    container.setSize(128, 64);
 
     this.physics.world.enable(container);
 
     container.body.setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true);
+}
+
+function update ()
+{
+    if (container.body.velocity.x < 0)
+    {
+        container.rotation -= 0.02;
+    }
+    else
+    {
+        container.rotation += 0.02;
+    }
 }

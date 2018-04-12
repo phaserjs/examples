ab4dd4949f2d3fb181b03db2c186106bea79ff5c
Richard Davey <rdavey@gmail.com>
2018-04-12 16:27:03 +0100

    More tests

--- public/src/game objects/container/camera transform test.js
+++ public/src/game objects/container/camera transform test.js
@@ -15,11 +15,17 @@ function preload ()
 {
     this.load.image('eye', 'assets/pics/lance-overdose-loader-eye.png');
     this.load.image('grid', 'assets/pics/debug-grid-1920x1920.png');
+    this.load.spritesheet('cursor', 'assets/sprites/centroid.png', { frameWidth: 16, frameHeight: 16 });
 }
 
 function create ()
 {
     var cam1 = this.cameras.main.setName('Camera 1');
+
+    // cam1.x = 100;
+    // cam1.y = 100;
+    // cam1.zoom = 0.8;
+    // cam1.rotation = 0.2;
 
     this.add.image(0, 0, 'grid').setOrigin(0).setAlpha(0.5);
 
@@ -29,17 +35,35 @@ function create ()
 
     container.add(sprite);
 
-    this.input.on('gameobjectover', function (pointer, gameObject) {
+    var text = this.add.text(10, 10, 'Click on sprite', { font: '16px Courier', fill: '#00ff00' });
 
-        gameObject.setTint(0xff0000);
+    var cursor1 = this.add.image(50, 50, 'cursor', 0);
+    var cursor2 = this.add.image(50, 50, 'cursor', 1);
 
-    });
+    this.input.on('pointerdown', function (pointer) {
 
-    this.input.on('gameobjectout', function (pointer, gameObject) {
+        var result = this.input.manager.debugHitTest(pointer.x, pointer.y, sprite, cam1);
 
-        gameObject.clearTint();
+        cursor1.setPosition(pointer.x, pointer.y);
+        cursor2.setPosition(result[1].x, result[1].y);
 
-    });
+        text.setText([
+            'down x: ' + pointer.x,
+            'down y: ' + pointer.y,
+            '',
+            'x: ' + result[0].tx,
+            'y: ' + result[0].ty,
+            'sx: ' + result[0].scaleX,
+            'sy: ' + result[0].scaleY,
+            'r: ' + Phaser.Math.RadToDeg(result[0].rotation),
+            '',
+            'px: ' + result[1].x,
+            'py: ' + result[1].y,
+            '',
+            'In: ' + result[2]
+        ]);
+
+    }, this);
 
     var gui = new dat.GUI();
 

1f918a6c447fb852d14c6a088eb81a6f760d8662
Richard Davey <rich@photonstorm.com>
2018-05-10 13:08:19 +0100

    New examples

---
+++ public/src/game config/custom canvas.js
@@ -0,0 +1,33 @@
+var myCustomCanvas = document.createElement('canvas');
+
+myCustomCanvas.id = 'myCustomCanvas';
+myCustomCanvas.style = 'border: 8px solid red';
+
+document.body.appendChild(myCustomCanvas);
+
+var myCustomContext = myCustomCanvas.getContext('2d');
+
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    canvas: myCustomCanvas,
+    context: myCustomContext,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('pic', 'assets/pics/baal-loader.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'pic');
+}

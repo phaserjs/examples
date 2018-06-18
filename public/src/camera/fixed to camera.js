b422b188470b8b43dfeae4410de461508c49b95f
DannyT <dan@moov2.com>
2018-06-18 21:06:06 +0100

    Added dat.GUI back in

--- public/src/camera/fixed to camera.js
+++ public/src/camera/fixed to camera.js
@@ -43,6 +43,23 @@ function create ()
     };
 
     controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+    
+    var cam = this.cameras.main;
+
+    gui = new dat.GUI();
+
+    var help = {
+        line1: 'Cursors to move'
+    }
+
+    var f1 = gui.addFolder('Camera');
+    f1.add(cam, 'x').listen();
+    f1.add(cam, 'y').listen();
+    f1.add(cam, 'scrollX').listen();
+    f1.add(cam, 'scrollY').listen();
+    f1.add(cam, 'rotation').min(0).step(0.01).listen();
+    f1.add(help, 'line1');
+    f1.open();
 }
 
 function update (time, delta)

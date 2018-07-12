8fe818b58bffa59510044be1c57c5321c1972f64
Richard Davey <rich@photonstorm.com>
2018-07-12 12:52:37 +0100

    Fixed search indexer

--- search/index-documents.js
+++ search/index-documents.js
@@ -1,159 +1,188 @@
 var fs = require('fs');
+var path = require('path');
 
-//toggles debug output.
+//  Toggles debug output.
 var debug = false;
-//list of directories containing files which should be excluded from the index.
+
+//  List of directories containing files which should be excluded from the index.
 var excludedDirectories = [];
-//files that were excluded due to being part of a bootable project
+
+//  Files that were excluded due to being part of a bootable project
 var excludedFiles = [];
-//the path to output the index file
-var indexFile           = 'public/documentIndex.json';
-//exclude files and folders beginning with `_` or `archived`
-var excludeDocuments    = /\/(?:archived|_)/;
-//includes files ending with `.json` or `.js`
-var includeDocuments    = /\.(?:js|json)$/;
-//it is bootable if the path ends in boot.json
+
+//  The path to output the index file
+var indexFile = 'public/documentIndex.json';
+
+//  Exclude files and folders beginning with `_` or `archived`
+var excludeDocuments = /\/(?:archived|_)/;
+
+//  Includes files ending with `.json` or `.js`
+var includeDocuments = /\.(?:js|json)$/;
+
+//  It is bootable if the path ends in boot.json
 var isBootableRegex = /boot\.json$/;
 
-function getContainingDirectory(filePath) {
-    var parts    = filePath.split('/');
+function getContainingDirectory (filePath)
+{
+    var parts = filePath.split(path.sep);
 
-    //remove last segment of path
+    //  Remove last segment of path
     parts.pop();
 
-    //join the rest of the path
+    //  Join the rest of the path
     return parts.join('/');
 }
 
-function isBootableProject(path){
+function isBootableProject (path)
+{
     return isBootableRegex.test(path);
 }
 
-function pathIsExcluded(path){
-    //we do want to include the actual bootable json in the search, so we dont exclude it
-    if(isBootableProject(path)){
-
+function pathIsExcluded (path)
+{
+    //  We do want to include the actual bootable json in the search, so we dont exclude it
+    if (isBootableProject(path))
+    {
         return false;
     }
 
     var containingDirectory = getContainingDirectory(path);
-    if(excludedDirectories.includes(containingDirectory)){
-        excludedFiles.push(path);
+
+    if (excludedDirectories.includes(containingDirectory))
+    {
+        excludedFiles.push({ entry: path, reason: 'excluded directory: ' + containingDirectory });
+
         return true;
     }
 
     return false;
 }
 
-function excludeIfBootableProject(path) {
-    if (isBootableProject(path)) {
+function excludeIfBootableProject (path)
+{
+    if (isBootableProject(path))
+    {
         var directory = getContainingDirectory(path);
 
         excludedDirectories.push(directory);
     }
 }
 
-function getDocuments(tree, documents) {
+function getDocuments (tree, documents)
+{
     var path = tree.path;
 
-    if (includeDocuments.test(path)) {
-        if (excludeDocuments.test(path)) {
-            excludedFiles.push(path);
+    if (includeDocuments.test(path))
+    {
+        if (excludeDocuments.test(path))
+        {
+            excludedFiles.push({ entry: path, reason: 'excluded path' });
         }
-        else {
+        else
+        {
             documents.push(path);
 
             excludeIfBootableProject(path);
         }
     }
-    else {
+    else
+    {
         excludedFiles.push(path);
     }
 
-    if (!tree.children) {
+    if (!tree.children)
+    {
         return documents;
     }
 
-    for (var i in tree.children) {
+    for (var i in tree.children)
+    {
         documents = getDocuments(tree.children[i], documents);
     }
 
     return documents;
 }
 
-function wordMap(string) {
-    var map   = {},
-        words = string.split(/[-\[\],:<>+*=;{}'().\s\d/\\]+/);
+function wordMap (string)
+{
+    var map= {};
+    var words = string.split(/[-\[\],:<>+*=;{}'().\s\d/\\]+/);
 
     words.forEach(function (word) {
+
         word = word.toLowerCase();
-        if (!map.hasOwnProperty(word)) {
+
+        if (!map.hasOwnProperty(word))
+        {
             map[word] = 0;
         }
 
         map[word]++;
+
     });
 
     return map;
 }
 
-function indexDocument(documentPath) {
-
-    var path     = documentPath.replace("public/src/", "");
-    var title    = path.replace(".json", "");
+function indexDocument (documentPath)
+{
+    var path = documentPath.replace("public/src/", "");
+    var title = path.replace(".json", "");
     var contents = fs.readFileSync(documentPath).toString();
 
     title = title.replace(".js", "");
 
-    return {
-        path       : path,
-        titleWords : wordMap(title),
-        bodyWords  : wordMap(contents)
+    return { path: path, titleWords: wordMap(title), bodyWords: wordMap(contents) }
+}
+
+function debugOutput ()
+{
+    if (debug)
+    {
+        excludedFiles.forEach(function(file) {
+            console.log("Excluded: " + file.entry  + ' - Reason: ' + file.reason);
+        });
+
+        console.log(excludedDirectories);
     }
 }
 
-function debugOutput() {
-    if(debug !== true){
-        return;
-    }
-
-    excludedFiles.forEach(function(file){
-        console.log("Excluded File: " + file);
-    });
-}
-
-function index(filteredTree) {
-
+function index (filteredTree)
+{
     var documents = getDocuments(filteredTree, []);
 
     var index = {};
+
     documents.forEach(function (path) {
-        if(pathIsExcluded(path)){
+
+        if (pathIsExcluded(path))
+        {
             return;
         }
 
-        var document         = indexDocument(path);
+        var document = indexDocument(path);
+
         index[document.path] = document;
+
     });
 
     var indexCount = Object.keys(index).length;
     var excludeCount = excludedFiles.length;
 
-    console.log("Indexed " + indexCount + " Files. Excluded " + excludeCount + " Files.");
-
     debugOutput();
 
-    if(debug){
-        //use prettiefied version for dev and testing
+    console.log("Indexed " + indexCount + " Files. Excluded " + excludeCount + " Files.");
+
+    if (debug)
+    {
+        //  Use prettiefied version for dev and testing
         fs.writeFileSync(indexFile, JSON.stringify(index, null, 2))
     }
-    else {
-        //use minified version for production
+    else
+    {
+        //  Use minified version for production
         fs.writeFileSync(indexFile, JSON.stringify(index))
     }
-
 }
 
-module.exports = {
-    index : index
-};
+module.exports = { index : index };

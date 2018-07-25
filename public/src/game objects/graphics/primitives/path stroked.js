cee76e1b9ee2fc63f54506c4f7af03919f3661e8
Richard Davey <rich@photonstorm.com>
2018-07-25 17:05:27 +0100

    Added Graphics tests and some new textures

---
+++ public/src/game objects/graphics/primitives/path stroked.js
@@ -0,0 +1,39 @@
+var config = {
+    width: 800,
+    height: 600,
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('metal', 'assets/textures/alien-metal.jpg');
+}
+
+function create ()
+{
+    this.sys.game.renderer.blankTexture = this.textures.getFrame('metal').glTexture;
+
+    var graphics = this.add.graphics();
+
+    graphics.lineStyle(64, 0x00ff00, 1);
+
+    graphics.beginPath();
+
+    graphics.moveTo(400, 100);
+    graphics.lineTo(200, 278);
+    graphics.lineTo(340, 430);
+    graphics.lineTo(650, 300);
+    graphics.lineTo(700, 180);
+    graphics.lineTo(600, 80);
+    graphics.lineTo(400, 100);
+
+    graphics.closePath();
+    graphics.strokePath();
+}

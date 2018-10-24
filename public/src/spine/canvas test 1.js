622f21f0893bfbe5eeaf8e42e8ddf52a8f001675
Richard Davey <rich@photonstorm.com>
2018-10-24 23:46:46 +0100

    Added controls to tests

--- public/src/spine/canvas test 1.js
+++ public/src/spine/canvas test 1.js
@@ -7,6 +7,7 @@ var config = {
     scene: {
         preload: preload,
         create: create,
+        update: update,
         pack: {
             files: [
                 { type: 'scenePlugin', key: 'SpineCanvasPlugin', url: 'plugins/SpineCanvasPlugin.js', sceneKey: 'spine' }
@@ -15,10 +16,14 @@ var config = {
     }
 };
 
+var controls;
+
 var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('logo', 'assets/sprites/phaser.png');
+
     this.load.setPath('assets/animations/spine/');
 
     this.load.spine('boy', 'spineboy.json', 'spineboy.atlas');
@@ -26,14 +31,39 @@ function preload ()
 
 function create ()
 {
+    this.add.image(0, 0, 'logo').setOrigin(0);
+
     // this.spine.skeletonRenderer.debugRendering = true;
     // this.spine.skeletonRenderer.triangleRendering = true;
 
-    var spineBoy = this.add.spine(600, 550, 'boy', 'run', true);
+    // var spineBoy = this.add.spine(600, 550, 'boy', 'run', true);
+
+    var spineBoy = this.add.spine(400, 300, 'boy', 'idle', true);
 
     spineBoy.setScale(0.5);
+    // spineBoy.setAngle(-45);
 
-    var spineBoy2 = this.add.spine(200, 400, 'boy', 'shoot', true);
+    // var spineBoy2 = this.add.spine(200, 400, 'boy', 'shoot', true);
 
-    spineBoy2.setScale(0.3);
+    // spineBoy2.setScale(0.3);
+
+    var cursors = this.input.keyboard.createCursorKeys();
+
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        acceleration: 0.06,
+        drag: 0.0005,
+        maxSpeed: 1.0
+    };
+
+    controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+}
+
+function update (time, delta)
+{
+    controls.update(delta);
 }
--- public/src/spine/webgl test 1.js
+++ public/src/spine/webgl test 1.js
@@ -7,6 +7,7 @@ var config = {
     scene: {
         preload: preload,
         create: create,
+        update: update,
         pack: {
             files: [
                 { type: 'scenePlugin', key: 'SpineWebGLPlugin', url: 'plugins/SpineWebGLPlugin.js', sceneKey: 'spine' }
@@ -15,10 +16,14 @@ var config = {
     }
 };
 
+var controls;
+
 var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('logo', 'assets/sprites/phaser.png');
+
     this.load.setPath('assets/animations/spine/');
 
     this.load.spine('boy', 'spineboy.json', 'spineboy.atlas');
@@ -26,13 +31,36 @@ function preload ()
 
 function create ()
 {
-    var spineBoy = this.add.spine(400, 550, 'boy', 'run', true);
+    this.add.image(0, 0, 'logo').setOrigin(0);
 
+    // var spineBoy = this.add.spine(400, 550, 'boy', 'run', true);
+
+    var spineBoy = this.add.spine(400, 300, 'boy', 'idle', true);
     spineBoy.setScale(0.5);
 
-    // spineBoy.setAngle(2);
+    // spineBoy.setAngle(-45);
 
     // var spineBoy2 = this.add.spine(200, 400, 'boy', 'shoot', true);
 
     // spineBoy2.setScale(0.3);
+
+    var cursors = this.input.keyboard.createCursorKeys();
+
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        acceleration: 0.06,
+        drag: 0.0005,
+        maxSpeed: 1.0
+    };
+
+    controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+}
+
+function update (time, delta)
+{
+    controls.update(delta);
 }

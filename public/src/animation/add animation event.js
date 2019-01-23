285d873bfb6af3dc715e24418a7e30ecf6c62705
Richard Davey <rich@photonstorm.com>
2019-01-23 09:37:29 +0000

    Updated tests

--- public/src/animation/add animation event.js
+++ public/src/animation/add animation event.js
@@ -21,7 +21,9 @@ function preload ()
 function create ()
 {
     //  Each time a new animation is added to the Animation Manager we'll call this function
-    this.anims.on('add', addAnimation, this);
+    // this.anims.on('add', addAnimation, this);
+
+    this.anims.on(Phaser.Animations.Events.ADD_ANIMATION, addAnimation, this);
 
     var i = 0;
 

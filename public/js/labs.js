64d0b1ce22e72ae5658359d609fb99193617930d
Richard Davey <rdavey@gmail.com>
2019-02-05 00:21:39 +0000

    3.16 Release

--- public/js/labs.js
+++ public/js/labs.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
     var versions = [
         { val : 'dev', text: 'Dev Build' },
+        { val : '3.16.0', text: '3.16.0' },
         { val : '3.15.1', text: '3.15.1' },
         { val : '3.15.0', text: '3.15.0' },
         { val : '3.14.0', text: '3.14.0' },

1654a5346f082e0d24a3dd263d83df3b2ba40271
Richard Davey <rich@photonstorm.com>
2019-03-07 16:27:07 +0000

    Weehee :)

--- public/src/physics/matterjs/draw chain.js
+++ public/src/physics/matterjs/draw chain.js
@@ -8,7 +8,7 @@ var config = {
         default: 'matter',
         matter: {
             gravity: {
-                y: 0.02
+                y: 0.1
             },
             enableSleep: true,
             debug: true
@@ -25,12 +25,15 @@ function create ()
 {
     this.matter.world.setBounds();
 
-    var sides = 4;
-    var size = 16;
-    var distance = size * 2;
+    var sides = 6;
+    var size = 14;
+    var distance = size * 4;
     var stiffness = 0.1;
     var lastPosition = new Phaser.Math.Vector2();
     var options = { friction: 0.005, frictionAir: 0, restitution: 1 };
+    var pinOptions = { friction: 0, frictionAir: 0, restitution: 0, ignoreGravity: true, inertia: Infinity, isStatic: true };
+
+    var current = null;
     var previous = null;
 
     this.input.on('pointerdown', function (pointer) {
@@ -38,7 +41,7 @@ function create ()
         lastPosition.x = pointer.x;
         lastPosition.y = pointer.y;
 
-        previous = this.matter.add.polygon(pointer.x, pointer.y, sides, size, options);
+        previous = this.matter.add.polygon(pointer.x, pointer.y, sides, size, pinOptions);
 
     }, this);
 
@@ -49,18 +52,25 @@ function create ()
             var x = pointer.x;
             var y = pointer.y;
 
-            if (Phaser.Math.Distance.Between(x, y, lastPosition.x, lastPosition.y) > (size * 2))
+            if (Phaser.Math.Distance.Between(x, y, lastPosition.x, lastPosition.y) > distance)
             {
                 lastPosition.x = x;
                 lastPosition.y = y;
 
-                var current = this.matter.add.polygon(pointer.x, pointer.y, sides, size, options);
+                current = this.matter.add.polygon(pointer.x, pointer.y, sides, size, options);
 
                 this.matter.add.constraint(previous, current, distance, stiffness);
 
                 previous = current;
             }
         }
+
+    }, this);
+
+    this.input.on('pointerup', function (pointer) {
+
+        previous.isStatic = true;
+        previous.ignoreGravity = true;
 
     }, this);
 }

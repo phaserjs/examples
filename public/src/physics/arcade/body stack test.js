44935a3905251d428949098c08f83bbf0abad5e9
Richard Davey <rich@photonstorm.com>
2019-03-11 17:29:15 +0000

    More tests

--- public/src/physics/arcade/body stack test.js
+++ public/src/physics/arcade/body stack test.js
@@ -28,38 +28,73 @@ function preload ()
 {
     this.load.image('block', 'assets/sprites/crate32.png');
     this.load.image('block2', 'assets/sprites/crate.png');
-    this.load.image('mushroom', 'assets/sprites/mushroom2.png');
+    // this.load.image('mushroom', 'assets/sprites/mushroom2.png');
+    this.load.image('mushroom', 'assets/sprites/box-item-boxed.png');
 }
 
 function create ()
 {
-    //  overlap = 1.5 (mushroom DOWN onto block, m v b)
-    //  overlap = -1.5 (mushroom DOWN onto block, b v m)
-
-    //  So, even though they were moving in the same direction, the overlap is negative because
-    //  we were checking them in a different order.
-
-    //  Positive overlap = BOTTOM of body A
-    //  Negative overlap = TOP of body A
-
-    //  We cannot rely on overlap to tell us the face of collision
-
     // mushroom = this.physics.add.image(410, 100, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true);
     // block = this.physics.add.image(400, 170, 'block2').setName('block').setVelocityY(200).setCollideWorldBounds(true);
 
     // mushroom = this.physics.add.image(410, 400, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true);
     // block = this.physics.add.image(400, 300, 'block2').setName('block').setVelocityY(120).setCollideWorldBounds(true);
 
-    mushroom = this.physics.add.image(410, 400, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true);
-    block = this.physics.add.image(400, 300, 'block2').setName('block').setVelocityY(120).setCollideWorldBounds(true);
+    // mushroom = this.physics.add.image(410, 400, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(120).setCollideWorldBounds(true);
+
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(-120).setCollideWorldBounds(true);
+    // mushroom = this.physics.add.image(410, 300, 'mushroom').setName('mushroom').setVelocityY(-100).setCollideWorldBounds(true);
+
+    // mushroom = this.physics.add.image(410, 200, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(0).setCollideWorldBounds(true).setImmovable(true);
+
+    // mushroom = this.physics.add.image(410, 500, 'mushroom').setName('mushroom').setVelocityY(-100).setCollideWorldBounds(true);
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(0).setCollideWorldBounds(true).setImmovable(true);
+
+    // mushroom = this.physics.add.image(410, 200, 'mushroom').setName('mushroom').setVelocityY(200).setCollideWorldBounds(true).setBounce(1);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(0).setCollideWorldBounds(true).setImmovable(true);
+
+    // mushroom = this.physics.add.image(410, 200, 'mushroom').setName('mushroom').setVelocityY(-50).setCollideWorldBounds(true).setImmovable(true);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(-200).setCollideWorldBounds(true).setImmovable(false);
+
+    // mushroom = this.physics.add.image(410, 300, 'mushroom').setName('mushroom').setVelocityY(-50).setCollideWorldBounds(true).setImmovable(false);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(-200).setCollideWorldBounds(true).setImmovable(true);
+
+    // mushroom = this.physics.add.image(410, 200, 'mushroom').setName('mushroom').setVelocityY(100).setCollideWorldBounds(true).setImmovable(false);
+    // block = this.physics.add.image(400, 300, 'block2').setName('block').setVelocityY(50).setCollideWorldBounds(true).setImmovable(true);
+
+    //  TEST 1
+    // mushroom = this.physics.add.image(410, 200, 'mushroom').setName('yellow').setVelocityY(100).setCollideWorldBounds(true).setImmovable(false);
+    // block = this.physics.add.image(400, 500, 'block2').setName('block').setVelocityY(100).setCollideWorldBounds(true).setImmovable(false);
+
+    //  TEST 2
+    // mushroom = this.physics.add.image(410, 500, 'mushroom').setName('yellow').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(false);
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(false);
+
+    //  TEST 3
+    // mushroom = this.physics.add.image(410, 500, 'mushroom').setName('yellow').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(false);
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(0).setCollideWorldBounds(true).setImmovable(true);
+
+    //  TEST 4
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(-20).setCollideWorldBounds(true).setImmovable(false);
+    // mushroom = this.physics.add.image(410, 500, 'mushroom').setName('yellow').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(true);
+
+    //  TEST 5
+    // block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(50).setCollideWorldBounds(true).setImmovable(false);
+    // mushroom = this.physics.add.image(410, 500, 'mushroom').setName('yellow').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(true);
+
+    //  TEST 6
+    block = this.physics.add.image(400, 200, 'block2').setName('block').setVelocityY(50).setCollideWorldBounds(true).setImmovable(true);
+    mushroom = this.physics.add.image(410, 500, 'mushroom').setName('yellow').setVelocityY(-100).setCollideWorldBounds(true).setImmovable(false);
 
     text = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
 }
 
 function update (time)
 {
-    this.physics.collide(mushroom, block);
-    // this.physics.collide(block, mushroom);
+    // this.physics.collide(mushroom, block);
+    this.physics.collide(block, mushroom);
 
     // for (var i = 0; i < 10; i++)
     // {
@@ -80,29 +115,54 @@ function update (time)
 
     // text.setText(t);
 
-    text.setText([
-        'mushroom',
-        'tup: ' + mushroom.body.touching.up,
-        'tdown: ' + mushroom.body.touching.down,
-        'wup: ' + mushroom.body.worldBlocked.up,
-        'up: ' + mushroom.body.blocked.up,
-        'down: ' + mushroom.body.blocked.down,
-        'y: ' + mushroom.body.y,
-        'bot: ' + mushroom.body.bottom,
-        'vy: ' + mushroom.body.velocity.y,
-        'dy: ' + mushroom.body._dy,
-        '',
-        'block',
-        'tup: ' + block.body.touching.up,
-        'tdown: ' + block.body.touching.down,
-        'up: ' + block.body.blocked.up,
-        'down: ' + block.body.blocked.down,
-        'wdown: ' + block.body.worldBlocked.down,
-        'y: ' + block.body.y,
-        'bot: ' + block.body.bottom,
-        'vy: ' + block.body.velocity.y,
-        'dy: ' + block.body._dy
-    ]);
-
-
+    if (Phaser.VERSION === '3.16.2')
+    {
+        text.setText([
+            'mushroom',
+            'tup: ' + mushroom.body.touching.up,
+            'tdown: ' + mushroom.body.touching.down,
+            'up: ' + mushroom.body.blocked.up,
+            'down: ' + mushroom.body.blocked.down,
+            'y: ' + mushroom.body.y,
+            'bot: ' + mushroom.body.bottom,
+            'vy: ' + mushroom.body.velocity.y,
+            'dy: ' + mushroom.body._dy,
+            '',
+            'block',
+            'tup: ' + block.body.touching.up,
+            'tdown: ' + block.body.touching.down,
+            'up: ' + block.body.blocked.up,
+            'down: ' + block.body.blocked.down,
+            'y: ' + block.body.y,
+            'bot: ' + block.body.bottom,
+            'vy: ' + block.body.velocity.y,
+            'dy: ' + block.body._dy
+        ]);
+    }
+    else
+    {
+        text.setText([
+            'mushroom',
+            'tup: ' + mushroom.body.touching.up,
+            'tdown: ' + mushroom.body.touching.down,
+            'wup: ' + mushroom.body.worldBlocked.up,
+            'up: ' + mushroom.body.blocked.up,
+            'down: ' + mushroom.body.blocked.down,
+            'y: ' + mushroom.body.y,
+            'bot: ' + mushroom.body.bottom,
+            'vy: ' + mushroom.body.velocity.y,
+            'dy: ' + mushroom.body._dy,
+            '',
+            'block',
+            'tup: ' + block.body.touching.up,
+            'tdown: ' + block.body.touching.down,
+            'up: ' + block.body.blocked.up,
+            'down: ' + block.body.blocked.down,
+            'wdown: ' + block.body.worldBlocked.down,
+            'y: ' + block.body.y,
+            'bot: ' + block.body.bottom,
+            'vy: ' + block.body.velocity.y,
+            'dy: ' + block.body._dy
+        ]);
+    }
 }

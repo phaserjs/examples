34849e114f70d0eaeaa2d86981f822fa2e48d96d
Richard Davey <rich@photonstorm.com>
2019-04-30 17:10:52 +0100

    Testing new loader

--- public/src/display/shaders/shader test 6.js
+++ public/src/display/shaders/shader test 6.js
@@ -13,14 +13,21 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('pic', 'assets/pics/rick-and-morty-by-sawuinhaff-da64e7y.png');
     this.load.glsl('test', 'assets/shaders/bundle.glsl.js');
 }
 
 function create ()
 {
+    this.add.image(400, 300, 'pic');
+
     var s1 = this.cache.shader.get('Shader1');
     var s2 = this.cache.shader.get('Plasma Mask');
 
-    console.log(s1);
-    console.log(s2);
+    // console.log(s1);
+    // console.log(s2);
+
+    var s = this.add.shader('Shader1', 400, 300, 512, 512);
+
+    console.log(s);
 }

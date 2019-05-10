f68eeb950da951270a74a9b67e4b4c2dfe57a6ab
Richard Davey <rich@photonstorm.com>
2019-05-10 16:38:51 +0100

    3.17 Release

--- public/js/labs.js
+++ public/js/labs.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
     var versions = [
         { val : 'dev', text: 'Dev Build' },
+        { val : '3.17.0', text: '3.17.0' },
         { val : '3.16.2', text: '3.16.2' },
         { val : '3.16.1', text: '3.16.1' },
         { val : '3.15.1', text: '3.15.1' },

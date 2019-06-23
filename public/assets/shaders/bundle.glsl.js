8776af62e1a250fe26c664279d641932ba38fa06
Richard Davey <rich@photonstorm.com>
2019-06-23 22:14:41 +0100

    New mass shader test

--- public/assets/shaders/bundle.glsl.js
+++ public/assets/shaders/bundle.glsl.js
@@ -1,4 +1,60 @@
 ---
+name: Colorful Voronoi
+type: fragment
+author: Brandon Fogerty (xdpixel.com)
+---
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float time;
+uniform vec2 resolution;
+varying vec2 fragCoord;
+
+vec2 hash(vec2 p)
+{
+    mat2 m = mat2(  13.85, 47.77,
+                    99.41, 8.48
+                );
+
+    return fract(sin(m*p) * 46738.29);
+}
+
+float voronoi(vec2 p)
+{
+    vec2 g = floor(p);
+    vec2 f = fract(p);
+
+    float distanceToClosestFeaturePoint = 1.0;
+    for(int y = -1; y <= 1; y++)
+    {
+        for(int x = -1; x <= 1; x++)
+        {
+            vec2 latticePoint = vec2(x, y);
+            float currentDistance = distance(latticePoint + hash(g+latticePoint), f);
+            distanceToClosestFeaturePoint = min(distanceToClosestFeaturePoint, currentDistance);
+        }
+    }
+
+    return distanceToClosestFeaturePoint;
+}
+
+void main( void )
+{
+    vec2 uv = ( fragCoord.xy / resolution.xy ) * 2.0 - 1.0;
+    uv.x *= resolution.x / resolution.y;
+
+    float offset = voronoi(uv*10.0 + vec2(time));
+    float t = 1.0/abs(((uv.x + sin(uv.y + time)) + offset) * 30.0);
+
+    float r = voronoi( uv * 1.0 ) * 1.0;
+    vec3 finalColor = vec3(10.0 * uv.y, 2.0, 1.0 * r ) * t;
+    
+    gl_FragColor = vec4(finalColor, 1.0 );
+}
+
+---
 name: Tunnel
 type: fragment
 uniform.alpha: { "type": "1f", "value": 1.0 }
--- public/src/game objects/shaders/shader mass texture test.js
+++ public/src/game objects/shaders/shader mass texture test.js
@@ -13,24 +13,20 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.glsl('bundle', 'http://192.168.0.100/100-phaser3-snippets/public/assets/shaders/bundle.glsl.js');
+    this.load.glsl('bundle', 'assets/shaders/bundle.glsl.js');
 }
 
 function create ()
 {
-    //  Here we create our shader. It has a size of 128 x 128.
-    var shader = this.add.shader('Raster Sky', 0, 0, 128, 128);
-
-    //  Now we tell it to render to a texture, instead of on the display list.
-    //  The string given here is the key that is used when saving it to the Texture Manager:
+    var shader = this.add.shader('Colorful Voronoi', 0, 0, 128, 128);
 
     shader.setRenderToTexture('wibble');
 
-    var blocks = this.add.group({ key: 'wibble', repeat: 62, setScale: { x: 0.3, y: 0.3 } });
+    var blocks = this.add.group({ key: 'wibble', repeat: 53, setScale: { x: 0.5, y: 0.5 } });
 
     Phaser.Actions.GridAlign(blocks.getChildren(), {
         width: 9,
-        height: 7,
+        height: 6,
         cellWidth: 128,
         cellHeight: 128,
         x: 0,
@@ -43,10 +39,11 @@ function create ()
 
         this.tweens.add({
             targets: child,
-            scaleX: 1.2,
-            scaleY: 1.2,
+            props: {
+                scale: { value: 1, duration: 500 },
+                angle: { value: 360, duration: 4000 }
+            },
             ease: 'Sine.easeInOut',
-            duration: 400,
             delay: i * 50,
             repeat: -1,
             yoyo: true
@@ -56,7 +53,7 @@ function create ()
 
         if (i % 10 === 0)
         {
-            i = 0;
+            //i = 0;
         }
 
     }, this);

f1c40d2f451ff928ffa417bbc1be552ab9f1f1d1
Richard Davey <rich@photonstorm.com>
2019-07-23 13:53:28 +0100

    Hit area tests

--- public/src/spine/add hit area.js
+++ public/src/spine/add hit area.js
@@ -1,8 +1,8 @@
 var config = {
     type: Phaser.WEBGL,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     backgroundColor: '#cdcdcd',
     scene: {
         preload: preload,
@@ -19,42 +19,83 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('logo', 'assets/sprites/phaser.png');
+
     this.load.setPath('assets/spine/demos/');
 
-    this.load.spine('set1', 'demos.json', 'atlas1.atlas');
+    this.load.spine('set1', 'demos.json', [ 'atlas1.atlas', 'atlas2.atlas', 'heroes.atlas' ]);
 }
 
 function create ()
 {
-    var b = this.add.spine(400, 500, 'set1.alien').setScale(0.4);
+    this.add.image(0, 0, 'logo').setOrigin(0);
 
-    // console.log(b.getBounds());
+    // var b = this.add.spine(400, 500, 'set1.alien').setScale(0.5);
+    // var b = this.add.spine(400, 500, 'set1.armorgirl').setScale(0.2);
+    // var b = this.add.spine(400, 500, 'set1.dragon').setScale(0.5);
+    var b = this.add.spine(600, 750, 'set1.greengirl').setScale(0.8);
+    // var b = this.add.spine(400, 500, 'set1.heroes').setSkinByName('Assassin');
+    // var b = this.add.spine(400, 700, 'set1.orangegirl').setScale(1);
+    // var b = this.add.spine(400, 500, 'set1.raptor').setScale(0.6);
+    // var b = this.add.spine(400, 500, 'set1.spineboy').setScale(1);
+    // var b = this.add.spine(400, 500, 'set1.stretchyman').setScale(0.4);
+    // var b = this.add.spine(400, 500, 'set1.tank').setScale(0.1);
+    // var b = this.add.spine(400, 500, 'set1.vine').setScale(0.5);
 
-    var d = b.getBounds();
+    b.setInteractive();
 
-    var r = new Phaser.Geom.Rectangle(0, -d.size.y / 2, d.size.x, d.size.y);
+    // this.input.enableDebug(b, 0xff00ff);
 
-    b.setInteractive(r, Phaser.Geom.Rectangle.Contains);
+    // this.add.rectangle(b.x, b.y, 4, 4, 0x00ff00);
 
-    // console.log(b);
 
-    var text = this.add.text(10, 10, 'click it', { font: '16px Courier', fill: '#00ff00' }).setShadow(1, 1);
+    var anims = b.getAnimationList();
+
+    console.log(anims);
+
+    // var text = this.add.text(10, 10, 'click it', { font: '16px Courier', fill: '#00ff00' }).setShadow(1, 1);
 
     b.on('pointerover', function () {
 
-        text.setText('Over');
+        // text.setText('Over');
+
+        // console.log(b.skeleton.x, b.skeleton.y);
 
     });
 
     b.on('pointerout', function () {
 
-        text.setText('Out');
+        // text.setText('Out');
 
     });
 
-    b.once('pointerdown', function () {
+    this.input.on('pointerdown', function () {
 
-        b.play('death', false);
+        // var bounds = b.getBounds();
 
-    });
+        // console.log(bounds.offset, bounds.size);
+        // console.log(b.width, b.height, b.displayOriginX, b.displayOriginY);
+
+        // console.log(b.skeleton.x, b.skeleton.y);
+
+        // Returns the axis aligned bounding box (AABB) of the region and mesh attachments for the current pose.
+        // offset - An output value, the distance from the skeleton origin to the bottom left corner of the AABB.
+        // size - An output value, the width and height of the AABB.
+
+
+        // var ox = b.skeleton.x;
+        // var oy = 600 - b.skeleton.y;
+        // var oy = 600 - b.y;
+
+        // var ox = b.x;
+        // var oy = b.y;
+
+
+        // var r = this.add.rectangle(bounds.offset.x, 768 - bounds.offset.y, bounds.size.x, bounds.size.y).setStrokeStyle(1, 0x00ff00).setOrigin(0, 1);
+
+        // console.log(r.x, r.y);
+
+        b.play(anims[0], true);
+
+    }, this);
 }

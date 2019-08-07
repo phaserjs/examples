875daf75242682dbf61854a647d215564b2cecbd
Richard Davey <rich@photonstorm.com>
2019-08-07 13:54:42 +0100

    New build

--- public/src/spine/basic spineboy.js
+++ public/src/spine/basic spineboy.js
@@ -19,12 +19,16 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('logo', 'assets/sprites/phaser.png');
+
     this.load.setPath('assets/spine/demos/');
 
-    this.load.spine('set1', 'demos.json', [ 'atlas1.atlas' ]);
+    this.load.spine('set1', 'demos.json', [ 'atlas1.atlas' ], true);
 }
 
 function create ()
 {
+    this.add.image(0, 0, 'logo').setOrigin(0);
+
     this.add.spine(400, 600, 'set1.spineboy', 'idle', true);
 }

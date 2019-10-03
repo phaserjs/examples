f58c29d22b6ab0c13720fc24b5a512bad184b9f5
Richard Davey <rich@photonstorm.com>
2019-10-03 17:51:30 +0100

    Update video.js

--- public/src/game objects/video/video.js
+++ public/src/game objects/video/video.js
@@ -5,19 +5,33 @@ var config = {
     backgroundColor: '#2d2d2d',
     parent: 'phaser-example',
     scene: {
+        preload: preload,
         create: create
     }
 };
 
 var game = new Phaser.Game(config);
 
+function preload ()
+{
+    this.load.video('wormhole', 'assets/video/wormhole.mp4', 'canplaythrough', false);
+    // this.load.video('wormhole', 'assets/video/wormhole.mp4', '', true);
+}
+
 function create ()
 {
-    var wormhole = this.add.video(400, 300);
+    var t = this.cache.video.get('wormhole');
+
+    console.log('CREATE cache test ---->');
+    console.log(t);
+
+    var vid = this.add.video(400, 300, 'wormhole');
 
     this.input.once('pointerdown', function () {
 
-        wormhole.createVideoFromURL('assets/video/wormhole.mp4', true);
+        vid.play(true);
+
+        // wormhole.createVideoFromURL('assets/video/wormhole.mp4', true);
 
     });
 }

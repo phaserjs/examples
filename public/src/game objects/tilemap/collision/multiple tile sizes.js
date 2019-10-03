fee7130a7c1e85568b64ff0582a28019cc742159
Richard Davey <rich@photonstorm.com>
2019-10-03 14:50:15 +0100

    Update multiple tile sizes.js

--- public/src/game objects/tilemap/collision/multiple tile sizes.js
+++ public/src/game objects/tilemap/collision/multiple tile sizes.js
@@ -7,7 +7,7 @@ var config = {
     pixelArt: true,
     physics: {
         default: 'arcade',
-        arcade: { gravity: { y: 300 } }
+        arcade: { gravity: { y: 400 }, debug: true }
     },
     scene: {
         preload: preload,
@@ -43,19 +43,21 @@ function create ()
 {
     map = this.add.tilemap('map');
 
-    var groundTiles = map.addTilesetImage('ground_1x1');
-    var kennyTiles = map.addTilesetImage('kenny_platformer_64x64');
-    var treeTiles = map.addTilesetImage('walls_1x2');
+    var groundTiles = map.addTilesetImage('ground_1x1', 'ground_1x1', 32, 32);
+    var kennyTiles = map.addTilesetImage('kenny_platformer_64x64', 'kenny_platformer_64x64', 64, 64);
+    var treeTiles = map.addTilesetImage('walls_1x2', 'walls_1x2', 32, 64);
 
     kenny64x64Layer = map.createDynamicLayer('Kenny 64x64 Layer', kennyTiles);
     ground32x32Layer = map.createDynamicLayer('Ground 32x32 Layer', groundTiles);
     tree32x64Layer = map.createDynamicLayer('Tree 32x64 Layer', treeTiles);
 
+    console.log(kenny64x64Layer);
+
     ground32x32Layer.setCollisionByExclusion([ -1 ]);
     tree32x64Layer.setCollisionByExclusion([ -1 ]);
     kenny64x64Layer.setCollision([ 73 ]);
 
-    player = this.physics.add.sprite(190, 400, 'player').setBounce(0.1);
+    player = this.physics.add.sprite(500, 300, 'player').setBounce(0.1);
 
     this.physics.add.collider(player, ground32x32Layer);
     this.physics.add.collider(player, kenny64x64Layer);

bc02fe17c96f8e6b7c3adebc06dc0faaf3fd9325
Richard Davey <rich@photonstorm.com>
2020-01-14 12:49:53 +0000

    Update advanced shape creation.js

--- public/src/physics/matterjs/advanced shape creation.js
+++ public/src/physics/matterjs/advanced shape creation.js
@@ -2,9 +2,7 @@
  *  This example show how to load complex shapes created with PhysicsEditor (https://www.codeandweb.com/physicseditor)
  */
 
-
 const config = {
-    type: Phaser.AUTO,
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 600,
@@ -14,40 +12,48 @@ const config = {
         create: create
     },
     physics: {
-        default: "matter",
+        default: 'matter',
         matter: {
             debug: true
         }
     }
 };
 
-
 var game = new Phaser.Game(config);
 
-function preload() {
-    // Load sprite sheet generated with TexturePacker
+function preload ()
+{
+    //  Load sprite sheet generated with TexturePacker
     this.load.atlas('sheet', 'assets/physics/fruit-sprites.png', 'assets/physics/fruit-sprites.json');
 
-    // Load body shapes from JSON file generated using PhysicsEditor
+    //  Load body shapes from JSON file generated using PhysicsEditor
     this.load.json('shapes', 'assets/physics/fruit-shapes.json');
 }
 
-function create() {
+function create ()
+{
+    this.matter.world.setBounds(0, 0, 600, 800);
+
     var shapes = this.cache.json.get('shapes');
 
-    this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
     this.add.image(0, 0, 'sheet', 'background').setOrigin(0, 0);
 
-    // sprites are positioned at their center of mass
-    var ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', {shape: shapes.ground});
-    ground.setPosition(0 + ground.centerOfMass.x, 0 + ground.centerOfMass.y);  // position (0,0)
+    var ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', { shape: shapes.ground });
 
-    this.matter.add.sprite(200, 50, 'sheet', 'crate', {shape: shapes.crate});
-    this.matter.add.sprite(250, 250, 'sheet', 'banana', {shape: shapes.banana});
-    this.matter.add.sprite(360, 50, 'sheet', 'orange', {shape: shapes.orange});
-    this.matter.add.sprite(400, 250, 'sheet', 'cherries', {shape: shapes.cherries});
+    this.matter.alignBody(ground, 300, 800, Phaser.Display.Align.BOTTOM_CENTER);
 
-    this.input.on('pointerdown', function (pointer) {
-        this.matter.add.sprite(pointer.x, pointer.y, 'sheet', 'banana', {shape: shapes.banana});
+    this.matter.add.sprite(200, 50, 'sheet', 'crate', { shape: shapes.crate });
+    this.matter.add.sprite(250, 250, 'sheet', 'banana', { shape: shapes.banana });
+    this.matter.add.sprite(360, 50, 'sheet', 'orange', { shape: shapes.orange });
+    this.matter.add.sprite(400, 250, 'sheet', 'cherries', { shape: shapes.cherries });
+
+    var shapeKeys = [ 'crate', 'banana', 'orange', 'cherries' ];
+
+    this.input.on('pointerdown', function (pointer)
+    {
+        var fruit = Phaser.Utils.Array.GetRandom(shapeKeys);
+
+        this.matter.add.image(pointer.x, pointer.y, 'sheet', fruit, { shape: shapes[fruit] });
+
     }, this);
 }

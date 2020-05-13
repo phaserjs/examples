f94a7225ea7c3d5ac0c251fe7fdc65f2e4275cab
samme <samme@users.noreply.github.com>
2020-05-13 16:56:44 -0700

    Run all the tests (can't be chained)

--- public/src/audio/No Audio/Basic Playback and Events.js
+++ public/src/audio/No Audio/Basic Playback and Events.js
@@ -48,517 +48,12 @@ var first;
 var second;
 var audioSprite;
 
-var tests = [
-
-    function (fn)
-    {
-        first.once('play', function (sound)
-        {
-            text.setText('Playing');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.play();
-    },
-
-    function (fn)
-    {
-        first.once('pause', function (sound)
-        {
-            text.setText('Paused');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.pause();
-    },
-
-    function(fn)
-    {
-        first.once('resume', function (sound)
-        {
-            text.setText('Resuming');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.resume();
-    },
-
-    function(fn)
-    {
-        first.once('stop', function (sound)
-        {
-            text.setText('Stopped');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.stop();
-    },
-
-    function(fn)
-    {
-        first.once('play', function (sound)
-        {
-            text.setText('Play from start');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.play();
-    },
-
-    function(fn)
-    {
-        first.once('rate', function (sound, value)
-        {
-            text.setText('Speed up rate');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.rate = 1.5;
-    },
-
-    function(fn)
-    {
-        first.once('detune', function (sound, value)
-        {
-            text.setText('Speed up detune');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.detune = 600;
-    },
-
-    function(fn)
-    {
-        first.once('rate', function (sound, value)
-        {
-            text.setText('Slow down rate');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.rate = 1;
-    },
-
-    function(fn)
-    {
-        first.once('detune', function (sound, value)
-        {
-            text.setText('Slow down detune');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.detune = 0;
-    },
-
-    function(fn)
-    {
-        this.tweens.add({
-
-            onStart: function ()
-            {
-                text.setText('Fade out');
-            },
-
-            targets: first,
-            volume: 0,
-
-            ease: 'Linear',
-            duration: 2000,
-
-            onComplete: fn
-        });
-    },
-
-    function(fn)
-    {
-        this.tweens.add({
-
-            onStart: function ()
-            {
-                text.setText('Fade in');
-            },
-
-            targets: first,
-            volume: 1,
-
-            ease: 'Linear',
-            duration: 2000,
-
-            onComplete: fn
-        });
-    },
-
-    function(fn)
-    {
-        first.once('mute', function()
-        {
-            text.setText('Mute');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.mute = true;
-    },
-
-    function(fn)
-    {
-        first.once('mute', function()
-        {
-            text.setText('Unmute');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.mute = false;
-    },
-
-    function(fn)
-    {
-        first.once('volume', function()
-        {
-            text.setText('Half volume');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.volume = 0.5;
-    },
-
-    function(fn)
-    {
-        first.once('volume', function()
-        {
-            text.setText('Full volume');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.volume = 1;
-    },
-
-    function(fn)
-    {
-        first.once('seek', function()
-        {
-            text.setText('Seek to start');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        first.seek = 0;
-    },
-
-    function(fn)
-    {
-        second.once('play', function()
-        {
-            text.setText('Play 2nd');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        second.play();
-    },
-
-    function(fn)
-    {
-        this.sound.once('mute', function (soundManager, value)
-        {
-            text.setText('Mute global');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.mute = true;
-    },
-
-    function(fn)
-    {
-        this.sound.once('mute', function (soundManager, value)
-        {
-            text.setText('Unmute global');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.mute = false;
-    },
-
-    function(fn)
-    {
-        this.sound.once('volume', function (soundManager, value)
-        {
-            text.setText('Half volume global');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.volume = 0.5;
-    },
-
-    function(fn)
-    {
-        this.tweens.add({
-
-            onStart: function ()
-            {
-                text.setText('Fade out global');
-            },
-
-            targets: this.sound,
-            volume: 0,
-
-            ease: 'Linear',
-            duration: 2000,
-
-            onComplete: fn
-        });
-    },
-
-    function(fn)
-    {
-        this.tweens.add({
-
-            onStart: function ()
-            {
-                text.setText('Fade in global');
-            },
-
-            targets: this.sound,
-            volume: 1,
-
-            ease: 'Linear',
-            duration: 2000,
-
-            onComplete: fn
-        });
-    },
-
-    function(fn)
-    {
-        this.sound.once('pauseall', function (soundManager)
-        {
-            text.setText('Pause all');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.pauseAll();
-    },
-
-    function(fn)
-    {
-        this.sound.once('resumeall', function (soundManager)
-        {
-            text.setText('Resume all');
-            this.time.addEvent({
-                delay: 2000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.resumeAll();
-    },
-
-    function(fn)
-    {
-        this.sound.once('stopall', function (soundManager)
-        {
-            text.setText('Stop all');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        this.sound.stopAll();
-    },
-
-    function(fn)
-    {
-        audioSprite.once('play', function (sound)
-        {
-            text.setText('Play sprite');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        audioSprite.play('07');
-    },
-
-    function(fn)
-    {
-        audioSprite.once('pause', function (sound)
-        {
-            text.setText('Pause sprite');
-            this.time.addEvent({
-                delay: 1000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        audioSprite.pause();
-    },
-
-    function(fn)
-    {
-        audioSprite.once('resume', function (sound)
-        {
-            text.setText('Resume sprite');
-            this.time.addEvent({
-                delay: 1500,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        audioSprite.resume();
-    },
-
-    function(fn)
-    {
-        audioSprite.once('play', function (sound)
-        {
-            text.setText('Multiple sprites');
-            this.time.addEvent({
-                delay: 10000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        var sounds = ['01', '02', '03', '03', '05'];
-
-        for (var i=0; i<sounds.length; i++)
-        {
-            this.time.addEvent({
-                delay: i * 2000,
-                callback: audioSprite.play.bind(audioSprite, sounds[i]),
-                callbackScope: audioSprite
-            });
-        }
-    },
-
-    function(fn)
-    {
-        audioSprite.once('play', function (sound)
-        {
-            text.setText('Loop sprite');
-            this.time.addEvent({
-                delay: 4000,
-                callback: fn,
-                callbackScope: this
-            });
-        }, this);
-
-        audioSprite.play('06', {
-            loop: true
-        });
-    },
-
-    function(fn)
-    {
-        this.tweens.add({
-
-            onStart: function ()
-            {
-                text.setText('Fade out sprite');
-            },
-
-            targets: audioSprite,
-            volume: 0,
-
-            ease: 'Linear',
-            duration: 4000,
-
-            onComplete: function()
-            {
-                audioSprite.volume = 1;
-                audioSprite.stop();
-
-                fn();
-            }
-        });
-    }
-];
-
 function create ()
 {
     this.sound.pauseOnBlur = false;
 
     var prometheus = this.add.image(400, 300, 'prometheus');
-    prometheus.setScale(600/prometheus.height);
+    prometheus.setScale(600 / prometheus.height);
 
     text = this.add.text(400, 300, 'Loading...', {
         fontFamily: '\'Sorts Mill Goudy\', serif',
@@ -567,42 +62,63 @@ function create ()
         align: 'center'
     });
     text.setOrigin(0.5);
-    text.setShadow(0, 1, "#888", 2);
+    text.setShadow(0, 1, '#8 88', 2);
 
     first = this.sound.add('overture', { loop: true });
     second = this.sound.add('overture', { loop: true });
     audioSprite = this.sound.addAudioSprite('creatures');
 
+    first.once('destroy', onDestroy);
+    second.once('destroy', onDestroy);
+    audioSprite.once('destroy', onDestroy);
+
     enableInput.call(this);
 }
 
-function chain(i)
+function onDestroy (sound)
 {
-    return function()
-    {
-        if (tests[i])
-        {
-            tests[i].call(this, chain.call(this, ++i));
-        }
-        else
-        {
-            text.setText('Complete!');
-
-            this.time.addEvent({
-                delay: 5000,
-                callback: enableInput,
-                callbackScope: this
-            });
-        }
-    }.bind(this);
+    console.log('destroy', sound.key);
 }
 
-function enableInput()
+function enableInput ()
 {
     text.setText('Click to start');
 
     this.input.once('pointerdown', function (pointer)
     {
-        tests[0].call(this, chain.call(this, 1));
+        first.play();
+        first.pause();
+        first.resume();
+        first.stop();
+        first.play();
+        first.rate = 1.5;
+        first.detune = 600;
+        first.rate = 1;
+        first.detune = 0;
+        first.volume = 0;
+        first.volume = 1;
+        first.mute = true;
+        first.mute = false;
+        first.seek = 0;
+        second.play();
+        this.sound.mute = true;
+        this.sound.mute = false;
+        this.sound.volume = 0.5;
+        this.sound.pauseAll();
+        this.sound.resumeAll();
+        this.sound.stopAll();
+        audioSprite.play('07');
+        audioSprite.pause();
+        audioSprite.resume();
+        audioSprite.play('06', {
+            loop: true
+        });
+        audioSprite.volume = 1;
+        audioSprite.stop();
+        first.destroy();
+        second.destroy();
+        audioSprite.destroy();
+
+        text.setText('Done');
     }, this);
 }

53281ffc478d819c6320052e54035f2c181e10dc
samme <samme@users.noreply.github.com>
2020-05-13 17:09:36 -0700

    Convert to Arcade Physics

--- public/src/paths/velocity from tangent vectors.js
+++ public/src/paths/velocity from tangent vectors.js
@@ -5,21 +5,20 @@ var config = {
     backgroundColor: '#2d2d2d',
     parent: 'phaser-example',
     physics: {
-        default: 'impact'
+        default: 'arcade',
+        arcade: { debug: true }
     },
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create
     }
 };
 
+var DELAY = 500;
 var t = 0;
 var curve;
 var points;
 var ship;
-var tempVec;
-var tempVecP;
 
 var game = new Phaser.Game(config);
 
@@ -38,59 +37,32 @@ function create ()
 
     curve = new Phaser.Curves.Ellipse(400, 300, 200);
 
+    curve.draw(this.add.graphics({ lineStyle: { color: 0xffffff } }));
+
+    // 32 divisions makes 33 points.
     points = curve.getSpacedPoints(32);
 
-    tempVec = new Phaser.Math.Vector2();
-    tempVecP = new Phaser.Math.Vector2();
+    // Ellipse's first and last points are identical.
+    // Discard the last one.
+    points.pop();
 
-    ship = this.impact.add.image(points[0].x, points[0].y, 'ship');
+    console.table(points, ['x', 'y']);
+
+    ship = this.physics.add.image(points[0].x, points[0].y, 'ship');
 
     nextPoint(this);
 }
 
 function nextPoint (scene)
 {
-    var next = points[t % points.length]; 
+    var next = points[t % points.length];
 
-    moveToXY(ship, next.x, next.y, 0, 500);
+    console.log('move from (%s, %s) to (%s, %s) [%s]', ship.x.toFixed(1), ship.y.toFixed(1), next.x.toFixed(1), next.y.toFixed(1), t % points.length);
+
+    ship.rotation = scene.physics.moveToObject(ship, next, 0, DELAY);
 
     t++;
 
-    scene.time.addEvent({ delay: 500, callback: nextPoint, callbackScope: scene, args: [ scene ] });
+    scene.time.addEvent({ delay: DELAY, callback: nextPoint, callbackScope: scene, args: [ scene ] });
 }
 
-function moveToXY (gameObject, x, y, speed, maxTime)
-{
-    if (speed === undefined) { speed = 60; }
-    if (maxTime === undefined) { maxTime = 0; }
-
-    var angle = Math.atan2(y - gameObject.y, x - gameObject.x);
-
-    if (maxTime > 0)
-    {
-        //  We know how many pixels we need to move, but how fast?
-        var dx = gameObject.x - x;
-        var dy = gameObject.y - y;
-
-        speed = Math.sqrt(dx * dx + dy * dy) / (maxTime / 1000);
-    }
-
-    gameObject.setVelocityX(Math.cos(angle) * speed);
-    gameObject.setVelocityY(Math.sin(angle) * speed);
-
-    // gameObject.rotation = angle;
-}
-
-function update ()
-{
-    // var t = curve.getUtoTmapping(map.u);
-
-    // curve.getPoint(t + 0.1, tempVecP);
-    // curve.getTangent(t, tempVec);
-
-    // tempVec.scale(180);
-
-    // ship.setVelocity(tempVec.x, tempVec.y);
-
-    // ship.rotation = Phaser.Math.Angle.Between(ship.x, ship.y, tempVecP.x, tempVecP.y);
-}

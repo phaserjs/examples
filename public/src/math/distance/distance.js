304f121ae3f194398b85ac6f95e7b0595df30a80
samme <samme@users.noreply.github.com>
2020-05-13 17:09:16 -0700

    Move with pointer instead

--- public/src/math/distance/distance.js
+++ public/src/math/distance/distance.js
@@ -1,52 +1,44 @@
 var config = {
     type: Phaser.AUTO,
+    width: 800,
+    height: 600,
     parent: 'phaser-example',
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create
     }
 };
 
-var cursors;
+var graphic;
 var player;
 var ufo;
 
 var game = new Phaser.Game(config);
 
-function preload() {
+function preload ()
+{
     this.load.image('eyes', 'assets/sprites/slimeeyes.png');
     this.load.image('ufo', 'assets/sprites/ufo.png');
 }
 
-function create() {
-    cursors = this.input.keyboard.createCursorKeys();
-
+function create ()
+{
     player = this.add.image(400, 300, 'eyes');
-
     ufo = this.add.image(200, 150, 'ufo');
-
     graphic = this.add.graphics({ lineStyle: { color: 0x00ffff } });
+
+    draw();
+
+    this.input.on('pointermove', function (pointer)
+    {
+        player.setPosition(pointer.x, pointer.y);
+        draw();
+    });
 }
 
-function update() {
-    if (cursors.left.isDown) {
-        player.x -= 5;
-    }
-    else if (cursors.right.isDown) {
-        player.x += 5;
-    }
-
-    if (cursors.up.isDown) {
-        player.y -= 5;
-    }
-    else if (cursors.down.isDown) {
-        player.y += 5;
-    }
-
+function draw ()
+{
     var dist = Phaser.Math.Distance.Between(player.x, player.y, ufo.x, ufo.y);
 
-    graphic
-        .clear()
-        .strokeCircle(player.x, player.y, dist);
+    graphic.clear().strokeCircle(player.x, player.y, dist);
 }

85b2ca96b558a1165951f0b232455f9f1fda77c8
samme <samme@users.noreply.github.com>
2020-05-13 17:02:15 -0700

    Rename and clean up

--- public/src/game objects/particle emitter/digital rain.js
+++ public/src/game objects/particle emitter/digital rain.js
@@ -14,7 +14,7 @@ new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.spritesheet('font', 'assets/fonts/retro/knighthawks-font-filled.png', { frameWidth: 32, frameHeight: 25 });
+    this.load.spritesheet('glyphs', 'assets/fonts/retro/knighthawks-font-filled.png', { frameWidth: 32, frameHeight: 25 });
 }
 
 function create ()
@@ -26,42 +26,43 @@ function create ()
         cellHeight: 16,
         getPoints: function (quantity)
         {
-            var cols = (new Array(codeRain.width)).fill(0);
-            var lastCol = cols.length - 1;
+            var cols = Array(codeRain.width).fill(0);
+            var xMax = codeRain.width - 1;
             var Between = Phaser.Math.Between;
-            var RND = Phaser.Math.RND;
+            var FloatBetween = Phaser.Math.FloatBetween;
             var points = [];
 
             for (var i = 0; i < quantity; i++)
             {
-                var col = Between(0, lastCol);
-                var row = (cols[col] += 1);
+                var x = Between(0, xMax);
+                var y = (cols[x] += 1);
 
-                if (RND.frac() < 0.01)
+                if (FloatBetween(0, 1) < 0.01)
                 {
-                    row *= RND.frac();
+                    y *= FloatBetween(0, 1);
+                    y |= 0;
                 }
 
-                row %= codeRain.height;
-                row |= 0;
+                y %= codeRain.height;
 
-                points[i] = new Phaser.Math.Vector2(16 * col, 16 * row);
+                points[i] = new Phaser.Math.Vector2(x * codeRain.cellWidth, y * codeRain.cellHeight);
             }
 
             return points;
         }
     };
 
-    this.add.particles('font').createEmitter({
-        alpha: { start: 1, end: 0.25, ease: 'Expo.easeOut' },
-        angle: 0,
-        blendMode: 'ADD',
-        emitZone: { source: codeRain, type: 'edge', quantity: 2000 },
-        frame: Phaser.Utils.Array.NumberArray(8, 58),
-        frequency: 100,
-        lifespan: 6000,
-        quantity: 25,
-        scale: -0.5,
-        tint: 0x0066ff00
-    });
+    this.add.particles('glyphs')
+        .createEmitter({
+            alpha: { start: 1, end: 0.25, ease: 'Expo.easeOut' },
+            angle: 0,
+            blendMode: 'ADD',
+            emitZone: { source: codeRain, type: 'edge', quantity: 2000 },
+            frame: Phaser.Utils.Array.NumberArray(8, 58),
+            frequency: 100,
+            lifespan: 6000,
+            quantity: 25,
+            scale: -0.5,
+            tint: 0x0066ff00
+        });
 }

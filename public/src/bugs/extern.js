69e5aa4f1a39e5f46a9b7bab0039f01e8e694090
Richard Davey <rich@photonstorm.com>
2020-10-08 15:54:01 +0100

    New Spine plugin

--- public/src/bugs/extern.js
+++ public/src/bugs/extern.js
@@ -11,45 +11,41 @@ var config = {
     }
 };
 
-var camera;
-var scene;
 var mesh;
-var renderer;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
     this.load.image('logo', 'assets/sprites/phaser.png');
-    this.load.script('threejs', 'assets/tests/three.min.js');
+    // this.load.script('threejs', 'assets/tests/three.min.js');
+    this.load.script('threejs', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r117/three.min.js');
 }
 
 function create ()
 {
-    // this.add.text(10, 10, 'Extern Test 1');
-
-    var e = this.add.extern();
+    var exten = this.add.extern();
 
     this.add.image(400, 300, 'logo');
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+    var camera = new THREE.PerspectiveCamera( 70, 800 / 600, 1, 1000 );
+
     camera.position.z = 400;
 
-    scene = new THREE.Scene();
+    var scene = new THREE.Scene();
 
     var texture = new THREE.TextureLoader().load('assets/textures/tiles.jpg');
 
-    var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-    var material = new THREE.MeshBasicMaterial( { map: texture } );
+    var geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+    var material = new THREE.MeshBasicMaterial({ map: texture });
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    mesh = new THREE.Mesh(geometry, material);
 
-    renderer = new THREE.WebGLRenderer( { canvas: this.game.canvas, context: this.game.context, antialias: true } );
-    // renderer = new THREE.WebGLRenderer();
-    // renderer.setSize(800, 600);
+    scene.add(mesh);
 
-    e.render = function (prenderer, pcamera, pcalcMatrix)
+    var renderer = new THREE.WebGLRenderer({ canvas: this.game.canvas, context: this.game.context, antialias: true });
+
+    exten.render = function (prenderer, pcamera, pcalcMatrix)
     {
         renderer.state.reset();
 
@@ -57,15 +53,10 @@ function create ()
     }
 
     renderer.autoClear = false;
-
-    // document.body.appendChild( renderer.domElement );
 }
 
 function update ()
 {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
-
-    // renderer.render( scene, camera );
 }
-

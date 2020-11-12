adc95eedca4551ca9d362fac50727dc2035c2df9
Francisco Pereira <gammafp@gmail.com>
2020-11-12 13:26:40 +0100

    Merge tilemap folders

--- public/src/3.50/camera/multi camera/DemoC.js
+++ public/src/3.50/camera/multi camera/DemoC.js
@@ -20,11 +20,11 @@ var DemoC = new Phaser.Class({
     {
         var frames = ['atari400', 'bunny', 'cokecan', 'copy-that-floppy', 'hotdog'];
 
-        for (var i = 0; i < frames.length; ++i) 
+        for (var i = 0; i < frames.length; ++i)
         {
             this.tilesprites[i] = this.add.tileSprite(i * 160, 0, 160, 600, 'atlas', frames[i]);
             this.tilesprites[i].setOrigin(0)
-            this.tilesprites[i].setTint(0xff00ff, 0xffff00, 0x0000ff, 0xff0000);
+            // this.tilesprites[i].setTint(0xff00ff, 0xffff00, 0x0000ff, 0xff0000);
         }
 
         var cam = this.cameras.main;
@@ -37,7 +37,7 @@ var DemoC = new Phaser.Class({
     {
         var x = 4;
 
-        for (var i = 0; i < this.tilesprites.length; ++i) 
+        for (var i = 0; i < this.tilesprites.length; ++i)
         {
             this.tilesprites[i].tilePositionX += x;
             this.tilesprites[i].tilePositionY += x;

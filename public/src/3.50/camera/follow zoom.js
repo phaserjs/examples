b9a1fde4feeebf90bc3bfff0a24ee7fe6dd29d75
gammafp <gammafp@gmail.com>
2020-11-18 19:18:25 +0100

    Updated ES6

--- public/src/3.50/camera/follow zoom.js
+++ public/src/3.50/camera/follow zoom.js
@@ -1,4 +1,83 @@
-var config = {
+class Example extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload () 
+    {
+        this.load.image('map', 'assets/tests/camera/earthbound-scarab.png');
+        this.load.image('ship', 'assets/sprites/fmship.png');
+    }
+
+    create () 
+    {
+        this.cameras.main.setBounds(0, 0, 1024, 2048);
+    
+        this.add.image(0, 0, 'map').setOrigin(0).setScrollFactor(1);
+    
+        this.cursors = this.input.keyboard.createCursorKeys();
+    
+        this.ship = this.physics.add.image(400.5, 301.3, 'ship');
+        // ship = this.add.image(400.5, 301.3, 'ship');
+    
+        this.cameras.main.startFollow(this.ship, true, 0.09, 0.09);
+        // this.cameras.main.roundPixels = true;
+    
+        this.cameras.main.setZoom(4);
+    }
+
+    updateDirect ()
+    {
+        if (this.cursors.left.isDown)
+        {
+            this.ship.setAngle(-90);
+            this.ship.x -= 2.5;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.ship.setAngle(90);
+            this.ship.x += 2.5;
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.ship.setAngle(0);
+            this.ship.y -= 2.5;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.ship.setAngle(-180);
+            this.ship.y += 2.5;
+        }
+    }
+
+    update () 
+    {
+        this.ship.setVelocity(0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.ship.setAngle(-90).setVelocityX(-200);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.ship.setAngle(90).setVelocityX(200);
+        }
+    
+        if (this.cursors.up.isDown)
+        {
+            this.ship.setAngle(0).setVelocityY(-200);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.ship.setAngle(-180).setVelocityY(200);
+        }
+    }
+}
+
+const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
@@ -7,84 +86,6 @@ var config = {
     physics: {
         default: 'arcade',
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [ Example ]
 };
-
-var ship;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('map', 'assets/tests/camera/earthbound-scarab.png');
-    this.load.image('ship', 'assets/sprites/fmship.png');
-}
-
-function create ()
-{
-    this.cameras.main.setBounds(0, 0, 1024, 2048);
-    
-    this.add.image(0, 0, 'map').setOrigin(0).setScrollFactor(1);
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    ship = this.physics.add.image(400.5, 301.3, 'ship');
-    // ship = this.add.image(400.5, 301.3, 'ship');
-
-    this.cameras.main.startFollow(ship, true, 0.09, 0.09);
-    // this.cameras.main.roundPixels = true;
-
-    this.cameras.main.setZoom(4);
-}
-
-function updateDirect ()
-{
-    if (cursors.left.isDown)
-    {
-        ship.setAngle(-90);
-        ship.x -= 2.5;
-    }
-    else if (cursors.right.isDown)
-    {
-        ship.setAngle(90);
-        ship.x += 2.5;
-    }
-
-    if (cursors.up.isDown)
-    {
-        ship.setAngle(0);
-        ship.y -= 2.5;
-    }
-    else if (cursors.down.isDown)
-    {
-        ship.setAngle(-180);
-        ship.y += 2.5;
-    }
-}
-
-function update ()
-{
-    ship.setVelocity(0);
-
-    if (cursors.left.isDown)
-    {
-        ship.setAngle(-90).setVelocityX(-200);
-    }
-    else if (cursors.right.isDown)
-    {
-        ship.setAngle(90).setVelocityX(200);
-    }
-
-    if (cursors.up.isDown)
-    {
-        ship.setAngle(0).setVelocityY(-200);
-    }
-    else if (cursors.down.isDown)
-    {
-        ship.setAngle(-180).setVelocityY(200);
-    }
-}
+const game = new Phaser.Game(config);
--- public/src/3.50/camera/ignore.js
+++ public/src/3.50/camera/ignore.js
@@ -1,49 +1,48 @@
-var config = {
+class Example extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload () 
+    {
+        this.load.image('einstein', 'assets/pics/ra-einstein.png');
+    }
+
+    create () 
+    {
+        const image = this.add.image(400, 300, 'einstein');
+
+        this.UIText1 = this.add.text(0, 32, '0');
+        this.UIText2 = this.add.text(0, 64, '0');
+    
+        //  Add in a new camera, the same size and position as the main camera
+        const UICam = this.cameras.add(0, 0, 800, 600);
+    
+        //  The main camera will not render the UI Text objects
+        this.cameras.main.ignore([ this.UIText1, this.UIText2 ]);
+    
+        //  The new UI Camera will not render the background image
+        UICam.ignore(image);
+    }
+
+    update () 
+    {
+        this.UIText1.setText("Main camera rotation: " + this.cameras.main.rotation);
+        this.UIText2.setText("Main camera zoom: " + this.cameras.main.zoom);
+    
+        this.cameras.main.setZoom(Math.abs(Math.sin(this.cameras.main.rotation)) * 0.5 + 1);
+        this.cameras.main.rotation += 0.01;
+    }
+}
+
+const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [ Example ]
 };
 
-var image;
-var UICam;
-var UIText1;
-var UIText2;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('einstein', 'assets/pics/ra-einstein.png');
-}
-
-function create ()
-{
-    image = this.add.image(400, 300, 'einstein');
-
-    UIText1 = this.add.text(0, 32, '0');
-    UIText2 = this.add.text(0, 64, '0');
-
-    //  Add in a new camera, the same size and position as the main camera
-    UICam = this.cameras.add(0, 0, 800, 600);
-
-    //  The main camera will not render the UI Text objects
-    this.cameras.main.ignore([ UIText1, UIText2 ]);
-
-    //  The new UI Camera will not render the background image
-    UICam.ignore(image);
-}
-
-function update() 
-{
-    UIText1.setText("Main camera rotation: " + this.cameras.main.rotation);
-    UIText2.setText("Main camera zoom: " + this.cameras.main.zoom);
-
-    this.cameras.main.setZoom(Math.abs(Math.sin(this.cameras.main.rotation)) * 0.5 + 1);
-    this.cameras.main.rotation += 0.01;
-}
+const game = new Phaser.Game(config);

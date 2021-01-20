c5f23d3eed4e189ab59dc35fac22fe96350ef0fb
Johannes <jcheihei2003@gmail.com>
2021-01-20 15:58:17 +1300

    Pacman arc updated to ES6

--- public/src/game objects/graphics/pacman arc.js
+++ public/src/game objects/graphics/pacman arc.js
@@ -1,22 +1,33 @@
-var config = {
-    width: 800,
-    height: 600,
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    scene: {
-        create: create
-    }
-};
-
-var game = new Phaser.Game(config);
-
-function create ()
+class Example extends Phaser.Scene
 {
-    var graphics = this.add.graphics();
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+    }
+
+    create ()
+    {
+    let graphics = this.add.graphics();
 
     graphics.fillStyle(0xffff00, 1);
 
     graphics.slice(400, 300, 200, Phaser.Math.DegToRad(340), Phaser.Math.DegToRad(20), true);
 
     graphics.fillPath();
+    }
 }
+
+const config = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    scene: [ Example ]
+};
+
+const game = new Phaser.Game(config);
+

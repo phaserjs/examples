82bf4b34cf2d5fcd7338ae627aec7dda4e16138f
Johannes <jcheihei2003@gmail.com>
2021-01-21 11:18:49 +1300

    Update format

--- public/src/game objects/graphics/arc stroke.js
+++ public/src/game objects/graphics/arc stroke.js
@@ -1,32 +1,29 @@
-class Example extends Phaser.Scene
-{
-    constructor ()
-    {
+class Example extends Phaser.Scene {
+    constructor () {
         super();
     }
 
-    preload ()
-    {
+    preload () {
     }
 
-    create ()
-    {
-    let graphics = this.add.graphics();
+    create () {
+        const graphics = this.add.graphics();
 
-    graphics.lineStyle(50, 0xffffff);
+        graphics.lineStyle(50, 0xffffff);
 
-    graphics.beginPath();
-    graphics.arc(400, 300, 200, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), false, 0.02);
-    graphics.strokePath();
-    graphics.closePath();
+        graphics.beginPath();
+        graphics.arc(400, 300, 200, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), false, 0.02);
+        graphics.strokePath();
+        graphics.closePath();
 
-    graphics.beginPath();
-    graphics.lineStyle(40, 0xff00ff);
-    graphics.arc(400, 300, 200, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), true, 0.02);
-    graphics.strokePath();
-    graphics.closePath();
+        graphics.beginPath();
+        graphics.lineStyle(40, 0xff00ff);
+        graphics.arc(400, 300, 200, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), true, 0.02);
+        graphics.strokePath();
+        graphics.closePath();
     }
 }
+
 const config = {
     width: 800,
     height: 600,
@@ -36,4 +33,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-

ea0ee70024888cae84b87e3740069bcf98e4868f
hades <tsunflowertea@gmail.com>
2021-01-21 15:34:37 +1300

    changed to es6

--- public/src/game objects/graphics/multiple cameras.js
+++ public/src/game objects/graphics/multiple cameras.js
@@ -1,10 +1,16 @@
-let i
-let stars = [];
-let cameraRotation = 0;
-let camera0, camera1, camera2, camera3;
-
 class Example extends Phaser.Scene
 {
+    constructor ()
+    {
+        super ();
+        this.stars = [];
+        this.cameraRotation = 0;
+        this.camera0 = undefined;
+        this.camera1 = undefined;
+        this.camera2 = undefined;
+        this.camera3 = undefined;
+    }
+
     create ()
     {
       let radius = 20;
@@ -16,40 +22,40 @@ class Example extends Phaser.Scene
           let graphics = this.add.graphics({x: radius + (i % maxWidth) * radius2, y: radius + ((i / maxWidth)|0) * radius2});
           this.drawStar(graphics, 0, 0, 5, radius, radius / 2, 0xff0000, 0xFFFF00);
           graphics.fillStyle(0xFFFF00 + (i % 0xFF), 1.0);
-          stars.push(graphics);
-          stars[i].rotation += 0.1;
+          this.stars.push(graphics);
+          this.stars[i].rotation += 0.1;
       }
 
       this.cameras.main.setSize(400, 300);
 
-      camera0 = this.cameras.main;
-      camera1 = this.cameras.add(400, 0, 400, 300);
-      camera2 = this.cameras.add(0, 300, 400, 300);
-      camera3 = this.cameras.add(400, 300, 400, 300);
+      this.camera0 = this.cameras.main;
+      this.camera1 = this.cameras.add(400, 0, 400, 300);
+      this.camera2 = this.cameras.add(0, 300, 400, 300);
+      this.camera3 = this.cameras.add(400, 300, 400, 300);
     }
 
     update ()
     {
-      camera0.scrollX = Math.cos(cameraRotation) * 100;
-      camera0.scrollY = Math.sin(cameraRotation) * 100;
-      camera1.shake(100, 0.01);
-      camera2.flash(2000);
-      camera3.fade(2000);
-      camera3.rotation = Math.sin(cameraRotation);
-      camera3.zoom = 0.5 + Math.abs(Math.sin(cameraRotation));
-      if (camera3._fadeAlpha >= 1.0)
+      this.camera0.scrollX = Math.cos(this.cameraRotation) * 100;
+      this.camera0.scrollY = Math.sin(this.cameraRotation) * 100;
+      this.camera1.shake(100, 0.01);
+      this.camera2.flash(2000);
+      this.camera3.fade(2000);
+      this.camera3.rotation = Math.sin(this.cameraRotation);
+      this.camera3.zoom = 0.5 + Math.abs(Math.sin(this.cameraRotation));
+      if (this.camera3._fadeAlpha >= 1.0)
       {
-          camera3._fadeAlpha = 0.0;
-          camera3.fade(1000);
+          this.camera3._fadeAlpha = 0.0;
+          this.camera3.fade(1000);
       }
-      for (let i = 0; i < stars.length; ++i)
+      for (let i = 0; i < this.stars.length; ++i)
       {
-          let star = stars[i];
+          let star = this.stars[i];
           star.rotation += 0.01;
           star.scaleX = 0.5 + Math.abs(Math.sin(star.rotation));
           star.scaleY = 0.5 + Math.abs(Math.sin(star.rotation));
       }
-      cameraRotation += 0.01;
+      this.cameraRotation += 0.01;
     }
 
     drawStar (graphics, cx, cy, spikes, outerRadius, innerRadius, color, lineColor)
@@ -62,7 +68,7 @@ class Example extends Phaser.Scene
       graphics.fillStyle(color, 1.0);
       graphics.beginPath();
       graphics.moveTo(cx, cy - outerRadius);
-      for (i = 0; i < spikes; i++) {
+      for (let i = 0; i < spikes; i++) {
           x = cx + Math.cos(rot) * outerRadius;
           y = cy + Math.sin(rot) * outerRadius;
           graphics.lineTo(x, y);
@@ -80,7 +86,6 @@ class Example extends Phaser.Scene
     }
 }
 
-
 const config = {
   type: Phaser.WEBGL,
   parent: 'phaser-example',
@@ -90,4 +95,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-

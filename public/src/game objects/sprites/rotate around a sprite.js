755b65fae895307c201a8b3deeba13434f5df4cb
hades <tsunflowertea@gmail.com>
2021-01-22 09:35:03 +1300

    consistent indentation

--- public/src/game objects/sprites/rotate around a sprite.js
+++ public/src/game objects/sprites/rotate around a sprite.js
@@ -13,37 +13,37 @@ class Example extends Phaser.Scene
     }
     preload ()
     {
-      this.load.image('ballRed', 'assets/demoscene/ball.png');
-      this.load.image('ballBlue', 'assets/demoscene/blue_ball.png');
-      this.load.image('ballSmall', 'assets/demoscene/ball-tlb.png');
+        this.load.image('ballRed', 'assets/demoscene/ball.png');
+        this.load.image('ballBlue', 'assets/demoscene/blue_ball.png');
+        this.load.image('ballSmall', 'assets/demoscene/ball-tlb.png');
     }
 
     create ()
     {
-      this.ball1 = this.add.sprite(400, 300, 'ballRed');
-      this.ball2 = this.add.sprite(400, 300, 'ballBlue');
-      this.ball3 = this.add.sprite(400, 300, 'ballSmall');
+        this.ball1 = this.add.sprite(400, 300, 'ballRed');
+        this.ball2 = this.add.sprite(400, 300, 'ballBlue');
+        this.ball3 = this.add.sprite(400, 300, 'ballSmall');
     }
 
     update()
     {
-      this.ball2.setPosition(400, 300);
-      this.ball3.setPosition(400, 300);
+        this.ball2.setPosition(400, 300);
+        this.ball3.setPosition(400, 300);
 
-      Phaser.Math.RotateAroundDistance(this.ball2, this.ball1.x, this.ball1.y, this.angle1, this.distance1);
-      Phaser.Math.RotateAroundDistance(this.ball3, this.ball2.x, this.ball2.y, this.angle2, this.distance2);
+        Phaser.Math.RotateAroundDistance(this.ball2, this.ball1.x, this.ball1.y, this.angle1, this.distance1);
+        Phaser.Math.RotateAroundDistance(this.ball3, this.ball2.x, this.ball2.y, this.angle2, this.distance2);
 
-      this.angle1 = Phaser.Math.Angle.Wrap(this.angle1 + 0.02);
-      this.angle2 = Phaser.Math.Angle.Wrap(this.angle2 + 0.03);
+        this.angle1 = Phaser.Math.Angle.Wrap(this.angle1 + 0.02);
+        this.angle2 = Phaser.Math.Angle.Wrap(this.angle2 + 0.03);
     }
 }
 
 const config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: [ Example]
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: [ Example]
 };
 
 const game = new Phaser.Game(config);

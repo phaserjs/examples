2adbf78cff9b99ab0d2d7da00186541ee0e3867f
hades <tsunflowertea@gmail.com>
2021-01-22 09:50:38 +1300

    consistent indentation

--- public/src/game objects/graphics/fill rounded rectangle.js
+++ public/src/game objects/graphics/fill rounded rectangle.js
@@ -4,26 +4,26 @@ class Example extends Phaser.Scene
 {
     create ()
     {
-      graphics = this.add.graphics();
+        graphics = this.add.graphics();
 
-      graphics.fillStyle(0xffff00, 1);
+        graphics.fillStyle(0xffff00, 1);
 
-      //  32px radius on the corners
-      graphics.fillRoundedRect(32, 32, 300, 200, 32);
+        //  32px radius on the corners
+        graphics.fillRoundedRect(32, 32, 300, 200, 32);
 
-      graphics.fillStyle(0xff00ff, 1);
+        graphics.fillStyle(0xff00ff, 1);
 
-      //  Using an object to define a different radius per corner
-      graphics.fillRoundedRect(360, 240, 400, 300, { tl: 64, tr: 22, bl: 12, br: 0 });
+        //  Using an object to define a different radius per corner
+        graphics.fillRoundedRect(360, 240, 400, 300, { tl: 64, tr: 22, bl: 12, br: 0 });
     }
 }
 
 let config = {
-  width: 800,
-  height: 600,
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  scene: [ Example ]
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    scene: [ Example ]
 };
 
 const game = new Phaser.Game(config);

7b21f26b9f423108a88e399a11c967d533f00fb2
Jameson Nash <>
2021-01-22 10:30:34 +1300

    Updates to use the ES6 format.

--- public/src/game objects/graphics/generate texture test.js
+++ public/src/game objects/graphics/generate texture test.js
@@ -1,22 +1,24 @@
-var config = {
+class Example extends Phaser.Scene
+{
+    create ()
+    {
+        let graphics = this.make.graphics().fillStyle(0x00ff00).fillRect(0, 0, 800, 100);
+
+        graphics.generateTexture('hudbar', 800, 100);
+
+        graphics.destroy();
+
+        this.add.image(400, 300, 'hudbar');
+    }
+}
+
+
+const config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: {
-        create: create
-    },
+    parent: 'phaser-example',
+    scene: [ Example ]
 };
 
-var game = new Phaser.Game(config);
-
-function create ()
-{
-    var graphics = this.make.graphics().fillStyle(0x00ff00).fillRect(0, 0, 800, 100);
-
-    graphics.generateTexture('hudbar', 800, 100);
-
-    graphics.destroy();
-
-    this.add.image(400, 300, 'hudbar');
-}
+const game = new Phaser.Game(config);

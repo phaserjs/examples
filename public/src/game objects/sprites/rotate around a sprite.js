26c510424aa996890bf29242d7f73978ed630b74
hades <tsunflowertea@gmail.com>
2021-01-21 15:54:57 +1300

    changed to es6

--- public/src/game objects/sprites/rotate around a sprite.js
+++ public/src/game objects/sprites/rotate around a sprite.js
@@ -3,8 +3,14 @@ class Example extends Phaser.Scene
     constructor ()
     {
         super();
+        this.ball1 = undefined;
+        this.ball2 = undefined;
+        this.ball3 = undefined;
+        this.angle1 = 0;
+        this.distance1 = 200;
+        this.angle2 = 0;
+        this.distance2 = 80;
     }
-
     preload ()
     {
       this.load.image('ballRed', 'assets/demoscene/ball.png');
@@ -14,33 +20,23 @@ class Example extends Phaser.Scene
 
     create ()
     {
-      ball1 = this.add.sprite(400, 300, 'ballRed');
-      ball2 = this.add.sprite(400, 300, 'ballBlue');
-      ball3 = this.add.sprite(400, 300, 'ballSmall');
+      this.ball1 = this.add.sprite(400, 300, 'ballRed');
+      this.ball2 = this.add.sprite(400, 300, 'ballBlue');
+      this.ball3 = this.add.sprite(400, 300, 'ballSmall');
     }
 
     update()
     {
-      ball2.setPosition(400, 300);
-      ball3.setPosition(400, 300);
+      this.ball2.setPosition(400, 300);
+      this.ball3.setPosition(400, 300);
 
-      Phaser.Math.RotateAroundDistance(ball2, ball1.x, ball1.y, angle1, distance1);
-      Phaser.Math.RotateAroundDistance(ball3, ball2.x, ball2.y, angle2, distance2);
+      Phaser.Math.RotateAroundDistance(this.ball2, this.ball1.x, this.ball1.y, this.angle1, this.distance1);
+      Phaser.Math.RotateAroundDistance(this.ball3, this.ball2.x, this.ball2.y, this.angle2, this.distance2);
 
-      angle1 = Phaser.Math.Angle.Wrap(angle1 + 0.02);
-      angle2 = Phaser.Math.Angle.Wrap(angle2 + 0.03);
+      this.angle1 = Phaser.Math.Angle.Wrap(this.angle1 + 0.02);
+      this.angle2 = Phaser.Math.Angle.Wrap(this.angle2 + 0.03);
     }
 }
-
-let ball1;
-let ball2;
-let ball3;
-
-let angle1 = 0;
-let distance1 = 200;
-
-let angle2 = 0;
-let distance2 = 80;
 
 const config = {
   type: Phaser.AUTO,
@@ -51,18 +47,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-}
-
-function update ()
-{
-    //  Reset the position so the rotation gets the correct _new_ position
-    ball2.setPosition(400, 300);
-    ball3.setPosition(400, 300);
-
-    Phaser.Math.RotateAroundDistance(ball2, ball1.x, ball1.y, angle1, distance1);
-    Phaser.Math.RotateAroundDistance(ball3, ball2.x, ball2.y, angle2, distance2);
-
-    angle1 = Phaser.Math.Angle.Wrap(angle1 + 0.02);
-    angle2 = Phaser.Math.Angle.Wrap(angle2 + 0.03);
-}

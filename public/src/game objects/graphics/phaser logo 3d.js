3bfee01e5047bf2cc1781255e5df6a27f1110904
rubycampbell-stokes <rcampbellstokes@gmail.com>
2021-01-21 14:26:04 +1300

    fixed let error and updated to ES6 format.

--- public/src/game objects/graphics/phaser logo 3d.js
+++ public/src/game objects/graphics/phaser logo 3d.js
@@ -2,62 +2,65 @@
 // This will give you an undefined error.
 // However, TweenMax will run if you launch.
 
-let graphics;
-let s;
-let r;
-let colors;
-let go = false;
-let temp = { t: 0 };
-let logos;
-let thickness;
-let alpha;
-
 class PhaserLogo3D extends Phaser.Scene
 {
+    constructor ()
+    {
+        super();
+        this.graphics = undefined;
+        this.s = undefined;
+        this.r = undefined;
+        this.colors = undefined;
+        this.go = false;
+        this.temp = { t: 0 };
+        this.logos = undefined;
+        this.thickness = undefined;
+        this.alpha = undefined;
+        this.hsv = Phaser.Display.Color.HSVColorWheel();
+
+    }
 
     create ()
     {
-        graphics = this.add.graphics();
+        this.graphics = this.add.graphics();
 
-        let hsv = Phaser.Display.Color.HSVColorWheel();
+        this.colors = [];
 
-        colors = [];
-
-        for (let i = 0; i < hsv.length; i += 4)
+        for (let i = 0; i < this.hsv.length; i += 4)
         {
-            colors.push(hsv[i].color);
+            this.colors.push(this.hsv[i].color);
         }
 
         // colors = [ 0x650a05, 0xa00d05, 0xcd1106, 0xf53719, 0xf25520, 0xf26f21, 0xf49214, 0xf6a90a, 0xfad400, 0xfef700, 0xffff45, 0xffffc4, 0xffffff ];
 
-        r = 0;
-        s = [];
-        logos = 13;
-        thickness = 10;
-        alpha = 1;
+        this.r = 0;
+        this.s = [];
+        this.logos = 13;
+        this.thickness = 10;
+        this.alpha = 1;
 
-        for (let i = 0; i < logos; i++)
+        for (let i = 0; i < this.logos; i++)
         {
-            s.push(0);
+            this.s.push(0);
         }
 
         TweenMax.delayedCall(5, function () {
 
-            r = 0;
-            go = true;
+            this.r = 0;
+            this.go = true;
 
         });
 
         TweenMax.delayedCall(14, function () {
 
-            TweenMax.to(temp, 0.1, {
+            TweenMax.to(this.temp, 0.1, {
 
                 t: 1,
 
                 repeat: -1,
 
                 onRepeat: function () {
-                    Phaser.Utils.Array.RotateRight(colors);
+                    Phaser.Utils.Array.RotateRight(this.colors);
                 }
 
             });
@@ -67,13 +70,13 @@ class PhaserLogo3D extends Phaser.Scene
 
     update ()
     {
-        graphics.clear();
+        this.graphics.clear();
 
-        r += 0.01;
+        this.r += 0.01;
 
-        let scale = 0.9 - (logos * 0.01);
+        let scale = 0.9 - (this.logos * 0.01);
 
-        for (let i = 0; i < logos; i++)
+        for (let i = 0; i < this.logos; i++)
         {
             //  3D slant :)
             // drawLogo(colors[i], -380 + (i * 2), -100 + (i * 2), scale, s[i]);
@@ -82,11 +85,11 @@ class PhaserLogo3D extends Phaser.Scene
 
             // drawLogo(colors[i], -380, -100 + ((i * 2) * Math.sin(r * 2)), scale, s[i]);
 
-            drawLogo(colors[i], -380 + ((i * 2) * Math.sin(r * 2)), -100 + ((i * 2) * Math.cos(r * 2)), scale, s[i]);
+            this.drawLogo(this.colors[i], -380 + ((i * 2) * Math.sin(this.r * 2)), -100 + ((i * 2) * Math.cos(this.r * 2)), scale, this.s[i]);
 
-            if (go)
+            if (this.go)
             {
-                s[i] = Math.sin(r / 2);
+                this.s[i] = Math.sin(this.r / 2);
             }
 
             // s[i] = Math.sin(r * i) / 16;
@@ -108,7 +111,7 @@ class PhaserLogo3D extends Phaser.Scene
         // let thickness = 10;
         // let alpha = 1;
 
-        graphics.lineStyle(thickness, color, alpha);
+        this.graphics.lineStyle(this.thickness, color, this.alpha);
 
         let w = 100;
         let h = 200;
@@ -118,77 +121,77 @@ class PhaserLogo3D extends Phaser.Scene
         let bot = y + 200;
         let s = 30;
 
-        graphics.save();
-        graphics.translateCanvas(400, 300);
-        graphics.scaleCanvas(scale, scale);
-        graphics.rotateCanvas(rot);
+        this.graphics.save();
+        this.graphics.translateCanvas(400, 300);
+        this.graphics.scaleCanvas(scale, scale);
+        this.graphics.rotateCanvas(rot);
 
-        graphics.beginPath();
+        this.graphics.beginPath();
 
         //  P
 
-        graphics.moveTo(x, top);
-        graphics.lineTo(x + w, top);
-        graphics.lineTo(x + w, mid);
-        graphics.lineTo(x, mid);
-        graphics.lineTo(x, bot);
+        this.graphics.moveTo(x, top);
+        this.graphics.lineTo(x + w, top);
+        this.graphics.lineTo(x + w, mid);
+        this.graphics.lineTo(x, mid);
+        this.graphics.lineTo(x, bot);
 
         //  H
 
         x += w + s;
 
-        graphics.moveTo(x, top);
-        graphics.lineTo(x, bot);
-        graphics.moveTo(x, mid);
-        graphics.lineTo(x + w, mid);
-        graphics.moveTo(x + w, top);
-        graphics.lineTo(x + w, bot);
+        this.graphics.moveTo(x, top);
+        this.graphics.lineTo(x, bot);
+        this.graphics.moveTo(x, mid);
+        this.graphics.lineTo(x + w, mid);
+        this.graphics.moveTo(x + w, top);
+        this.graphics.lineTo(x + w, bot);
 
         //  A
 
         x += w + s;
 
-        graphics.moveTo(x, bot);
-        graphics.lineTo(x + (w * 0.75), top);
-        graphics.lineTo(x + (w * 0.75) + (w * 0.75), bot);
+        this.graphics.moveTo(x, bot);
+        this.graphics.lineTo(x + (w * 0.75), top);
+        this.graphics.lineTo(x + (w * 0.75) + (w * 0.75), bot);
 
         //  S
 
         x += ((w * 0.75) * 2) + s;
 
-        graphics.moveTo(x + w, top);
-        graphics.lineTo(x, top);
-        graphics.lineTo(x, mid);
-        graphics.lineTo(x + w, mid);
-        graphics.lineTo(x + w, bot);
-        graphics.lineTo(x, bot);
+        this.graphics.moveTo(x + w, top);
+        this.graphics.lineTo(x, top);
+        this.graphics.lineTo(x, mid);
+        this.graphics.lineTo(x + w, mid);
+        this.graphics.lineTo(x + w, bot);
+        this.graphics.lineTo(x, bot);
 
         //  E
 
         x += w + s;
 
-        graphics.moveTo(x + w, top);
-        graphics.lineTo(x, top);
-        graphics.lineTo(x, bot);
-        graphics.lineTo(x + w, bot);
-        graphics.moveTo(x, mid);
-        graphics.lineTo(x + w, mid);
+        this.graphics.moveTo(x + w, top);
+        this.graphics.lineTo(x, top);
+        this.graphics.lineTo(x, bot);
+        this.graphics.lineTo(x + w, bot);
+        this.graphics.moveTo(x, mid);
+        this.graphics.lineTo(x + w, mid);
 
         //  R
 
         x += w + s;
 
-        graphics.moveTo(x, top);
-        graphics.lineTo(x + w, top);
-        graphics.lineTo(x + w, mid);
-        graphics.lineTo(x, mid);
-        graphics.lineTo(x, bot);
-        graphics.moveTo(x, mid);
-        graphics.lineTo(x + w, bot);
+        this.graphics.moveTo(x, top);
+        this.graphics.lineTo(x + w, top);
+        this.graphics.lineTo(x + w, mid);
+        this.graphics.lineTo(x, mid);
+        this.graphics.lineTo(x, bot);
+        this.graphics.moveTo(x, mid);
+        this.graphics.lineTo(x + w, bot);
 
-        graphics.strokePath();
+        this.graphics.strokePath();
 
-        graphics.restore();
+        this.graphics.restore();
     }
 
 }

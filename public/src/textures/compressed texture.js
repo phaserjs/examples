234ca551c94d02156afc539587860c6af89f73ac
Richard Davey <rich@photonstorm.com>
2021-10-06 18:48:45 +0100

    Compressed atlas tests

--- public/src/textures/compressed texture.js
+++ public/src/textures/compressed texture.js
@@ -1,61 +1,36 @@
+class Demo extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.texture('labs', {
+            'ASTC': 'assets/compressed/labs-astc-4x4.pvr',
+            'ETC1': 'assets/compressed/labs-etc1.pvr',
+            'PVRTC': 'assets/compressed/labs-pvrtc-4bpp-rgba-srgb.pvr',
+            'S3TC': 'assets/compressed/labs-bc3.pvr',
+            'IMG': 'assets/compressed/labs.png'
+        });
+    }
+
+    create ()
+    {
+        const logo = this.add.image(400, 300, 'labs');
+
+        this.add.text(400, 570, logo.frame.source.compressionAlgorithm).setOrigin(0.5, 0);
+    }
+}
+
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    backgroundColor: '#000000',
-    scene: {
-        preload: preload,
-        create: create
-    }
+    backgroundColor: '#2d2d6d',
+    scene: Demo
 };
 
 const game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.texture('labs', {
-        'ASTC': 'assets/compressed/labs-astc-4x4.pvr',
-        'ETC1': 'assets/compressed/labs-etc1.pvr',
-        'PVRTC': 'assets/compressed/labs-pvrtc-4bpp-rgba-srgb.pvr',
-        'S3TC': 'assets/compressed/labs-bc3-srgb.pvr',
-        'IMG': 'assets/compressed/labs.png'
-    });
-}
-
-function create ()
-{
-    console.log(this.game.renderer);
-
-    // const png = this.add.image(0, 0, 'mechPNG').setScale(0.54).setOrigin(0);
-    // const avif = this.add.image(0, 0, 'mechAVIF').setScale(0.54).setOrigin(0);
-
-    // this.add.text(32, 604, '^ PNG - 2,953 KB');
-    // this.add.text(992, 604, '^ AVIF - 135 KB').setOrigin(1, 0);
-
-    // const debug = this.add.graphics();
-
-    // const drawMarker = (x) => {
-
-    //     debug.clear();
-    //     debug.fillStyle(0x00ff00);
-    //     debug.fillRect(x - 1, 0, 3, 583);
-    //     debug.fillCircle(x, 300, 12);
-    //     debug.fillStyle(0x000000);
-    //     debug.fillTriangle(x - 2, 300 - 6, x - 2, 300 + 6, x - 2 - 6, 300);
-    //     debug.fillTriangle(x + 2, 300 - 6, x + 2, 300 + 6, x + 2 + 6, 300);
-
-    // };
-
-    // drawMarker(512);
-
-    // avif.setCrop(512 * (1 / png.scaleX), 0, 1920, 1080);
-
-    // this.input.on('pointermove', (pointer) => {
-
-    //     drawMarker(pointer.x);
-
-    //     avif.setCrop(pointer.x * (1 / png.scaleX), 0, 1920, 1080);
-
-    // });
-}

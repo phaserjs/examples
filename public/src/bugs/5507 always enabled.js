bc84ba30d1700994b40493feb15af35b7a2d4fe3
Richard Davey <rich@photonstorm.com>
2022-01-31 20:37:15 +0000

    Update 5507 always enabled.js

--- public/src/bugs/5507 always enabled.js
+++ public/src/bugs/5507 always enabled.js
@@ -12,6 +12,9 @@ var game = new Phaser.Game(config);
 
 function create()
 {
+    //  Test 1
+
+    /*
     var invisibleRect = this.add.rectangle(400, 300, 300, 300, 0x00ff00);
     var rect = this.add.rectangle(400, 300, 256, 256, 0x0000ff);
     var text = this.add.text(128, 128, "0").setFontSize(28);
@@ -33,4 +36,34 @@ function create()
 
     // invisibleRect.setDepth(0);
     rect.setDepth(-1);
+    */
+
+    //  Test 2
+    var invisibleRect = this.add.rectangle(400, 300, 300, 300, 0x00ff00);
+    var rect = this.add.rectangle(400, 300, 256, 256, 0x0000ff).setAlpha(0.5);
+    var rect2 = this.add.rectangle(350, 300, 256, 256, 0xff0000).setAlpha(0.5);
+    var text = this.add.text(128, 128, "0").setFontSize(28);
+    var count = 0;
+
+    invisibleRect.name = 'invis';
+    invisibleRect.setInteractive();
+    invisibleRect.input.alwaysEnabled = true;
+
+    invisibleRect.on('pointerdown', function () {
+        count++;
+        text.text = count.toString();
+    });
+
+    rect.name = 'rect';
+    rect.setInteractive();
+
+    rect2.name = "rect2"
+    rect2.setInteractive();
+
+    invisibleRect.alpha = 0;
+    //invisibleRect.alpha = 0.3;
+
+    invisibleRect.setDepth(3);
+    rect.setDepth(2);
+    rect2.setDepth(1);
 }

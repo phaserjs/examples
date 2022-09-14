fe6c997cf787763a383118359ab434b33f4e4956
Richard Davey <rich@photonstorm.com>
2022-09-14 21:02:05 +0100

    Update chained tweens 1.js

--- public/src/tweens/chained tweens 1.js
+++ public/src/tweens/chained tweens 1.js
@@ -14,6 +14,48 @@ class Example extends Phaser.Scene
     {
         const image = this.add.image(100, 100, 'block');
 
+        /*
+        const chain1 = this.tweens.create({
+            targets: image,
+            tweens: [
+                {
+                    x: 700,
+                },
+                {
+                    y: 500,
+                },
+                {
+                    x: 100,
+                },
+                {
+                    y: 100,
+                }
+            ]
+        });
+
+        const chain2 = this.tweens.create({
+            targets: image,
+            tweens: [
+                {
+                    y: 500,
+                },
+                {
+                    x: 700,
+                },
+                {
+                    y: 100,
+                },
+                {
+                    x: 100,
+                }
+            ]
+        });
+
+        chain1.add(chain2);
+
+        const chain = this.tweens.add(chain1);
+        */
+
         const chain = this.tweens.chain({
             targets: image,
             tweens: [
@@ -31,7 +73,7 @@ class Example extends Phaser.Scene
                 }
             ],
             // paused: true,
-            delay: 4000,
+            // delay: 4000,
             loop: 2,
             loopDelay: 500
         });

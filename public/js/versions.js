6f4f0a8475ad64edb26f4f8ba54e125e8cea0799
Richard Davey <rich@photonstorm.com>
2022-10-26 17:09:06 +0100

    3.60 Beta 13

--- public/js/versions.js
+++ public/js/versions.js
@@ -1,5 +1,6 @@
 var versions = [
     { val : 'dev', text: 'Dev Build' },
+    { val : '3.60.0-beta.13', text: '3.60.0 Beta 13' },
     { val : '3.60.0-beta.12', text: '3.60.0 Beta 12' },
     { val : '3.60.0-beta.11', text: '3.60.0 Beta 11' },
     { val : '3.60.0-beta.10', text: '3.60.0 Beta 10' },

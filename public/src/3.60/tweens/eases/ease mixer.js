f71b458f77c171aa0e6a0807099fcf30129f06c0
Richard Davey <rich@photonstorm.com>
2022-11-08 13:19:12 +0000

    Update ease mixer.js

--- public/src/3.60/tweens/eases/ease mixer.js
+++ public/src/3.60/tweens/eases/ease mixer.js
@@ -16,25 +16,23 @@ class Example extends Phaser.Scene
 
         this.add.text(400, 28, 'Ease Mixer').setColor('#00ff00').setFontSize(32).setShadow(2, 2).setOrigin(0.5, 0);
 
-        const list1 = this.createSelectList('x', 300, 80);
-        const list2 = this.createSelectList('y', 450, 80);
-        const button = this.createButton(550, 80);
+        const list1 = this.createSelectList('x', 250, 80);
+        const list2 = this.createSelectList('y', 425, 80);
+        const button = this.createButton(580, 80);
 
-        list1.addListener('change');
-        list2.addListener('change');
         button.addListener('click');
 
         let easeX = 'Linear';
         let easeY = 'Linear';
 
-        list1.on('change', event => {
+        list1.addEventListener('change', event => {
 
             easeX = event.target.selectedOptions[0].value;
             graphEase();
 
         });
 
-        list2.on('change', event => {
+        list2.addEventListener('change', event => {
 
             easeY = event.target.selectedOptions[0].value;
             graphEase();
@@ -49,8 +47,14 @@ class Example extends Phaser.Scene
 
         let tween;
 
+        //  Draw the x/y bounds
+        this.add.rectangle(150, 0, 1, 600, 0xe84dff).setOrigin(0, 0);
+        this.add.rectangle(650, 0, 1, 600, 0xe84dff).setOrigin(0, 0);
+        this.add.rectangle(0, 150, 800, 1, 0xe84dff).setOrigin(0, 0);
+        this.add.rectangle(0, 500, 800, 1, 0xe84dff).setOrigin(0, 0);
+
         const graph = this.add.graphics();
-        const rect = this.add.rectangle(100, 500, 2, 2, 0x00ff00);
+        const rect = this.add.rectangle(150, 500, 2, 2, 0x00ff00);
         const rt = this.add.renderTexture(400, 300, 800, 600);
 
         const graphEase = () => {
@@ -62,16 +66,17 @@ class Example extends Phaser.Scene
 
             rt.clear();
 
+            rect.setPosition(150, 500);
+
             graph.clear();
             graph.lineStyle(3, 0x00ff00);
             graph.beginPath();
-
-            rect.setPosition(100, 500);
+            graph.moveTo(rect.x, rect.y);
 
             tween = this.tweens.add({
                 targets: rect,
-                x: { value: 600, ease: easeX },
-                y: { value: 200, ease: easeY },
+                x: { value: 650, ease: easeX },
+                y: { value: 150, ease: easeY },
                 duration: 2000,
                 onUpdate: (tween, target, key) => {
                     if (key === 'x')
@@ -81,6 +86,7 @@ class Example extends Phaser.Scene
                     }
                 },
                 onComplete: () => {
+                    graph.lineTo(rect.x, rect.y);
                     graph.stroke();
                 }
             });
@@ -120,6 +126,8 @@ class Example extends Phaser.Scene
             'Bounce.inOut'
         ];
 
+        const div = document.createElement('div');
+
         const list = document.createElement('select');
 
         list.id = id;
@@ -135,7 +143,18 @@ class Example extends Phaser.Scene
 
         });
 
-        return this.add.dom(x, y, list);
+        const label = document.createElement('label');
+
+        label.for = id;
+        label.innerText = id;
+        label.style = 'color: #00ff00; font: 24px Courier; padding-right: 10px';
+
+        div.appendChild(label);
+        div.appendChild(list);
+
+        this.add.dom(x, y, div);
+
+        return list;
     }
 
     createButton (x, y)

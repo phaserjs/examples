c21724eb864ee7ec802a5633ac3e41cf06305276
Richard Davey <rich@photonstorm.com>
2022-12-06 22:48:46 +0000

    nine slice demo

--- public/src/3.60/game objects/nine slice/create nine slice.js
+++ public/src/3.60/game objects/nine slice/create nine slice.js
@@ -9,21 +9,29 @@ class Example extends Phaser.Scene
     {
         this.load.image('test', 'assets/sprites/128x128.png');
         this.load.image('test2', 'assets/sprites/button-bg.png');
+        this.load.image('box', 'assets/tests/blue-box.png');
     }
 
     create ()
     {
-        // this.add.image(400, 170, 'test');
-
-        window.nine = this.add.nineslice({ left: 50, right: 50, width: 700 }, 400, 300, 'test2');
+        window.nine = this.add.nineslice({ left: 16, right: 16, top: 32, bottom: 16, width: 600, height: 400 }, 400, 300, 'box');
 
         this.tweens.add({
             targets: window.nine,
-            width: 300,
+            width: 100,
             duration: 3000,
             ease: 'sine.inout',
             yoyo: true,
-            repeat: -1
+            repeat: -1,
+        });
+
+        this.tweens.add({
+            targets: window.nine,
+            height: 100,
+            duration: 6000,
+            ease: 'sine.inout',
+            yoyo: true,
+            repeat: -1,
         });
     }
 }
@@ -32,7 +40,7 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#2d2d2d',
+    backgroundColor: '#000000',
     parent: 'phaser-example',
     scene: Example
 };

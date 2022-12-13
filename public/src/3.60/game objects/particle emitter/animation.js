ef2786e15cfbaaa33bfd4f796e363ecaf6d6275b
Richard Davey <rich@photonstorm.com>
2022-12-13 18:51:43 +0000

    particle animation test

---
+++ public/src/3.60/game objects/particle emitter/animation.js
@@ -0,0 +1,37 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.atlas('flares', 'assets/particles/flares.png', 'assets/particles/flares.json');
+        this.load.atlas('gems', 'assets/tests/columns/gems.png', 'assets/tests/columns/gems.json');
+    }
+
+    create ()
+    {
+        this.anims.create({ key: 'diamond', frames: this.anims.generateFrameNames('gems', { prefix: 'diamond_', end: 15, zeroPad: 4 }), repeat: -1 });
+        this.anims.create({ key: 'prism', frames: this.anims.generateFrameNames('gems', { prefix: 'prism_', end: 6, zeroPad: 4 }), repeat: -1 });
+        this.anims.create({ key: 'ruby', frames: this.anims.generateFrameNames('gems', { prefix: 'ruby_', end: 6, zeroPad: 4 }), repeat: -1 });
+        this.anims.create({ key: 'square', frames: this.anims.generateFrameNames('gems', { prefix: 'square_', end: 14, zeroPad: 4 }), repeat: -1 });
+
+        const particles = this.add.particles('gems');
+
+        particles.createEmitter({
+            x: 64,
+            y: { start: 500, end: 100, steps: 8 },
+            lifespan: 4000,
+            accelerationX: 200,
+            frequency: 100
+        });
+    }
+}
+
+const config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

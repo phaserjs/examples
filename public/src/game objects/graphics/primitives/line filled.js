5bcf06c9c9bb2a680b5cef02859f8824e562cc82
Francisco Pereira Alvarado <gammafp@gmail.com>
2023-01-10 16:58:20 +0100

    fix and update examples.
    line filled
    rectangle filled texture
    rectangle stroked.js

--- public/src/game objects/graphics/primitives/line filled.js
+++ public/src/game objects/graphics/primitives/line filled.js
@@ -1,28 +1,19 @@
-var config = {
+class Example extends Phaser.Scene
+{
+    create ()
+    {
+        const graphics = this.add.graphics();
+        graphics.lineStyle(128, 0x00ff00, 1);
+        graphics.lineBetween(100, 100, 600, 500);
+    }
+}
+
+const config = {
     width: 800,
     height: 600,
     type: Phaser.WEBGL,
     parent: 'phaser-example',
-    scene: {
-        preload: preload,
-        create: create
-    }
+    scene: Example
 };
 
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('metal', 'assets/textures/alien-metal.jpg');
-}
-
-function create ()
-{
-    var graphics = this.add.graphics();
-
-    graphics.setTexture('metal');
-
-    graphics.lineStyle(128, 0x00ff00, 1);
-
-    graphics.lineBetween(100, 100, 600, 500);
-}
+const game = new Phaser.Game(config);

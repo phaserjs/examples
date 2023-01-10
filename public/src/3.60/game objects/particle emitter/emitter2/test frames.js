a700f359b8f5ed1fbfc7dee8793e2f869456dbc7
Richard Davey <rich@photonstorm.com>
2023-01-10 18:53:00 +0000

    emitter tests

---
+++ public/src/3.60/game objects/particle emitter/emitter2/test frames.js
@@ -0,0 +1,53 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.atlas('bubbles', 'assets/particles/bubbles.png', 'assets/particles/bubbles.json');
+    }
+
+    create ()
+    {
+        const emitter = this.add.particles2('bubbles', {
+            frame: [ 'bluebubble', 'redbubble', 'greenbubble', 'silverbubble' ],
+            x: 100,
+            y: 100,
+            lifespan: 5000,
+            angle: { min: -30, max: 30 },
+            speed: 150,
+            frequency: 200
+        });
+
+        // this.add.particles2('bubbles', {
+        //     frame: 'bluebubble',
+        //     x: 300,
+        //     y: 100,
+        //     lifespan: 5000,
+        //     angle: { min: -30, max: 30 },
+        //     speed: 150,
+        //     frequency: 200
+        // });
+
+        this.add.particles2('bubbles', {
+            frame: { frames: [ 'bluebubble', 'redbubble' ], cycle: true, quantity: 4 },
+            x: 300,
+            y: 400,
+            lifespan: 5000,
+            angle: { min: -30, max: 30 },
+            speed: 150,
+            frequency: 200
+        });
+
+        window.emma = emitter;
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

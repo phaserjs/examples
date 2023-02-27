f66e564b7919628fcd071910e6c10cb262037603
Richard Davey <rich@photonstorm.com>
2023-02-27 18:14:20 +0000

    Updated example

--- public/src/3.60/fx/displacement fx.js
+++ public/src/3.60/fx/displacement fx.js
@@ -2,29 +2,20 @@ class Example extends Phaser.Scene
 {
     preload ()
     {
-        // this.load.image('pic', 'assets/pics/taikodrummaster.jpg');
-        // this.load.image('pic', 'assets/pics/swirl1.jpg');
-        this.load.image('pic', 'assets/pics/skull-and-bones.jpg');
-        this.load.image('distort1', 'assets/textures/distortion.png');
-        this.load.image('distort2', 'assets/tests/noisesmall.png');
-        // this.load.image('distort3', 'assets/textures/distortion3.png');
-        this.load.image('distort3', 'assets/textures/distortion5.png');
+        this.load.image('pic', 'assets/pics/taikodrummaster.jpg');
+        this.load.image('distort3', 'assets/tests/noisesmall.png');
     }
 
     create ()
     {
         const pic = this.add.image(400, 300, 'pic');
 
-        const fx = pic.preFX.addDisplacement('distort3', -0.3, 0);
-
-        pic.preFX.addCircle(8, 0x2d2d2d);
-
-        // const fx = pic.preFX.addDisplacement('distort3', 0, -1);
+        const fx = pic.preFX.addDisplacement('distort3', -0.03, -0.03);
 
         this.tweens.add({
             targets: fx,
-            x: 0.3,
-            // y: 0.1,
+            x: 0.03,
+            y: 0.03,
             yoyo: true,
             loop: -1,
             duration: 2000,

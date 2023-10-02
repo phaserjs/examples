9dcbb19cc63451e0d29415bd67890d49ad6a6459
Richard Davey <rich@photonstorm.com>
2023-10-02 18:25:59 +0100

    New tests

--- public/src/bugs/6604 line width.js
+++ public/src/bugs/6604 line width.js
@@ -3,15 +3,16 @@ class Example extends Phaser.Scene
 
     create ()
     {
-        const line = this.add.line(100, 100, 200, 300, 500, 200, 0xff0000);
-        line.setLineWidth(40);
+        const line = this.add.line(400, 300, 0, 0, 256, 0, 0xff0000);
+
+        line.setLineWidth(32, 64);
     }
 
 }
 
 const config = {
-    // type: Phaser.CANVAS,
-    type: Phaser.WEBGL,
+    type: Phaser.CANVAS,
+    // type: Phaser.WEBGL,
     width: 800,
     height: 600,
     backgroundColor: '#2d2d2d',
---
+++ public/src/physics/arcade/body push blocks.js
@@ -0,0 +1,120 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.image('block', 'assets/sprites/crate32.png');
+        this.load.image('ice', 'assets/sprites/block-ice.png');
+        this.load.image('be', 'assets/sprites/beball1.png');
+    }
+
+    create ()
+    {
+        this.player = this.physics.add.sprite(400, 300, 'be');
+
+        this.player.setCollideWorldBounds(true);
+        this.player.setPushable(false);
+
+        const boxes = [];
+
+        for (let i = 0; i < 16; i++)
+        {
+            const x = Phaser.Math.Between(0, 800);
+            const y = Phaser.Math.Between(0, 600);
+
+            const box = this.physics.add.image(x, y, 'block');
+
+            box.setCollideWorldBounds(true);
+            box.setDrag(10000);
+
+            boxes.push(box);
+        }
+
+        for (let i = 0; i < 16; i++)
+        {
+            const x = Phaser.Math.Between(0, 800);
+            const y = Phaser.Math.Between(0, 600);
+
+            const box = this.physics.add.image(x, y, 'ice').setScale(0.125);
+
+            box.setCollideWorldBounds(true);
+            box.setDrag(100);
+            box.setBounce(1);
+
+            boxes.push(box);
+        }
+
+        let playerIsNPC = false;
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.player, boxes, null, (player, box) => {
+
+            if (playerIsNPC)
+            {
+                box.setPushable(false);
+            }
+            else
+            {
+                box.setPushable(true);
+            }
+
+            return true;
+
+        });
+
+        this.input.on('pointerdown', () => {
+
+            playerIsNPC = !playerIsNPC;
+
+            if (playerIsNPC)
+            {
+                this.player.setTint(0xff0000);
+            }
+            else
+            {
+                this.player.clearTint();
+            }
+
+        });
+    }
+
+    update ()
+    {
+        this.player.setVelocity(0, 0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-200);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(200);
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.player.setVelocityY(-200);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.setVelocityY(200);
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

e1921eca66b94dcc84f5767ddeceb898f125141e
Yash Mittal <yashmittalmm@gmail.com>
2024-01-23 01:57:38 +0530

    Fixed the typo in tween timescale

--- public/src/animation/tween timescale.js
+++ public/src/animation/tween timescale.js
@@ -39,7 +39,7 @@ class Example extends Phaser.Scene {
             repeat: -1,
             repeatDelay: 1000,
             hold: 1000,
-            duraton: 3000
+            duration: 12000
         });
     }
 

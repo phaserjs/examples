70d100a1cb54ba60e8285c5f47b3623810498fc6
Robert Kowalski <rgkmg25@gmail.com>
2024-02-08 15:39:44 -0500

    Add mesh pointer input.js

---
+++ public/src/game objects/mesh/mesh pointer input.js
@@ -0,0 +1,39 @@
+class Example extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('bg', 'assets/skies/wtf.png');
+        this.load.obj('skull', 'assets/obj/skull.obj');
+        this.load.image('powerups', 'assets/obj/powerups.png');
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'bg');
+
+        const mesh = this.add.mesh(400, 300, 'powerups');
+
+        mesh.addVerticesFromObj('skull', 0.1);
+
+        mesh.panZ(7);
+        mesh.modelRotation.y += 0.5;
+
+        mesh.setInteractive({ useHandCursor: true });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000022',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+let game = new Phaser.Game(config);

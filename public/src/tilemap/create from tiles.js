64e489faf7449ccb905f7f9142df9cc9f2104f92
Richard Davey <rich@photonstorm.com>
2024-02-20 21:40:29 +0000

    Fixed create from tiles example

--- public/src/tilemap/create from tiles.js
+++ public/src/tilemap/create from tiles.js
@@ -2,7 +2,7 @@ class Example extends Phaser.Scene
 {
     preload ()
     {
-        this.load.image('fantasy-tiles', 'assets/tilemaps/tiles/fantasy-tiles.png');
+        this.load.spritesheet('fantasy-tiles', 'assets/tilemaps/tiles/fantasy-tiles.png', { frameWidth: 64 });
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/maps/create from tiles.json');
     }
 
@@ -14,10 +14,23 @@ class Example extends Phaser.Scene
 
         const layer = map.createLayer('Tile Layer 1', tiles);
 
-        //  52 = ? block
-        //  49 = chest
-        // map.createFromTiles();
+        //  Because we have loaded the tileset as a spritesheet, we can set the
+        //  'useSpriteSheet' property to set the texture and frame when creating the sprites.
+        //  If you have loaded the tileset as an image, then this will not work and you
+        //  should set the texture and frame manually after creating the sprites.
 
+        const sprites = map.createFromTiles([ 53, 50 ], -1, { useSpriteSheet: true });
+
+        //  Bounce the sprites just to show they're no longer tiles:
+
+        this.tweens.add({
+            targets: sprites,
+            y: '-=32',
+            duration: 1000,
+            ease: 'Sine.easeInOut',
+            yoyo: true,
+            repeat: -1
+        });
     }
 }
 

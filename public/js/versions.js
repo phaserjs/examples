d2a54ba8eb9557cebe91b86ce919a6b830726531
Richard Davey <rich@photonstorm.com>
2024-02-27 16:00:10 +0000

    3.80.1 release

--- public/js/versions.js
+++ public/js/versions.js
@@ -1,5 +1,6 @@
 var versions = [
     { val : 'dev', text: 'Dev Build' },
+    { val : '3.80.1', text: '3.80.1' },
     { val : '3.80.0', text: '3.80.0' },
     { val : '3.70.0', text: '3.70.0' },
     // { val : '3.61.0-beta.4', text: '3.61.0 Beta 4' },

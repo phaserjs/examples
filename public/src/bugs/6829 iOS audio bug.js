6ac870d4373da4ba9f9d04c304f1d9cce8403156
Zeke Chan <zeke.chan.tk@gmail.com>
2024-06-26 17:53:19 +0800

    Create 6829 iOS audio bug.js

---
+++ public/src/bugs/6829 iOS audio bug.js
@@ -0,0 +1,35 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.audio('theme', [
+            'assets/audio/oedipus_wizball_highscore.ogg',
+            'assets/audio/oedipus_wizball_highscore.mp3'
+        ]);
+
+        this.load.image('wizball', 'assets/sprites/wizball.png');
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'wizball').setScale(4);
+
+        const music = this.sound.add('theme');
+
+        console.log(music);
+        console.log('play?', music.play());
+
+        this.sound.pauseOnBlur = true;
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    pixelArt: true,
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

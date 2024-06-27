92aaa3e977fc6c690c3826c18168f1e0747953e5
Zeke Chan <zeke.chan.tk@gmail.com>
2024-06-27 10:11:05 +0800

    Update 6806 dynamic texture.js

--- public/src/bugs/6806 dynamic texture.js
+++ public/src/bugs/6806 dynamic texture.js
@@ -17,8 +17,8 @@ class Example extends Phaser.Scene
             .setScale(scale)
             .setOrigin(0, 0);
 
-        // const glowController = birdie1.preFX.addGlow(0xffff00, 4, 0, false, 0.1, 32);
-        const blurController = birdie1.preFX.addBlur();
+        const glowController = birdie1.preFX.addGlow(0xffff00, 4, 0, false, 0.1, 32);
+        // const blurController = birdie1.preFX.addBlur();
         // const barrelController = birdie1.preFX.addBarrel(2);
         // const bloomController = birdie1.preFX.addBloom(0xffffff, 1, 1, 2, 1.2);
         // const cmController = birdie1.preFX.addColorMatrix().night();
@@ -26,31 +26,25 @@ class Example extends Phaser.Scene
         // const gradientController = birdie1.preFX.addGradient(0x0000ff, 0x00ff00, 0);
         // const pixelateController = birdie1.preFX.addPixelate(5);
         // const shadowController = birdie1.preFX.addShadow(0, 0, 0.006, 2, 0x999999, 10);
-        const vignetteController = birdie1.preFX.addVignette(0.5, 0.5, 0.3, 0.5);
+        // const vignetteController = birdie1.preFX.addVignette(0.5, 0.5, 0.3, 0.5);
         // const shineController = birdie1.postFX.addShine(1, .2, 5);
         // console.log(glowController);
         
-        const texture1 = this.textures.addDynamicTexture("birdie_glow1", 1200, 900); //500, 334
+        const texture1 = this.textures.addDynamicTexture("birdie_glow1", 800, 600); //500, 334
         texture1.draw(birdie1, 0, 0);
-        // texture1.drawFrame('birdie');
 
-        // This is the expected usage and creates a texture as big as the image
-        // const texture2 = this.textures.addDynamicTexture("birdie_glow2", 600, 500);//birdie1.width * scale, birdie1.height * scale);
-        // texture1.drawFrame('birdie_glow1', 400, 300);
-        // texture2.stamp('birdie_glow1', null, 400, 300);//, { scale: 0.2, originX: 1 });
-        // texture2.draw(birdie1, 0, 0);
+        const texture2 = this.textures.addDynamicTexture("birdie_glow2", birdie1.width * scale, birdie1.height * scale);
+        texture2.stamp('birdie_glow1', null, 400, 300);
 
         const clone1 = this.add.sprite(100, 100, "birdie_glow1").setOrigin(0);
         const bounds1 = clone1.getBounds();
         graphics.strokeRectShape(bounds1);
 
+        const clone2 = this.add.sprite(400, 300, "birdie_glow2")
+        const bounds2 = clone2.getBounds();
+        graphics.strokeRectShape(bounds2);
 
-        // const clone2 = this.add.sprite(400, 300, "birdie_glow2")
-        // const bounds2 = clone2.getBounds();
-        // graphics.strokeRectShape(bounds2);
-
-        // birdie1.destroy();
-        // birdie1.setAlpha(0.1);
+        birdie1.destroy();
     }
 }
 

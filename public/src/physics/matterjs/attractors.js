0a2e09729a26e480c7207856b5f19ca98fa2e09a
Zeke Chan <zeke.chan.tk@gmail.com>
2024-07-01 17:00:14 +0800

    Update attractors.js

--- public/src/physics/matterjs/attractors.js
+++ public/src/physics/matterjs/attractors.js
@@ -8,9 +8,6 @@ class Example extends Phaser.Scene
 
     create ()
     {
-        //  You can enable the Attractors plugin either via the game config (see above), or explicitly in code:
-        // this.matter.enableAttractorPlugin();
-
         this.matter.world.setBounds();
 
         this.matter.add.imageStack('alien', null, 0, 500, 50, 2, 0, 0, {
@@ -23,14 +20,12 @@ class Example extends Phaser.Scene
                 type: 'circle',
                 radius: 64
             },
-            plugin: {
-                attractors: [
-                    (bodyA, bodyB) => ({
-                        x: (bodyA.position.x - bodyB.position.x) * 0.000001,
-                        y: (bodyA.position.y - bodyB.position.y) * 0.000001
-                    })
-                ]
-            }
+            attractors: [
+                (bodyA, bodyB) => ({
+                    x: (bodyA.position.x - bodyB.position.x) * 0.000001,
+                    y: (bodyA.position.y - bodyB.position.y) * 0.000001
+                })
+            ]
         });
 
         this.matter.add.mouseSpring();
@@ -49,9 +44,6 @@ const config = {
             gravity: {
                 scale: 0
             },
-            plugins: {
-                attractors: true
-            }
         }
     },
     scene: Example

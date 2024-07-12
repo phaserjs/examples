2d4c0b08777d4270cc7dbe02d52f316f894ccb18
Richard Davey <rich@photonstorm.com>
2024-07-12 15:02:51 +0100

    Create 6812 timeline.js

---
+++ public/src/bugs/6812 timeline.js
@@ -0,0 +1,83 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.atlas('timeline', 'assets/atlas/timeline.png', 'assets/atlas/timeline.json');
+        this.load.image('bg', 'assets/skies/spookysky.jpg');
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'bg');
+
+        this.thread = this.add.graphics();
+
+        this.spider = this.add.sprite(400, 100, 'timeline', 'spider');
+
+        const timeline = this.add.timeline([
+            {
+                at: 2000,
+                tween: {
+                    targets: this.spider,
+                    y: 400,
+                    ease: 'bounce.out',
+                    duration: 5000,
+                    onComplete: () => {
+                        console.log("Tween completed");
+                    }
+                }
+            },
+            {
+                at: 4000,
+                tween: {
+                    targets: this.spider,
+                    x: 200,
+                    angle: 30,
+                    ease: 'sine.out',
+                    duration: 1000,
+                    yoyo: true,
+                    repeat: -1,
+                    repeatDelay: 2000
+                }
+            },
+            {
+                at: 6000,
+                tween: {
+                    targets: this.spider,
+                    x: 600,
+                    angle: -30,
+                    ease: 'sine.out',
+                    duration: 1000,
+                    yoyo: true,
+                    repeat: -1,
+                    repeatDelay: 2000
+                }
+            }
+        ]);
+
+        timeline.play();
+
+        this.time.delayedCall(3000, () => {
+            timeline.destroy();
+            console.log("Timeline destroyed");
+        })
+    }
+
+    update ()
+    {
+        this.thread.clear();
+        this.thread.lineStyle(1, 0xffffff, 0.7);
+        this.thread.lineBetween(400, 0, this.spider.x, this.spider.y);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000000',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

d517a7541b7635d34debe68150f28f0566c2b4dc
Richard Davey <rich@photonstorm.com>
2024-07-15 17:58:46 +0100

    Create 6769 mask resize.js

---
+++ public/src/bugs/6769 mask resize.js
@@ -0,0 +1,34 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.image('ayu', 'assets/pics/ayu.png');
+    }
+
+    create ()
+    {
+        this.game.scale.resize(800, 600);
+
+        const sprite = this.add.sprite(400, 300, 'ayu');
+
+        const rt = this.make.renderTexture({x: 0, y: 0, width: 800, height: 600, add: false}).setOrigin(0.0);
+        const mask = new Phaser.Display.Masks.BitmapMask(this, rt);
+
+        rt.fill(0x000000, 1);
+
+        this.input.on('pointerdown', pointer => {
+            sprite.setMask(mask);
+        })        
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 200,
+    height: 150,
+    backgroundColor: '#0a440a',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

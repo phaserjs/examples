6d46d605b6ef88b1c45f9a5ec9eb8e04ab3c5869
Richard Davey <rich@photonstorm.com>
2024-08-06 22:09:40 +0100

    Create 6874 rp.js

---
+++ public/src/bugs/6874 rp.js
@@ -0,0 +1,26 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.image('bg', 'assets/skies/spookysky.jpg');
+    }
+
+    create ()
+    {
+        const img = this.add.image(400, 300, 'bg');
+
+        img.setScale(1).setDisplaySize(800,600);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    roundPixels: true,
+    backgroundColor: '#020286',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

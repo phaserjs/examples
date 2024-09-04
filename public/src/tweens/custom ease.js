c6235dd8500bf3e37be0426077ff9e977563f225
Zeke Chan <zeke.chan.tk@gmail.com>
2024-09-04 14:55:06 +0800

    Create custom ease.js

---
+++ public/src/tweens/custom ease.js
@@ -0,0 +1,40 @@
+class Example extends Phaser.Scene
+{
+    constructor()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('block', 'assets/sprites/block.png');
+    }
+
+    create ()
+    {
+        var marker = this.add.image(100, 300, 'block').setAlpha(0.3);
+        var image = this.add.image(100, 300, 'block');
+
+        this.tweens.add({
+            targets: image,
+            x: 600,
+            duration: 3000,
+            ease: function (t)
+            {
+                return Math.pow(Math.sin(t * 3), 3);
+            },
+            delay: 1000
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+var game = new Phaser.Game(config);

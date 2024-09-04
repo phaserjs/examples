e748e5f194f6ddad202a82129d67f9c865799a09
Zeke Chan <zeke.chan.tk@gmail.com>
2024-09-04 17:06:30 +0800

    Create dynamic properties.js

---
+++ public/src/tweens/dynamic properties.js
@@ -0,0 +1,55 @@
+class Example extends Phaser.Scene
+{
+    constructor()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('ball', 'assets/sprites/shinyball.png');
+    }
+
+    create ()
+    {
+        var image = this.add.image(100, 300, 'ball');
+
+        var destX = 700;
+
+        var tween = this.tweens.add({
+            targets: image,
+            duration: 500,
+            yoyo: true,
+            repeat: 8,
+            ease: 'Sine.easeInOut',
+
+            x: {
+
+                getEnd: function (target, key, value)
+                {
+                    destX -= 30;
+
+                    return destX;
+                },
+
+                getStart: function (target, key, value)
+                {
+                    return value + 30;
+                }
+
+            }
+
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

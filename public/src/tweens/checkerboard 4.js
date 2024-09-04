3d3317424735cdcb248877fbd7e9c0f82e3f1463
Zeke Chan <zeke.chan.tk@gmail.com>
2024-09-04 14:40:05 +0800

    Update checkerboard 4.js

--- public/src/tweens/checkerboard 4.js
+++ public/src/tweens/checkerboard 4.js
@@ -1,66 +1,70 @@
-var config = {
-    type: Phaser.CANVAS,
-    width: 800,
-    height: 600,
-    backgroundColor: '#080808',
-    parent: 'phaser-example',
-    scene: {
-        preload: preload,
-        create: create
+class Example extends Phaser.Scene
+{
+    constructor()
+    {
+        super();
     }
-};
 
-var game = new Phaser.Game(config);
+    preload ()
+    {
+        this.load.image('bg', 'assets/pics/skull-and-bones.jpg');
+        this.load.image('block', 'assets/sprites/50x50-black.png');
+    }
 
-function preload ()
-{
-    this.load.image('bg', 'assets/pics/skull-and-bones.jpg');
-    this.load.image('block', 'assets/sprites/50x50-black.png');
-}
+    create ()
+    {
+        this.add.image(400, 300, 'bg');
 
-function create ()
-{
-    this.add.image(400, 300, 'bg');
+        const blocks = this.add.group({ key: 'block', repeat: 191 });
 
-    const blocks = this.add.group({ key: 'block', repeat: 191 });
-
-    Phaser.Actions.GridAlign(blocks.getChildren(), {
-        width: 16,
-        cellWidth: 50,
-        cellHeight: 50,
-        x: 0,
-        y: 0
-    });
-
-    var _this = this;
-
-    let i = 0;
-
-    blocks.children.iterate(child => {
-
-        this.tweens.add({
-            targets: child,
-            scaleX: 0,
-            scaleY: 0,
-            alpha: 0,
-            y: '+=64',
-            angle: 180,
-            ease: 'Power3',
-            duration: 1000,
-            yoyo: true,
-            // repeat: -1,
-            delay: 1000 + (i * 100),
-            hold: 1500,
-            // repeatDelay: 2000 - (i * 100),
+        Phaser.Actions.GridAlign(blocks.getChildren(), {
+            width: 16,
+            cellWidth: 50,
+            cellHeight: 50,
+            x: 0,
+            y: 0
         });
 
-        i++;
+        var _this = this;
 
-        //  Change the value 32 for different results
-        if (i % 16 === 0)
+        let i = 0;
+
+        blocks.children.iterate(child =>
         {
-            i = 0;
-        }
 
-    });
+            this.tweens.add({
+                targets: child,
+                scaleX: 0,
+                scaleY: 0,
+                alpha: 0,
+                y: '+=64',
+                angle: 180,
+                ease: 'Power3',
+                duration: 1000,
+                yoyo: true,
+                delay: 1000 + (i * 100),
+                hold: 1500,
+            });
+
+            i++;
+
+            //  Change the value 32 for different results
+            if (i % 16 === 0)
+            {
+                i = 0;
+            }
+
+        });
+    }
 }
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

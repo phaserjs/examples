15e974a1a2a0f9754124f9992c1ce55f9bb60b1a
Zeke Chan <zeke.chan.tk@gmail.com>
2024-09-04 15:08:35 +0800

    Create delay.js

---
+++ public/src/tweens/delay.js
@@ -0,0 +1,37 @@
+class Example extends Phaser.Scene
+{
+    constructor()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('block', 'assets/sprites/block.png');
+    }
+
+    create ()
+    {
+        var marker = this.add.image(100, 100, 'block').setAlpha(0.3);
+        var image = this.add.image(100, 100, 'block');
+
+        var tween = this.tweens.add({
+            targets: image,
+            x: 600,
+            ease: 'Power1',
+            duration: 3000,
+            delay: 2000
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

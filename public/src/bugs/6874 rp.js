2f83ea401bd4adb355181925b1e9f28cf1e400c3
Richard Davey <rich@photonstorm.com>
2024-09-25 16:41:00 +0100

    Update 6874 rp.js

--- public/src/bugs/6874 rp.js
+++ public/src/bugs/6874 rp.js
@@ -2,27 +2,47 @@ class Example extends Phaser.Scene
 {
     preload ()
     {
+        // this.load.image('bg', 'assets/pics/checker.png');
         this.load.image('bg', 'assets/skies/spookysky.jpg');
     }
 
     create ()
     {
-        const img = this.add.image(window.innerWidth/2,window.innerHeight/2, 'bg');
+        // this.add.image(0, 0, 'bg').setOrigin(0, 0).setDisplaySize(256, 256);
 
-        img.setScale(1).setDisplaySize(window.innerWidth,window.innerHeight);
+        // this.add.image(1, 1, 'bg').setOrigin(0, 0).setDisplaySize(256, 256);
+
+        // this.add.image(140, 140, 'bg').setDisplaySize(257, 257);
+        // this.add.image(140+257, 140, 'bg').setDisplaySize(257, 257);
+        // this.add.image(140+257+257, 140, 'bg').setDisplaySize(257, 257);
+        //   this.add.image(201, 300, 'bg').setDisplaySize(101, 101);
+
+        //  49 x 249 (3.80)
+        //  50 x 249 (dev)
+
+        const p1 = this.add.image(100, 100, 'bg').setDisplaySize(101, 101);
+
+        this.add.image(251.32, 103, 'bg').setDisplaySize(101, 101);
+
+        this.add.image(401, 123, 'bg').setOrigin(0, 0).setDisplaySize(101, 101);
+
+
+        // const p2 = this.add.image(201, 100, 'bg').setDisplaySize(101, 101);
+
+        // console.log/(p1.x, p1.y, 'pic2', p2.x, p2.y);
+        // console.log(p1.getBounds()); // x: 49.5, y: 249.5, wh: 101 (3.80 and dev)
+
     }
 }
 
 const config = {
     type: Phaser.WEBGL,
-    roundPixels: true,
-    scale: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-    },
-    backgroundColor: '#00ff00',
+    backgroundColor: "00dd00",
     parent: 'phaser-example',
-    scene: Example
+    scene: Example,
+    roundPixels: true,
+    width: 800,
+    height: 600
 };
 
 const game = new Phaser.Game(config);

d962e58b323a5ec5226b4a35fab09937f37d3c27
Zeke Chan <zeke.chan.tk@gmail.com>
2025-01-15 23:45:31 +0800

    Create 7007 tweenchain onstart.js

---
+++ public/src/bugs/7007 tweenchain onstart.js
@@ -0,0 +1,39 @@
+class Example extends Phaser.Scene
+{
+    create ()
+    {
+        this.objToTween = this.add.circle(this.scale.width / 2, this.scale.height / 2, 50, 0xff0000);
+
+        const chain = this.tweens.chain({
+            tweens: [
+                { targets: this.objToTween, duration: 500, props: { alpha: 0 } },
+                { targets: this.objToTween, duration: 500, props: { alpha: 1 } },
+                { targets: this.objToTween, duration: 500, props: { alpha: 0 } },
+                { targets: this.objToTween, duration: 500, props: { alpha: 1 } },
+            ]
+        });
+
+        chain.on(Phaser.Tweens.Events.TWEEN_START, () => 
+        {
+            console.log('added on start listener')
+        }); // I assume this should work
+
+        // const persistTweenWithoutCompleteDelay = this.add.tween({ targets: this.objToTween, duration: 500, props: { scale: 0 }, yoyo: true });
+
+        // persistTweenWithoutCompleteDelay.on(Phaser.Tweens.Events.TWEEN_START, () =>
+        // {
+        //     console.log('tween start!')
+        // });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d88',
+    scene: Example
+};
+
+const game = new Phaser.Game(config);

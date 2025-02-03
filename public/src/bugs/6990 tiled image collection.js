7965fe29423170ec4318ab6078790f6825e86e63
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-03 11:21:15 +0800

    Update 6990 tiled image collection.js

--- public/src/bugs/6990 tiled image collection.js
+++ public/src/bugs/6990 tiled image collection.js
@@ -19,7 +19,6 @@ class Example extends Phaser.Scene
 
     create ()
     {
-        // this.create2(); return;
         const map = this.make.tilemap({ key: 'map' });
 
         // The map was created with 8x8 tiles, but we want to load it with a 2x high resolution tileset
@@ -41,59 +40,8 @@ class Example extends Phaser.Scene
 
         const layer = map.createLayer('MapLayer', tilesetCollections, 0, 0);
 
-        const cursors = this.input.keyboard.createCursorKeys();
-
-        const controlConfig = {
-            camera: this.cameras.main,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            acceleration: 0.04,
-            drag: 0.0005,
-            maxSpeed: 0.7
-        };
-
-        this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-
-        this.cameras.main.setBounds(0, 0, layer.width, layer.height);
-
         const debugWindow = this.add.graphics();
         map.renderDebug(debugWindow);
-    }
-
-    create2 ()
-    {
-        const map = this.make.tilemap({ key: 'map2' });
-
-        // The map was created with 8x8 tiles, but we want to load it with a 2x high resolution tileset
-        map.setBaseTileSize(16, 16);
-
-        const tileset = map.addTilesetImage('DangerousKiss_bank.png', 'tiles', 16, 16);
-
-        const layer = map.createLayer('ShoeBox Tile Grab', tileset, 0, 0);
-
-        const cursors = this.input.keyboard.createCursorKeys();
-
-        const controlConfig = {
-            camera: this.cameras.main,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            acceleration: 0.04,
-            drag: 0.0005,
-            maxSpeed: 0.7
-        };
-
-        this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-
-        this.cameras.main.setBounds(0, 0, layer.width, layer.height);
-    }
-
-    update (time, delta)
-    {
-        this.controls.update(delta);
     }
 }
 

9671a4f476c3f83454a7e352058b3a13ca204713
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-13 17:58:53 +0800

    Create animationKeys.js

---
+++ public/src/games/my first game/animationKeys.js
@@ -0,0 +1,10 @@
+const ANIMATION_KEYS = {
+
+    PLAYER_LEFT: 'player-left',
+    PLAYER_RIGHT: 'player-right',
+    PLAYER_IDLE: 'player-idle',
+    
+    COIN_SPIN: 'coin-spin',
+};
+
+export default ANIMATION_KEYS;

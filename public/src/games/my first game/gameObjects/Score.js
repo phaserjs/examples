c891c0f2ebe57d3efa7e2715899d47e282758ae4
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-13 17:35:54 +0800

    Create Score.js

---
+++ public/src/games/my first game/gameObjects/Score.js
@@ -0,0 +1,22 @@
+export class Score extends Phaser.GameObjects.BitmapText
+{
+    moveY = 96;
+
+    constructor (scene, x, y, points)
+    {
+        super(scene, x, y, 'nokia16', points);
+
+        this.setOrigin(0.5);
+        scene.add.existing(this);
+        
+        scene.tweens.add({
+            targets: this,
+            y: y - this.moveY,
+            duration: 1000,
+            ease: 'Quad.easeOut',
+            onComplete: () => {
+                this.destroy();
+            }
+        });
+    }
+}

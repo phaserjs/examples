39ed38d4db3ef5804b7d8b0d6a765b11b9d39d41
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-13 17:35:22 +0800

    Create MysteryBox.js

---
+++ public/src/games/my first game/gameObjects/MysteryBox.js
@@ -0,0 +1,31 @@
+export class MysteryBox extends Phaser.Physics.Arcade.Sprite
+{
+    isActivated = false;
+    moveY = 16;
+
+    constructor (scene, x, y)
+    {
+        super(scene, x, y, 'fantasy', 52);
+
+        this.setOrigin(0, 0);
+        scene.add.existing(this);
+        scene.physics.add.existing(this, true);
+    }
+
+    activate()
+    {
+        if (this.isActivated || !this.body.touching.down) return;
+
+        this.isActivated = true;
+        this.setTint(0x666666);
+        this.scene.addCoin(this.x + (this.width*0.5), this.y - this.height + (this.height*0.5));
+
+        this.scene.tweens.add({
+            targets: this,
+            y: this.y - this.moveY,
+            duration: 100,
+            ease: 'Quad.easeOut',
+            yoyo: true,
+        });
+    }
+}

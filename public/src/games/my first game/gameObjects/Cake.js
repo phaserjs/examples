fe656ec4a4b8af8822b4c83937f16be504ea3661
zekechan <zeke.chan.tk@gmail.com>
2025-02-14 20:52:29 +0800

    Update Cake.js

--- public/src/games/my first game/gameObjects/Cake.js
+++ public/src/games/my first game/gameObjects/Cake.js
@@ -5,6 +5,9 @@ export class Cake extends Phaser.Physics.Arcade.Sprite
 {
     moveVelocity = 50;
     direction = -1;
+    sensorSize = 10;
+    groundSensor;
+    groundOffset;
 
     constructor(scene, x, y, direction = -1)
     {
@@ -13,17 +16,19 @@ export class Cake extends Phaser.Physics.Arcade.Sprite
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        this.setCollideWorldBounds(true, 0, 0, true);
+        this.setCollideWorldBounds(true);
         this.setDepth(100);
-        this.setDirection(direction);
+
         this.anims.play(ANIMATION_KEYS.CAKE_WALK, true);
+        this.groundOffset = { x: this.body.halfWidth + (this.sensorSize * 0.5), y: this.body.halfHeight + (this.sensorSize * 0.5) };
+        this.groundSensor = scene.addSensor(x - this.groundOffset.x, y + this.groundOffset.y, this.sensorSize, this.sensorSize);
+        this.setDirection(direction);
     }
 
     preUpdate (time, delta)
     {
         super.preUpdate(time, delta);
 
-        console.log(this.direction)
         if (this.direction === -1)
         {
             this.moveLeft();
@@ -34,23 +39,23 @@ export class Cake extends Phaser.Physics.Arcade.Sprite
         }
     }
 
-    onWorldBounds ()
-    {
-        this.setDirection();
-    }
-
     setDirection (direction)
     {
         this.direction = direction || this.direction * -1;
+
+        const sensorPosition = { x: this.x + (this.direction * this.groundOffset.x), y: this.y + this.groundOffset.y };
+        this.groundSensor.body.reset(sensorPosition.x, sensorPosition.y);
     }
 
     moveLeft ()
     {
-        this.setVelocityX(-this.moveVelocity);
+        const sensorPosition = { x: this.x - this.groundOffset.x, y: this.y + this.groundOffset.y };
 
+        this.setVelocityX(-this.moveVelocity);
+        this.groundSensor.setVelocityX(-this.moveVelocity);
         this.flipX = true;
 
-        if (!this.body.touching.bottom)
+        if (this.groundSensor.body.touching.none && !this.groundSensor.body.wasTouching.none)
         {
             this.setDirection();
         }
@@ -58,11 +63,14 @@ export class Cake extends Phaser.Physics.Arcade.Sprite
 
     moveRight ()
     {
+        const sensorPosition = { x: this.x + this.groundOffset.x, y: this.y + this.groundOffset.y };
+
         this.setVelocityX(this.moveVelocity);
+        this.groundSensor.setVelocityX(this.moveVelocity);
 
         this.flipX = false;
 
-        if (!this.body.touching.bottom)
+        if (this.groundSensor.body.touching.none && !this.groundSensor.body.wasTouching.none)
         {
             this.setDirection();
         }

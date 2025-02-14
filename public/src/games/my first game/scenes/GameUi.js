b7bb7d083ee0346c55efb3623a34e760e7fe4473
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-14 15:59:32 +0800

    Update GameUi.js

--- public/src/games/my first game/scenes/GameUi.js
+++ public/src/games/my first game/scenes/GameUi.js
@@ -3,14 +3,6 @@ import SPRITE_KEYS from '../spriteKeys.js';
 
 export class GameUi extends Phaser.Scene
 {
-    scoreText;
-    keyText;
-
-    score = 0;
-    keys = 0;
-
-    audio = {};
-
     constructor()
     {
         super('GameUi');
@@ -31,52 +23,78 @@ export class GameUi extends Phaser.Scene
 
     create ()
     {
+        this.initVariables();
+
         const background = this.add.graphics();
         // set a fill and line style
         background.fillStyle(0x000000);
         background.fillRect(0, 0, this.scale.width, 40);
 
         this.scoreText = this.add.bitmapText(10, 4, SPRITE_KEYS.FONT_NOKIA, 'Score: 0');
-        this.keyText = this.add.bitmapText(this.scale.width - 75, 4, SPRITE_KEYS.FONT_NOKIA, 'x 0');
+        this.keyText = this.add.bitmapText(this.scale.width - 75, 4, SPRITE_KEYS.FONT_NOKIA, `x ${this.keys}`);
 
         //  Grab a reference to the Game Scene
         const gameScene = this.scene.get('Game');
 
         //  Listen for events from it
-        gameScene.events.on('updateScore', function (points) 
-        {
-            this.score += points;
-            this.scoreText.text = `Score: ${this.score}`;
-        }, this);
+        // gameScene.events.on('updateScore', function (points) 
+        // {
+        //     this.score += points;
+        //     this.scoreText.text = `Score: ${this.score}`;
+        // }, this);
 
-        //  Listen for events from it
-        gameScene.events.on('updateKeys', function (keys) 
-        {
-            this.keys += keys;
-            this.keyText.text = `x ${this.keys}`;
-        }, this);
+        // //  Listen for events from it
+        // gameScene.events.on('updateKeys', function (keys) 
+        // {
+        //     this.keys += keys;
+        //     this.keyText.text = `x ${this.keys}`;
+        // }, this);
 
         const keyIcon = this.add.image(this.scale.width - 100, 20, SPRITE_KEYS.FANTASY_TILES, 51);
 
         this.initAudio();
     }
 
-    initAudio()
+    initVariables ()
+    {
+        this.scoreText = undefined;
+        this.keyText = undefined;
+
+        this.score = 0;
+        this.keys = 0;
+
+        this.audio = {};
+    }
+
+    initAudio ()
     {
         for (let key in AUDIO_KEYS)
         {
-            this.audio[AUDIO_KEYS[key]] = this.sound.add(AUDIO_KEYS[key]);
+            this.audio[ AUDIO_KEYS[ key ] ] = this.sound.add(AUDIO_KEYS[ key ]);
         }
     }
 
-    playAudio(key, overlap = true)
+    playAudio (key, overlap = true)
     {
-        if (!overlap && this.audio[key].isPlaying) return;
+        if (!overlap && this.audio[ key ].isPlaying) return;
 
-        this.audio[key].play();
+        this.audio[ key ].play();
     }
 
-    getKeys()
+    updateScore (points) 
+    {
+        this.score += points;
+        this.scoreText.text = `Score: ${this.score}`;
+    };
+
+    updateKeys (keys)
+    {
+        console.log(keys);
+        this.keys += keys;
+        this.keyText.text = `x ${this.keys}`;
+    }
+
+    getKeys ()
     {
         return this.keys;
     }

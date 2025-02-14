e13d2a5f06fc2f3857d949a0c7fd56d1f9fbf77e
Zeke Chan <zeke.chan.tk@gmail.com>
2025-02-14 15:59:16 +0800

    Update game.js

--- public/src/games/my first game/game.js
+++ public/src/games/my first game/game.js
@@ -13,7 +13,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 1000 },
-            debug: false
+            debug: true
         }
     },
     scene: [

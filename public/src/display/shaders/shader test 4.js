71ec0fc78299726049fa5fbf5c6fcc9e034e1efa
Zeke Chan <zeke.chan.tk@gmail.com>
2025-04-25 10:30:34 +0800

    Update shader test 4.js

--- public/src/display/shaders/shader test 4.js
+++ public/src/display/shaders/shader test 4.js
@@ -5,7 +5,7 @@ class Example extends Phaser.Scene
         super();
     }
 
-    preload()
+    preload ()
     {
         // this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
         this.load.glsl('spiralTime', 'assets/shaders/spiralTime.frag');
@@ -17,45 +17,25 @@ class Example extends Phaser.Scene
         this.load.image('splat3', 'assets/pics/splat3.png');
     }
 
-    create()
+    create ()
     {
         const shape1 = this.make.graphics().fillCircle(400, 300, 300);
         const shape2 = this.make.graphics().fillCircle(400, 300, 200);
 
         const geomask1 = shape1.createGeometryMask();
-            geomask1.geometryMask.setName('geo1');
+        geomask1.geometryMask.setName('geo1');
         const geomask2 = shape2.createGeometryMask();
-            geomask2.geometryMask.setName('geo2');
+        geomask2.geometryMask.setName('geo2');
 
         const maskImage1 = this.make.image({ x: 400, y: 300, key: 'splat1', add: false });
-        const maskImage2 = this.make.image({ x: 400, y: 300, key: 'splat2', add: false });
-        const maskImage3 = this.make.image({ x: 400, y: 300, key: 'splat3', add: false });
-        const maskImage4 = this.make.image({ x: 400, y: 300, key: 'bunny', add: false });
-
-        // const bitmask1 = maskImage1.createBitmapMask();
-        // const bitmask2 = maskImage2.createBitmapMask();
-        // const bitmask3 = maskImage3.createBitmapMask();
-        // const bitmask4 = maskImage4.createBitmapMask();
-
-        // bitmask3.invertAlpha = true;
-
-        // mask3.invertAlpha = true;
-
-        // this.cameras.main.setMask(geomask1, false);
-        // this.cameras.main.setMask(bitmask1, false);
+        const maskImage2 = this.make.image({ x: 400, y: 300, key: 'splat3', add: false });
+        
         this.cameras.main.filters.external.addMask(maskImage1);
 
         this.add.image(700, 300, 'bunny').setName('bunny');
 
         this.add.image(400, 300, 'pic');
-        // this.add.image(400, 300, 'pic').setName('rick').setMask(geomask2);
-        // this.add.image(400, 300, 'pic').setMask(bitmask3);
 
-        // var shader = this.add.shader(600, 300, 800, 600, fragmentShader7);
-
-        // mask4.invertAlpha = true;
-        // var shader = this.add.shader(400, 300, 800, 400, fragmentShader);
-        
         const shader = this.add.shader({
             name: 'spiralTime',
             fragmentKey: 'spiralTime',
@@ -64,21 +44,14 @@ class Example extends Phaser.Scene
                 setUniform('time', this.game.loop.getDuration());
             },
         }, 400, 300, this.scale.width, this.scale.width);
-        shader.enableFilters().filters.external.addMask(maskImage3);
-        // var shader = this.add.shader(700, 300, 800, 400, fragmentShader).setMask(mask2);
-
-        // shader.setPointer(this.input.activePointer);
+        shader.enableFilters().filters.external.addMask(maskImage2);
 
         this.text = this.add.text(80, 320, '', { font: '16px Courier', fill: '#00ff00' }).setName('text');
 
         this.add.image(400, 300, 'logo').setName('logo');
-        // this.add.image(400, 300, 'logo').setMask(mask2);
-        // this.add.image(300, 300, 'logo').setMask(mask4);
-
-        // this.add.image(400, 500, 'logo').setMask(mask4);
     }
 
-    update()
+    update ()
     {
         if (this.text)
         {
